5f9fe7c88e8266d2672f217eec43b43a05fe4101
Anton Kozhukhovsky <a.kozhukhovsky@gmail.com>
2019-10-15 16:50:09 +0300

    refactored

--- src/test/java/core/basesyntax/CalculatorTest.java
+++ src/test/java/core/basesyntax/CalculatorTest.java
@@ -1,71 +1,124 @@
 package core.basesyntax;
 
+import org.junit.After;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class CalculatorTest {
 
-    private static Calculator calculator;
     private static final double DELTA = 0.0001;
+    private static Calculator calculator;
+    private double expectedResult;
+    private double actualResult;
 
     @BeforeClass
     public static void setUp() {
         calculator = new Calculator();
     }
 
+    @After
+    public void tearDown() {
+        expectedResult = 0;
+        actualResult = 0;
+    }
+
     @Test
     public void methodCalculateShouldReturnCorrectResultOfAdditionalWhenInputDataIsPositive() {
-        assertEquals(4.0, calculator.calculate(2, 2, '+'), DELTA);
-        assertEquals(52489.0, calculator.calculate(15648, 36841, '+'), DELTA);
+        expectedResult = 4.0;
+        actualResult = calculator.calculate(2, 2, '+');
+        assertEquals(expectedResult, actualResult, DELTA);
+
+        expectedResult = 52489.0;
+        actualResult = calculator.calculate(15648, 36841, '+');
+        assertEquals(expectedResult, actualResult, DELTA);
     }
 
     @Test
     public void methodCalculateShouldReturnCorrectResultOfAdditionalWhenInputDataIsNegative() {
-        assertEquals(-450.0, calculator.calculate(-100, -350, '+'), DELTA);
-        assertEquals(-174628.0, calculator.calculate(-109341, -65287, '+'), DELTA);
+        expectedResult = -450.0;
+        actualResult = calculator.calculate(-100, -350, '+');
+        assertEquals(expectedResult, actualResult, DELTA);
+
+        expectedResult = -174628.0;
+        actualResult = calculator.calculate(-109341, -65287, '+');
+        assertEquals(expectedResult, actualResult, DELTA);
     }
 
     @Test
     public void methodCalculateShouldReturnCorrectResultOfSubtractionWhenInputDataIsPositive() {
-        assertEquals(2.0, calculator.calculate(4, 2, '-'), DELTA);
-        assertEquals(22143.0, calculator.calculate(54632, 32489, '-'), DELTA);
+        expectedResult = 2.0;
+        actualResult = calculator.calculate(4, 2, '-');
+        assertEquals(expectedResult, actualResult, DELTA);
+
+        expectedResult = 22143.0;
+        actualResult = calculator.calculate(54632, 32489, '-');
+        assertEquals(expectedResult, actualResult, DELTA);
     }
 
     @Test
     public void methodCalculateShouldReturnCorrectResultOfSubtractionWhenInputDataIsNegative() {
-        assertEquals(2.0, calculator.calculate(-2, -4, '-'), DELTA);
-        assertEquals(-1495.0, calculator.calculate(-5147, -3652, '-'), DELTA);
+        expectedResult = 2.0;
+        actualResult = calculator.calculate(-2, -4, '-');
+        assertEquals(expectedResult, actualResult, DELTA);
+
+        expectedResult = -1495.0;
+        actualResult = calculator.calculate(-5147, -3652, '-');
+        assertEquals(expectedResult, actualResult, DELTA);
     }
 
     @Test
     public void methodCalculateShouldReturnCorrectResultOfMultiplicationWhenInputDataIsPositive() {
-        assertEquals(4.0, calculator.calculate(2, 2, '*'), DELTA);
-        assertEquals(1926672.0, calculator.calculate(5412, 356, '*'), DELTA);
+        expectedResult = 4.0;
+        actualResult = calculator.calculate(2, 2, '*');
+        assertEquals(expectedResult, actualResult, DELTA);
+
+        expectedResult = 1926672.0;
+        actualResult = calculator.calculate(5412, 356, '*');
+        assertEquals(expectedResult, actualResult, DELTA);
     }
 
     @Test
     public void methodCalculateShouldReturnCorrectResultOfMultiplicationWhenInputDataIsNegative() {
-        assertEquals(108.0, calculator.calculate(-54, -2, '*'), DELTA);
-        assertEquals(10491852.0, calculator.calculate(-4234, -2478, '*'), DELTA);
+        expectedResult = 108.0;
+        actualResult = calculator.calculate(-54, -2, '*');
+        assertEquals(expectedResult, actualResult, DELTA);
+
+        expectedResult = 10491852.0;
+        actualResult = calculator.calculate(-4234, -2478, '*');
+        assertEquals(expectedResult, actualResult, DELTA);
     }
 
     @Test
     public void methodCalculateShouldReturnCorrectResultOfDivisionWhenInputDataIsPositive() {
-        assertEquals(10.0, calculator.calculate(100, 10, '/'), DELTA);
-        assertEquals(796.0, calculator.calculate(54128, 68, '/'), DELTA);
+        expectedResult = 10.0;
+        actualResult = calculator.calculate(100, 10, '/');
+        assertEquals(expectedResult, actualResult, DELTA);
+
+        expectedResult = 796.0;
+        actualResult = calculator.calculate(54128, 68, '/');
+        assertEquals(expectedResult, actualResult, DELTA);
     }
 
     @Test
     public void methodCalculateShouldReturnCorrectResultOfDivisionWhenInputDataIsNegative() {
-        assertEquals(10.0, calculator.calculate(-100, -10, '/'), DELTA);
-        assertEquals(796.0, calculator.calculate(-54128, -68, '/'), DELTA);
+        expectedResult = 10.0;
+        actualResult = calculator.calculate(-100, -10, '/');
+        assertEquals(expectedResult, actualResult, DELTA);
+
+        expectedResult = 796.0;
+        actualResult = calculator.calculate(-54128, -68, '/');
+        assertEquals(expectedResult, actualResult, DELTA);
     }
 
     @Test
     public void methodCalculateShouldReturnCorrectResultOfDivisionWhenDividendEqualsZero() {
-        assertEquals(0.0, calculator.calculate(0, -123, '/'), DELTA);
-        assertEquals(0.0, calculator.calculate(0, 234, '/'), DELTA);
+        expectedResult = 0.0;
+        actualResult = calculator.calculate(0, -123, '/');
+        assertEquals(expectedResult, actualResult, DELTA);
+
+        actualResult = calculator.calculate(0, 234, '/');
+        assertEquals(expectedResult, actualResult, DELTA);
     }
 
     @Test(expected = ArithmeticException.class)
@@ -75,19 +128,30 @@ public class CalculatorTest {
 
     @Test
     public void methodCalculateShouldReturnCorrectExponentiationWhenInputDataIsPositive() {
-        assertEquals(16.0, calculator.calculate(2, 4, '^'), DELTA);
-        assertEquals(512.0, calculator.calculate(2, 9, '^'), DELTA);
+        expectedResult = 16.0;
+        actualResult = calculator.calculate(2, 4, '^');
+        assertEquals(expectedResult, actualResult, DELTA);
+
+        expectedResult = 512.0;
+        actualResult = calculator.calculate(2, 9, '^');
+        assertEquals(expectedResult, actualResult, DELTA);
     }
 
     @Test
     public void methodCalculateShouldReturnCorrectExponentiationWhenPowerEqualsZero() {
-        assertEquals(1, calculator.calculate(2, 0, '^'), DELTA);
-        assertEquals(1, calculator.calculate(234, 0, '^'), DELTA);
+        expectedResult = 1;
+        actualResult = calculator.calculate(2, 0, '^');
+        assertEquals(expectedResult, actualResult, DELTA);
+
+        actualResult = calculator.calculate(234, 0, '^');
+        assertEquals(expectedResult, actualResult, DELTA);
     }
 
     @Test
     public void methodCalculateShouldReturnCorrectExponentiationWhenInputDataEqualsZero() {
-        assertEquals(1, calculator.calculate(0, 0, '^'), DELTA);
+        expectedResult = 1;
+        actualResult = calculator.calculate(0, 0, '^');
+        assertEquals(expectedResult, actualResult, DELTA);
     }
 
     @Test(expected = IllegalArgumentException.class)
