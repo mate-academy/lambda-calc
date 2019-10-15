cd80dfe978812932122343d203832a3dab09e9f4
Sasha192 <sasha192.bunin@gmail.com>
2019-10-15 08:22:22 +0300

    fixed some issues

--- src/main/java/core/basesyntax/Calculator.java
+++ src/main/java/core/basesyntax/Calculator.java
@@ -1,12 +1,9 @@
 package core.basesyntax;
 
-import org.apache.log4j.Logger;
-
 import java.util.HashMap;
 import java.util.function.BinaryOperator;
 
 public class Calculator {
-    private static Logger LOGGER = Logger.getLogger(Calculator.class);
     private static HashMap<Character, BinaryOperator<Long>> hashMap;
 
     static {
@@ -15,11 +12,11 @@ public class Calculator {
         hashMap.put(Character.valueOf('-'), (a, b) -> a - b);
         hashMap.put(Character.valueOf('/'), (a, b) -> a / b);
         hashMap.put(Character.valueOf('*'), (a, b) -> a * b);
-        hashMap.put(Character.valueOf('^'), (a, b) -> (long)Math.pow(a, b));
+        hashMap.put(Character.valueOf('^'), (a, b) -> (long) Math.pow(a, b));
     }
 
     public static Long apply(Long a, Character op, Long b)
-            throws IllegalArgumentException, NullPointerException{
+            throws IllegalArgumentException, NullPointerException {
         return hashMap.get(op).apply(a, b);
     }
 }
