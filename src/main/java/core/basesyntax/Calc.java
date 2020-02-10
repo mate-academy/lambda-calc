b81892834735e7933835eebb9c153cecabdb81c5
MrNopi <Smart.like1998@gmail.com>
2020-02-10 23:20:34 +0300

    Made calculate method non-static

--- src/main/java/core/basesyntax/Calc.java
+++ src/main/java/core/basesyntax/Calc.java
@@ -7,7 +7,7 @@ import java.util.function.BinaryOperator;
 public class Calc {
     private static Map<Character, BinaryOperator<Double>> operations;
 
-    public static void main() {
+    public static void main(String[] args) {
         operations = new HashMap<>();
         operations.put('+', Double::sum);
         operations.put('-', (x, y) -> x - y);
@@ -16,7 +16,7 @@ public class Calc {
         operations.put('p', Math::pow);
     }
 
-    public static double calculate(Double x, Double y, Character operator) {
+    public double calculate(Double x, Double y, Character operator) {
         return operations.get(operator).apply(x, y);
     }
 }
--- src/test/java/core/basesyntax/CalcTest.java
+++ src/test/java/core/basesyntax/CalcTest.java
@@ -8,77 +8,79 @@ public class CalcTest {
     private Double a = 2d;
     private Double b = 1d;
     private Double nullDouble = null;
+    private static Calc calc;
 
     @BeforeClass
     public static void beforeClass() {
-        Calc.main();
+        Calc.main(new String[0]);
+        calc = new Calc();
     }
 
     @Test
     public void addingTestCorrect() {
-        Double result = Calc.calculate(a, b, '+');
+        Double result = calc.calculate(a, b, '+');
         Assert.assertEquals(java.util.Optional.of(a + b).get(), result);
     }
 
     @Test
     public void addingTestWrong() {
-        Double result = Calc.calculate(a, b, '+');
+        Double result = calc.calculate(a, b, '+');
         Assert.assertNotEquals(a * b, result);
 
     }
 
     @Test
     public void subtractionTestCorrect() {
-        Double result = Calc.calculate(a, b, '-');
+        Double result = calc.calculate(a, b, '-');
         Assert.assertEquals(java.util.Optional.of(a - b).get(), result);
     }
 
     @Test
     public void subtractionTestWrong() {
-        Double result = Calc.calculate(a, b, '-');
+        Double result = calc.calculate(a, b, '-');
         Assert.assertNotEquals(a / b, result);
     }
 
     @Test
     public void multiplyTestCorrect() {
-        Double result = Calc.calculate(a, b, '*');
+        Double result = calc.calculate(a, b, '*');
         Assert.assertEquals(java.util.Optional.of(a * b).get(), result);
     }
 
     @Test
     public void multiplyTestWrong() {
-        Double result = Calc.calculate(a, b, '*');
+        Double result = calc.calculate(a, b, '*');
         Assert.assertNotEquals(a - b, result);
     }
 
     @Test
     public void divisionTestCorrect() {
-        Double result = Calc.calculate(a, b, '/');
+        Double result = calc.calculate(a, b, '/');
         Assert.assertEquals(java.util.Optional.of(a / b).get(), result);
     }
 
     @Test
     public void divisionTestWrong() {
-        Double result = Calc.calculate(a, b, '*');
+        Double result = calc.calculate(a, b, '*');
         Assert.assertNotEquals(a + b, result);
     }
 
     @Test
     public void powerTestCorrect() {
-        Double result = Calc.calculate(a, b, 'p');
+        Double result = calc.calculate(a, b, 'p');
         Assert.assertEquals(java.util.Optional.of(Math.pow(a, b)).get(), result);
     }
 
     @Test
     public void powerTestWrong() {
-        Double result = Calc.calculate(a, b, '+');
+        Double result = calc.calculate(a, b, '+');
         Assert.assertNotEquals(Math.pow(a, b), result);
     }
 
     @Test
     public void powerTestNull() {
         try {
-            Double result = Calc.calculate(nullDouble, b, 'p');
+            Double result = calc.calculate(nullDouble, b, 'p');
         } catch(NullPointerException e) {
             return;
         }
@@ -88,7 +90,7 @@ public class CalcTest {
     @Test
     public void subtractionTestNull() {
         try {
-            Double result = Calc.calculate(nullDouble, b, '-');
+            Double result = calc.calculate(nullDouble, b, '-');
         } catch(NullPointerException e) {
             return;
         }
@@ -98,7 +100,7 @@ public class CalcTest {
     @Test
     public void substitutionTestNull() {
         try {
-            Double result = Calc.calculate(nullDouble, b, '+');
+            Double result = calc.calculate(nullDouble, b, '+');
         } catch(NullPointerException e) {
             return;
         }
@@ -108,7 +110,7 @@ public class CalcTest {
     @Test
     public void divisionTestNull() {
         try {
-            Double result = Calc.calculate(nullDouble, b, '/');
+            Double result = calc.calculate(nullDouble, b, '/');
         } catch(NullPointerException e) {
             return;
         }
@@ -118,7 +120,7 @@ public class CalcTest {
     @Test
     public void multiplyingTestNull() {
         try {
-            Double result = Calc.calculate(nullDouble, b, '*');
+            Double result = calc.calculate(nullDouble, b, '*');
         } catch(NullPointerException e) {
             return;
         }
