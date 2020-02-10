ec0ef57ce3bb5d4f0f65cc044fb6e8cfc79d288a
ElvenNurse <58395217+ElvenNurse@users.noreply.github.com>
2020-02-10 19:12:08 +0200

    Reworked with Map

--- src/main/java/core/basesyntax/Calculator.java
+++ src/main/java/core/basesyntax/Calculator.java
@@ -1,22 +1,21 @@
 package core.basesyntax;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BinaryOperator;
 import java.util.function.DoubleBinaryOperator;
 
 public class Calculator {
-    public static Double calculate(double num1, double num2, Character operator) {
+    public double calculate(double num1, double num2, Character operator) {
         if (num2 == 0 && operator == '/') {
             throw new IllegalArgumentException("Division by 0 is not possible");
         }
-        DoubleBinaryOperator result =
-                operator == '+' ? Double::sum
-                        : operator == '-' ? (a, b) -> a - b
-                        : operator == '*' ? (a, b) -> a * b
-                        : operator == '/' ? (a, b) -> a / b
-                        : operator == '^' ? Math::pow
-                        : (a, b) -> {
-                            throw new IllegalArgumentException(
-                                    "Illegal operator, use only + - / * ^ as operator");
-                        };
-        return result.applyAsDouble(num1, num2);
+        Map<Character, DoubleBinaryOperator> mathOperations = new HashMap<>();
+        mathOperations.put('+', Double::sum);
+        mathOperations.put('-', (a, b) -> a - b);
+        mathOperations.put('*', (a, b) -> a * b);
+        mathOperations.put('/', (a, b) -> a / b);
+        mathOperations.put('^', Math::pow);
+        return mathOperations.get(operator).applyAsDouble(num1, num2);
     }
 }
--- src/test/java/core/basesyntax/CalculatorTest.java
+++ src/test/java/core/basesyntax/CalculatorTest.java
@@ -1,134 +1,135 @@
 package core.basesyntax;
 
 import org.junit.Assert;
-import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class CalculatorTest {
     private static final double DELTA = 0.001;
+    private static Calculator calculator;
 
-    @Before
-    public void before() {
-        Calculator calculator = new Calculator();
+    @BeforeClass
+    public static void before() {
+        calculator = new Calculator();
     }
 
     @Test
     public void sumValidTest() {
-        double result = Calculator.calculate(10d, 20d, '+');
+        double result = calculator.calculate(10d, 20d, '+');
         Assert.assertEquals(30d, result, DELTA);
-        result = Calculator.calculate(10d, 0d, '+');
+        result = calculator.calculate(10d, 0d, '+');
         Assert.assertEquals(10d, result, DELTA);
-        result = Calculator.calculate(10.501, 20.302, '+');
+        result = calculator.calculate(10.501, 20.302, '+');
         Assert.assertEquals(30.803, result, DELTA);
     }
 
     @Test
     public void sumInValidTest() {
-        double result = Calculator.calculate(10d, 20d, '+');
+        double result = calculator.calculate(10d, 20d, '+');
         Assert.assertNotEquals(35d, result);
     }
 
     @Test(expected = NullPointerException.class)
     public void sumNullTest() {
         Double a = null;
-        double result = Calculator.calculate(10d, a, '+');
+        double result = calculator.calculate(10d, a, '+');
     }
 
     @Test
     public void subValidTest() {
-        double result = Calculator.calculate(50D, 20D, '-');
+        double result = calculator.calculate(50D, 20D, '-');
         Assert.assertEquals(30d, result, DELTA);
-        result = Calculator.calculate(10D, 0D, '-');
+        result = calculator.calculate(10D, 0D, '-');
         Assert.assertEquals(10d, result, DELTA);
-        result = Calculator.calculate(50.505, 20.302, '-');
+        result = calculator.calculate(50.505, 20.302, '-');
         Assert.assertEquals(30.203, result, DELTA);
     }
 
     @Test
     public void subInValidTest() {
-        double result = Calculator.calculate(10d, 20d, '-');
+        double result = calculator.calculate(10d, 20d, '-');
         Assert.assertNotEquals(35d, result, DELTA);
     }
 
     @Test(expected = NullPointerException.class)
     public void subNullTest() {
         Double a = null;
-        Double result = Calculator.calculate(10d, a, '-');
+        Double result = calculator.calculate(10d, a, '-');
     }
 
     @Test
     public void multValidTest() {
-        double result = Calculator.calculate(10d, 20d, '*');
+        double result = calculator.calculate(10d, 20d, '*');
         Assert.assertEquals(200d, result, DELTA);
-        result = Calculator.calculate(10d, 0d, '*');
+        result = calculator.calculate(10d, 0d, '*');
         Assert.assertEquals(0d, result, DELTA);
-        result = Calculator.calculate(10.5, 20.3, '*');
+        result = calculator.calculate(10.5, 20.3, '*');
         Assert.assertEquals(213.15, result, DELTA);
     }
 
     @Test
     public void multInValidTest() {
-        double result = Calculator.calculate(10d, 20d, '*');
+        double result = calculator.calculate(10d, 20d, '*');
         Assert.assertNotEquals(35d, result);
     }
 
     @Test(expected = NullPointerException.class)
     public void multNullTest() {
         Double a = null;
-        double result = Calculator.calculate(10d, a, '*');
+        double result = calculator.calculate(10d, a, '*');
     }
 
     @Test
     public void divValidTest() {
-        double result = Calculator.calculate(40d, 20d, '/');
+        double result = calculator.calculate(40d, 20d, '/');
         Assert.assertEquals(2d, result, DELTA);
-        result = Calculator.calculate(0d, 10d, '/');
+        result = calculator.calculate(0d, 10d, '/');
         Assert.assertEquals(0d, result, DELTA);
-        result = Calculator.calculate(10.2, 20.4, '/');
+        result = calculator.calculate(10.2, 20.4, '/');
         Assert.assertEquals(0.5, result, DELTA);
     }
 
     @Test
     public void divInValidTest() {
-        double result = Calculator.calculate(10d, 20d, '/');
+        double result = calculator.calculate(10d, 20d, '/');
         Assert.assertNotEquals(35d, result);
     }
 
     @Test(expected = NullPointerException.class)
     public void divNullTest() {
         Double a = null;
-        double result = Calculator.calculate(10d, a, '/');
+        double result = calculator.calculate(10d, a, '/');
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void divZeroTest() {
-        double result = Calculator.calculate(10d, 0, '/');
+        double result = calculator.calculate(10d, 0, '/');
     }
 
     @Test
     public void powValidTest() {
-        double result = Calculator.calculate(2d, 10d, '^');
+        double result = calculator.calculate(2d, 10d, '^');
         Assert.assertEquals(1024d, result, DELTA);
-        result = Calculator.calculate(4d, 2d, '^');
+        result = calculator.calculate(4d, 2d, '^');
         Assert.assertEquals(16d, result, DELTA);
-        result = Calculator.calculate(16, 0.5, '^');
+        result = calculator.calculate(16, 0.5, '^');
         Assert.assertEquals(4, result, DELTA);
     }
 
     @Test
     public void powInValidTest() {
-        double result = Calculator.calculate(10d, 2d, '^');
+        double result = calculator.calculate(10d, 2d, '^');
         Assert.assertNotEquals(10d, result);
     }
 
     @Test(expected = NullPointerException.class)
     public void powNullTest() {
         Double a = null;
-        double result = Calculator.calculate(10d, a, '^');
+        double result = calculator.calculate(10d, a, '^');
     }
 
-    @Test(expected = IllegalArgumentException.class)
+    @Test(expected = NullPointerException.class)
     public void wrongArgument() {
-        double result = Calculator.calculate(10d, 20d, 'a');
+        double result = calculator.calculate(10d, 20d, 'a');
     }
 }
