fe8ea9ba2fde4ebe12c094833eaa08dc12326cca
Dmytro Hryhoruk <dimamyhgrygoruk@gmail.com>
2020-02-10 11:29:35 +0200

    added solution

---
+++ src/main/java/core/basesyntax/Calculator.java
@@ -0,0 +1,15 @@
+package core.basesyntax;
+
+import java.util.function.DoubleBinaryOperator;
+
+public class Calculator {
+    public double calculete(double first, double second, char operation) {
+        DoubleBinaryOperator operator =
+                operation == '+' ? (a, b) -> a + b :
+                        operation == '-' ? (a, b) -> a - b :
+                                operation == '/' ? (a, b) -> a / b :
+                                        operation == '*' ? (a, b) -> a * b :
+                                                operation == 'p' ? (a, b) -> Math.pow(a, b) : null;
+        return operator.applyAsDouble(first, second);
+    }
+}
