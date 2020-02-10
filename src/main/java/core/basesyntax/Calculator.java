67114ac17c5586c6778a3549c9d75f422ce1c297
Dmytro Hryhoruk <dimamyhgrygoruk@gmail.com>
2020-02-10 15:03:30 +0200

    formatted code

--- src/main/java/core/basesyntax/Calculator.java
+++ src/main/java/core/basesyntax/Calculator.java
@@ -6,10 +6,10 @@ public class Calculator {
     public double calculete(double first, double second, char operation) {
         DoubleBinaryOperator operator =
                 operation == '+' ? (a, b) -> a + b :
-                        operation == '-' ? (a, b) -> a - b :
-                                operation == '/' ? (a, b) -> a / b :
-                                        operation == '*' ? (a, b) -> a * b :
-                                                operation == 'p' ? (a, b) -> Math.pow(a, b) : null;
+                operation == '-' ? (a, b) -> a - b :
+                operation == '/' ? (a, b) -> a / b :
+                operation == '*' ? (a, b) -> a * b :
+                operation == 'p' ? (a, b) -> Math.pow(a, b) : null;
         return operator.applyAsDouble(first, second);
     }
 }
