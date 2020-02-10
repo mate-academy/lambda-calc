cd1f93edf5ec1e47fd7d3ca449aeb2499ab0420a
karsv <Pbvf2019!>
2020-02-10 17:15:20 +0200

    made fixes

--- src/main/java/core/basesyntax/IllegalSignifier.java
+++
@@ -1,7 +0,0 @@
-package core.basesyntax;
-
-public class IllegalSignifier extends RuntimeException {
-    public IllegalSignifier(String message) {
-        super(message);
-    }
-}
--- src/test/java/core/basesyntax/CalculatorTest.java
+++ src/test/java/core/basesyntax/CalculatorTest.java
@@ -6,7 +6,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class CalculatorTest {
-    private final double DELTA = 0.00001;
+    private static final double DELTA = 0.00001;
 
     private Calculator calculator = null;
 
@@ -16,85 +16,85 @@ public class CalculatorTest {
     }
 
     @Test
-    public void operationsWitPositiveNumbers() {
+    public void testSum() {
         assertEquals("The sum of calculate",
                 12, calculator.calculate(7, 5, '+'), DELTA);
-        assertEquals("The subtrction of calculate",
-                9, calculator.calculate(21, 12, '-'), DELTA);
-        assertEquals("The multipiler of calculate",
-                18, calculator.calculate(2, 9, '*'), DELTA);
-        assertEquals("The division of calculate",
-                5, calculator.calculate(35, 7, '/'), DELTA);
-        assertEquals("The pow of calculate",
-                144, calculator.calculate(12, 2, '^'), DELTA);
-        assertEquals("The pow of calculate",
-                12, calculator.calculate(12, 1, '^'), DELTA);
-    }
-
-    @Test
-    public void operationsWitNegativeNumbers() {
         assertEquals("The sum of calculate",
                 -2, calculator.calculate(-7, 5, '+'), DELTA);
         assertEquals("The sum of calculate",
                 2, calculator.calculate(7, -5, '+'), DELTA);
         assertEquals("The sum of calculate",
                 -12, calculator.calculate(-7, -5, '+'), DELTA);
-
-        assertEquals("The subtrction of calculate",
-                33, calculator.calculate(21, -12, '-'), DELTA);
-        assertEquals("The subtrction of calculate",
-                -33, calculator.calculate(-21, 12, '-'), DELTA);
-        assertEquals("The subtrction of calculate",
-                -9, calculator.calculate(-21, -12, '-'), DELTA);
-
-        assertEquals("The multipiler of calculate",
-                -18, calculator.calculate(-2, 9, '*'), DELTA);
-        assertEquals("The multipiler of calculate",
-                -18, calculator.calculate(2, -9, '*'), DELTA);
-        assertEquals("The multipiler of calculate",
-                18, calculator.calculate(-2, -9, '*'), DELTA);
-
-        assertEquals("The division of calculate",
-                -5, calculator.calculate(-35, 7, '/'), DELTA);
-        assertEquals("The division of calculate",
-                -5, calculator.calculate(35, -7, '/'), DELTA);
-        assertEquals("The division of calculate",
-                5, calculator.calculate(-35, -7, '/'), DELTA);
-
-        assertEquals("The pow of calculate1",
-                0.00694444, calculator.calculate(12, -2, '^'), DELTA);
-        assertEquals("The pow of calculate2",
-                144, calculator.calculate(-12, 2, '^'), DELTA);
-        assertEquals("The pow of calculate3",
-                0.00694444, calculator.calculate(-12, -2, '^'), DELTA);
-    }
-
-    @Test
-    public void operationsWitZero() {
         assertEquals("The sum of calculate",
                 7, calculator.calculate(7, 0, '+'), DELTA);
         assertEquals("The sum of calculate",
                 5, calculator.calculate(0, 5, '+'), DELTA);
         assertEquals("The sum of calculate",
                 0, calculator.calculate(0, 0, '+'), DELTA);
+    }
 
+    @Test
+    public void testSubtraction() {
+        assertEquals("The subtrction of calculate",
+                9, calculator.calculate(21, 12, '-'), DELTA);
+        assertEquals("The subtrction of calculate",
+                33, calculator.calculate(21, -12, '-'), DELTA);
+        assertEquals("The subtrction of calculate",
+                -33, calculator.calculate(-21, 12, '-'), DELTA);
+        assertEquals("The subtrction of calculate",
+                -9, calculator.calculate(-21, -12, '-'), DELTA);
         assertEquals("The subtrction of calculate",
                 21, calculator.calculate(21, 0, '-'), DELTA);
         assertEquals("The subtrction of calculate",
                 -12, calculator.calculate(0, 12, '-'), DELTA);
         assertEquals("The subtrction of calculate",
                 0, calculator.calculate(0, 0, '-'), DELTA);
+    }
 
+    @Test
+    public void testMultiplier() {
+        assertEquals("The multipiler of calculate",
+                18, calculator.calculate(2, 9, '*'), DELTA);
+        assertEquals("The multipiler of calculate",
+                -18, calculator.calculate(-2, 9, '*'), DELTA);
+        assertEquals("The multipiler of calculate",
+                -18, calculator.calculate(2, -9, '*'), DELTA);
+        assertEquals("The multipiler of calculate",
+                18, calculator.calculate(-2, -9, '*'), DELTA);
         assertEquals("The multipiler of calculate",
                 0, calculator.calculate(0, 9, '*'), DELTA);
         assertEquals("The multipiler of calculate",
                 0, calculator.calculate(2, 0, '*'), DELTA);
         assertEquals("The multipiler of calculate",
                 0, calculator.calculate(0, 0, '*'), DELTA);
+    }
 
+    @Test
+    public void testDivision() {
+        assertEquals("The division of calculate",
+                5, calculator.calculate(35, 7, '/'), DELTA);
+        assertEquals("The division of calculate",
+                -5, calculator.calculate(-35, 7, '/'), DELTA);
+        assertEquals("The division of calculate",
+                -5, calculator.calculate(35, -7, '/'), DELTA);
+        assertEquals("The division of calculate",
+                5, calculator.calculate(-35, -7, '/'), DELTA);
         assertEquals("The division of calculate",
                 0, calculator.calculate(0, 7, '/'), DELTA);
+    }
 
+    @Test
+    public void testPow() {
+        assertEquals("The pow of calculate",
+                144, calculator.calculate(12, 2, '^'), DELTA);
+        assertEquals("The pow of calculate",
+                12, calculator.calculate(12, 1, '^'), DELTA);
+        assertEquals("The pow of calculate1",
+                0.00694444, calculator.calculate(12, -2, '^'), DELTA);
+        assertEquals("The pow of calculate2",
+                144, calculator.calculate(-12, 2, '^'), DELTA);
+        assertEquals("The pow of calculate3",
+                0.00694444, calculator.calculate(-12, -2, '^'), DELTA);
         assertEquals("The pow of calculate",
                 1, calculator.calculate(12, 0, '^'), DELTA);
         assertEquals("The pow of calculate",
