9b6b5e02ed3d93afa1be29b77a498ba2e0fc8f34
IegorSoboliev <evsobolev@gmail.com>
2020-02-11 12:53:07 +0200

    implemented your recommendations

--- src/main/java/core/basesyntax/Calculator.java
+++ src/main/java/core/basesyntax/Calculator.java
@@ -6,12 +6,12 @@ import java.util.function.DoubleBinaryOperator;
 
 public class Calculator {
 
-    public double calculate(double a, double b, char f) {
-        DoubleBinaryOperator result = (f == '+') ? (x, y) -> x + y
-                : (f == '-') ? (x, y) -> x - y
-                : (f == '*') ? (x, y) -> x * y
-                : (f == '/') ? (x, y) -> x / y
-                : (f == 'p') ? (x, y) -> Math.pow(x, y)
+    public double calculate(double a, double b, char operation) {
+        DoubleBinaryOperator result = (operation == '+') ? (x, y) -> x + y
+                : (operation == '-') ? (x, y) -> x - y
+                : (operation == '*') ? (x, y) -> x * y
+                : (operation == '/') ? (x, y) -> x / y
+                : (operation == 'p') ? (x, y) -> Math.pow(x, y)
                 : (x, y) -> {
                     throw new UndefinedOperationException("Use + for addition, - for substraction,"
                     + " * for multiplication, / for divisiion and p for powering");
--- src/test/java/core/basesyntax/CalculatorTest.java
+++ src/test/java/core/basesyntax/CalculatorTest.java
@@ -8,60 +8,90 @@ public class CalculatorTest {
     private Calculator calculator = new Calculator();
     private double a = 2.00;
     private double b = 3.00;
-    private double result = 0.00;
+    private double c = -2.00;
+    private double d = -3.00;
     private static final double DELTA = 0.001;
-    private static final double INFINITY = 1.00 / 0;
 
     @Test
     public void additionIsValid() {
-        result = calculator.calculate(a, b, '+');
-        Assert.assertEquals(5.00, result, DELTA);
+        double actualResult = calculator.calculate(a, b, '+');
+        Assert.assertEquals(5.00, actualResult, DELTA);
+    }
+
+    @Test
+    public void additionWithNegativeValues() {
+        double actualResult = calculator.calculate(c, d, '+');
+        Assert.assertEquals(-5.00, actualResult, DELTA);
     }
 
     @Test
     public void subsractionIsValid() {
-        result = calculator.calculate(a, b, '-');
-        Assert.assertEquals(-1.00, result, DELTA);
+        double actualResult = calculator.calculate(a, b, '-');
+        Assert.assertEquals(-1.00, actualResult, DELTA);
+    }
+
+    @Test
+    public void substractionWithNegativeValues() {
+        double actualResult = calculator.calculate(c, d, '-');
+        Assert.assertEquals(1.00, actualResult, DELTA);
     }
 
     @Test
     public void multiplicationIsValid() {
-        result = calculator.calculate(a, b, '*');
-        Assert.assertEquals(6.00, result, DELTA);
+        double actualResult = calculator.calculate(a, b, '*');
+        Assert.assertEquals(6.00, actualResult, DELTA);
+    }
+
+    @Test
+    public void multiplicationWithNegativeValues() {
+        double actualResult = calculator.calculate(c, d, '*');
+        Assert.assertEquals(6.00, actualResult, DELTA);
     }
 
     @Test
     public void divisionIsValid() {
-        result = calculator.calculate(a, b, '/');
-        Assert.assertEquals(0.666, result, DELTA);
+        double actualResult = calculator.calculate(a, b, '/');
+        Assert.assertEquals(0.666, actualResult, DELTA);
+    }
+
+    @Test
+    public void divisionWithNegativeValues() {
+        double actualResult = calculator.calculate(c, d, '/');
+        Assert.assertEquals(0.666, actualResult, DELTA);
     }
 
     @Test
     public void divisionByZero() {
-        result = calculator.calculate(a, 0, '/');
-        Assert.assertEquals(INFINITY, result, DELTA);
+        double actualResult = calculator.calculate(a, 0, '/');
+        Assert.assertEquals(Double.POSITIVE_INFINITY, actualResult, DELTA);
     }
 
     @Test
     public void powerIsValid() {
-        result = calculator.calculate(a, b, 'p');
-        Assert.assertEquals(8.00, result, DELTA);
+        double actualResult = calculator.calculate(a, b, 'p');
+        Assert.assertEquals(8.00, actualResult, DELTA);
+    }
+
+    @Test
+    public void powerWithNegativeValues() {
+        double actualResult = calculator.calculate(c, d, 'p');
+        Assert.assertEquals(-0.125, actualResult, DELTA);
     }
 
     @Test
     public void powerWithNegativeExponent() {
-        result = calculator.calculate(a, -3, 'p');
-        Assert.assertEquals(0.125, result, DELTA);
+        double actualResult = calculator.calculate(a, d, 'p');
+        Assert.assertEquals(0.125, actualResult, DELTA);
     }
 
     @Test(expected = NullPointerException.class)
     public void usageNull() {
         Double theNull = null;
-        result = calculator.calculate(theNull, b, '+');
+        double actualResult = calculator.calculate(theNull, b, '+');
     }
 
     @Test(expected = UndefinedOperationException.class)
     public void undefinedOperation() {
-        result = calculator.calculate(a, b, 's');
+        double actualResult = calculator.calculate(a, b, 's');
     }
 }
