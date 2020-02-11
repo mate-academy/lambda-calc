a1e0f2861cc1ba4b4004244bdc06a69c03099506
levko <levkosvd@gmail.com>
2020-02-11 13:46:17 +0200

    add DELTA const

--- src/test/java/core/basesyntax/CalculatorTest.java
+++ src/test/java/core/basesyntax/CalculatorTest.java
@@ -7,15 +7,16 @@ import org.junit.Test;
 
 public class CalculatorTest {
     private Calculator calculator = new Calculator();
+    private static final double DELTA = 0.001;
 
     @Test
     public void addition() {
         double expectedResult1 = 7;
         double expectedResult2 = -16;
         double expectedResult3 = 0;
-        Assert.assertEquals(expectedResult1, calculator.calculate('+', 3, 4), 0.001);
-        Assert.assertEquals(expectedResult2, calculator.calculate('+', -16, 0), 0.001);
-        Assert.assertEquals(expectedResult3, calculator.calculate('+', 10, -10), 0.001);
+        Assert.assertEquals(expectedResult1, calculator.calculate('+', 3, 4), DELTA);
+        Assert.assertEquals(expectedResult2, calculator.calculate('+', -16, 0), DELTA);
+        Assert.assertEquals(expectedResult3, calculator.calculate('+', 10, -10), DELTA);
     }
 
     @Test
@@ -23,18 +24,18 @@ public class CalculatorTest {
         double expectedResult1 = 25;
         double expectedResult2 = -11;
         double expectedResult3 = 55;
-        Assert.assertEquals(expectedResult1, calculator.calculate('-', 32, 7), 0.001);
-        Assert.assertEquals(expectedResult2, calculator.calculate('-', 10, 21), 0.001);
-        Assert.assertEquals(expectedResult3, calculator.calculate('-', -75, -130), 0.001);
+        Assert.assertEquals(expectedResult1, calculator.calculate('-', 32, 7), DELTA);
+        Assert.assertEquals(expectedResult2, calculator.calculate('-', 10, 21), DELTA);
+        Assert.assertEquals(expectedResult3, calculator.calculate('-', -75, -130), DELTA);
     }
 
     @Test
     public void multiplication() {
         double expectedResult1 = 56;
         double expectedResult3 = 0;
-        Assert.assertEquals(expectedResult1, calculator.calculate('*', 7, 8), 0.001);
-        Assert.assertEquals(expectedResult1, calculator.calculate('*', 8, 7), 0.001);
-        Assert.assertEquals(expectedResult3, calculator.calculate('*', 0, 565675675), 0.001);
+        Assert.assertEquals(expectedResult1, calculator.calculate('*', 7, 8), DELTA);
+        Assert.assertEquals(expectedResult1, calculator.calculate('*', 8, 7), DELTA);
+        Assert.assertEquals(expectedResult3, calculator.calculate('*', 0, 565675675), DELTA);
     }
 
     @Test
@@ -42,9 +43,9 @@ public class CalculatorTest {
         double expectedResult1 = 3.333;
         double expectedResult2 = 0;
         double expectedResult3 = 1;
-        Assert.assertEquals(expectedResult1, calculator.calculate('/', 10, 3), 0.001);
-        Assert.assertEquals(expectedResult2, calculator.calculate('/', 0, 5), 0.001);
-        Assert.assertEquals(expectedResult3, calculator.calculate('/', 25, 25), 0.001);
+        Assert.assertEquals(expectedResult1, calculator.calculate('/', 10, 3), DELTA);
+        Assert.assertEquals(expectedResult2, calculator.calculate('/', 0, 5), DELTA);
+        Assert.assertEquals(expectedResult3, calculator.calculate('/', 25, 25), DELTA);
     }
 
     @Test
@@ -53,23 +54,23 @@ public class CalculatorTest {
         double expectedResult2 = 0;
         double expectedResult3 = -27;
         double expectedResult4 = 0.037;
-        Assert.assertEquals(expectedResult1, calculator.calculate('^', 10, 3),  0.001);
-        Assert.assertEquals(expectedResult2, calculator.calculate('^', 0, 5), 0.001);
-        Assert.assertEquals(expectedResult3, calculator.calculate('^', -3, 3), 0.001);
-        Assert.assertEquals(expectedResult4, calculator.calculate('^', 3, -3), 0.001);
+        Assert.assertEquals(expectedResult1, calculator.calculate('^', 10, 3),  DELTA);
+        Assert.assertEquals(expectedResult2, calculator.calculate('^', 0, 5), DELTA);
+        Assert.assertEquals(expectedResult3, calculator.calculate('^', -3, 3), DELTA);
+        Assert.assertEquals(expectedResult4, calculator.calculate('^', 3, -3), DELTA);
     }
 
     @Test
     public void powerWithZero() {
         double expectedResult = 1;
-        Assert.assertEquals(expectedResult, calculator.calculate('^', 3, 0), 0.001);
-        Assert.assertEquals(expectedResult, calculator.calculate('^', 0, 0), 0.001);
-        Assert.assertEquals(expectedResult, calculator.calculate('^', -15, 0), 0.001);
+        Assert.assertEquals(expectedResult, calculator.calculate('^', 3, 0), DELTA);
+        Assert.assertEquals(expectedResult, calculator.calculate('^', 0, 0), DELTA);
+        Assert.assertEquals(expectedResult, calculator.calculate('^', -15, 0), DELTA);
     }
 
     @Test
     public void divideByZero() {
-        Assert.assertEquals(Double.NaN, calculator.calculate('/', 0, 0),0.001);
+        Assert.assertEquals(Double.NaN, calculator.calculate('/', 0, 0),DELTA);
     }
 
     @Test(expected = IllegalArgumentException.class)
