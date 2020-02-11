438b2182576578c082ca7317706834a72b0a3047
AlinaPodoliaka <57722396+AlinaPodoliaka@users.noreply.github.com>
2020-02-11 12:22:06 +0200

    added constant for DELTA, changed positive tests name.

--- src/test/java/core/basesyntax/CalculatorTest.java
+++ src/test/java/core/basesyntax/CalculatorTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 
 public class CalculatorTest {
 
-    private static final double INFINITY = 1.0 / 0.0;
+    private static final double DELTA = 0.0;
     private Calculator calculator;
 
     @Before
@@ -15,58 +15,58 @@ public class CalculatorTest {
     }
 
     @Test
-    public void sum() {
+    public void sumPositiveTest() {
         double res = calculator.calculate(10, 5, '+');
         double res1 = calculator.calculate(10.01, -5.01, '+');
         double res2 = calculator.calculate(10000, 5000, '+');
 
-        Assert.assertEquals(15, res, 0.0);
-        Assert.assertEquals(5, res1, 0.0);
-        Assert.assertEquals(15000, res2, 0.0);
+        Assert.assertEquals(15, res, DELTA);
+        Assert.assertEquals(5, res1, DELTA);
+        Assert.assertEquals(15000, res2, DELTA);
     }
 
     @Test
-    public void minus() {
+    public void minusPositiveTest() {
         double res = calculator.calculate(10, 5, '-');
         double res1 = calculator.calculate(10.01, -5, '-');
         double res2 = calculator.calculate(10000, 5000, '-');
 
-        Assert.assertEquals(5, res, 0.0);
-        Assert.assertEquals(15.01, res1, 0.0);
-        Assert.assertEquals(5000, res2, 0.0);
+        Assert.assertEquals(5, res, DELTA);
+        Assert.assertEquals(15.01, res1, DELTA);
+        Assert.assertEquals(5000, res2, DELTA);
     }
 
     @Test
-    public void division() {
+    public void divisionPositiveTest() {
         double res = calculator.calculate(10, 5, '/');
         double res1 = calculator.calculate(10.10, -5, '/');
         double res2 = calculator.calculate(10000, 50, '/');
 
-        Assert.assertEquals(2, res, 0.0);
-        Assert.assertEquals(-2.02, res1, 0.0);
-        Assert.assertEquals(200, res2, 0.0);
+        Assert.assertEquals(2, res, DELTA);
+        Assert.assertEquals(-2.02, res1, DELTA);
+        Assert.assertEquals(200, res2, DELTA);
     }
 
     @Test
-    public void multiplication() {
+    public void multiplicationPositiveTest() {
         double res = calculator.calculate(10, 5, '*');
         double res1 = calculator.calculate(10.10, 5.10, '*');
         double res2 = calculator.calculate(10000, -5, '*');
 
-        Assert.assertEquals(50, res, 0.0);
-        Assert.assertEquals(51.51, res1, 0.0);
-        Assert.assertEquals(-50000, res2, 0.0);
+        Assert.assertEquals(50, res, DELTA);
+        Assert.assertEquals(51.51, res1, DELTA);
+        Assert.assertEquals(-50000, res2, DELTA);
     }
 
     @Test
-    public void exponentiation() {
+    public void exponentiationPositiveTest() {
         double res = calculator.calculate(2, 3, '^');
         double res1 = calculator.calculate(10, -1, '^');
         double res2 = calculator.calculate(15, 0, '^');
 
-        Assert.assertEquals(8, res, 0.0);
-        Assert.assertEquals(0.1, res1, 0.0);
-        Assert.assertEquals(1, res2, 0.0);
+        Assert.assertEquals(8, res, DELTA);
+        Assert.assertEquals(0.1, res1, DELTA);
+        Assert.assertEquals(1, res2, DELTA);
     }
 
     @Test(expected = NullPointerException.class)
@@ -97,19 +97,19 @@ public class CalculatorTest {
     @Test
     public void divisionZero() {
         double res = calculator.calculate(10, 0.0, '/');
-        Assert.assertEquals(INFINITY, res, 0.0);
+        Assert.assertEquals(Double.POSITIVE_INFINITY, res, DELTA);
     }
 
     @Test
     public void multiplicationInfinity() {
-        double res = calculator.calculate(10, INFINITY, '*');
-        Assert.assertEquals(INFINITY, res, 0.0);
+        double res = calculator.calculate(10, Double.POSITIVE_INFINITY, '*');
+        Assert.assertEquals(Double.POSITIVE_INFINITY, res, DELTA);
     }
 
     @Test
     public void devitionInfinity() {
-        double res = calculator.calculate(1, INFINITY, '/');
-        Assert.assertEquals(0.0, res, 0.0);
+        double res = calculator.calculate(1, Double.POSITIVE_INFINITY, '/');
+        Assert.assertEquals(0.0, res, DELTA);
     }
 
     @Test(expected = NullPointerException.class)
