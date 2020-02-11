0d04d31bda92a99cf8eddec32f5d575b54dd937d
Vadim Chaika <proxik777@gmail.com>
2020-02-11 15:37:02 +0200

    added additional tests

--- src/main/java/core/basesyntax/Calculator.java
+++ src/main/java/core/basesyntax/Calculator.java
@@ -3,12 +3,12 @@ package core.basesyntax;
 import java.util.function.DoubleBinaryOperator;
 
 public class Calculator {
-    public double calculate(double a, double b, char c) {
-        DoubleBinaryOperator result = (c == '+') ? (x, y) -> x + y
-                : (c == '-') ? (x, y) -> x - y
-                : (c == '*') ? (x, y) -> x * y
-                : (c == '/') ? (x, y) -> x / y
-                : (c == '^') ? (x, y) -> Math.pow(x, y)
+    public double calculate(double a, double b, char operation) {
+        DoubleBinaryOperator result = (operation == '+') ? (x, y) -> x + y
+                : (operation == '-') ? (x, y) -> x - y
+                : (operation == '*') ? (x, y) -> x * y
+                : (operation == '/') ? (x, y) -> x / y
+                : (operation == '^') ? (x, y) -> Math.pow(x, y)
                 : (x, y) -> {
                     throw new IllegalArgumentException("No such operation. Use +, -, /, *, ^");
                 };
--- src/test/java/core/basesyntax/CalculatorTest.java
+++ src/test/java/core/basesyntax/CalculatorTest.java
@@ -7,6 +7,8 @@ public class CalculatorTest {
     private Calculator calculator = new Calculator();
     private double a = 2.00;
     private double b = 4.00;
+    private double c = -6.00;
+    private double d = -7.00;
     private static final double DELTA = 0.001;
 
     @Test
@@ -16,15 +18,33 @@ public class CalculatorTest {
     }
 
     @Test
+    public void additionWithNegative() {
+        double res = calculator.calculate(c, d, '+');
+        Assert.assertEquals(-13.00, res, DELTA);
+    }
+
+    @Test
     public void subtractionIsValid() {
         double res = calculator.calculate(a, b, '-');
         Assert.assertEquals(-2.00, res, DELTA);
     }
 
     @Test
+    public void subtractionWithNegative() {
+        double res = calculator.calculate(c, d, '-');
+        Assert.assertEquals(1.00, res, DELTA);
+    }
+
+    @Test
     public void multiplicationIsValid() {
         double res = calculator.calculate(a, b, '*');
         Assert.assertEquals(8.00, res, DELTA);
+    }
+
+    @Test
+    public void multiplicationWithNegative() {
+        double res = calculator.calculate(c, d, '*');
+        Assert.assertEquals(42.00, res, DELTA);
     }
 
     @Test
@@ -49,6 +69,12 @@ public class CalculatorTest {
     public void powerWithNegativeExponent() {
         double res = calculator.calculate(a,-3,'^');
         Assert.assertEquals(0.125, res, DELTA);
+    }
+
+    @Test
+    public void powerWithZero() {
+        double res = calculator.calculate(a,0,'^');
+        Assert.assertEquals(1, res, DELTA);
     }
 
     @Test(expected = NullPointerException.class)
