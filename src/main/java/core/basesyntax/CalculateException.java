12571a89d7d695ccb3a129b96d01019667c1564f
Максим Матуляк <maksymmatuliak@gmail.com>
2020-05-28 14:15:35 +0300

    changed calculator

--- src/main/java/core/basesyntax/CalculateException.java
+++ src/main/java/core/basesyntax/CalculateException.java
@@ -1,6 +1,6 @@
 package core.basesyntax;
 
-public class CalculateException extends RuntimeException{
+public class CalculateException extends RuntimeException {
     public CalculateException(String message) {
         super(message);
     }
--- src/main/java/core/basesyntax/Calculator.java
+++ src/main/java/core/basesyntax/Calculator.java
@@ -2,19 +2,16 @@ package core.basesyntax;
 
 public class Calculator {
     public static Integer calculate(int num1, int num2, char sign) {
-        switch (sign) {
-            case '+':
-                return num1 + num2;
-            case '-':
-                return num1 - num2;
-            case '*':
-                return num1 * num2;
-            case '/':
-                if (num2 != 0) {
-                    return num1 / num2;
-                }
-            default:
-                throw new CalculateException("Sorry, something is wrong");
+        if (sign == '-') {
+            return num1 - num2;
+        } else if (sign == '+') {
+            return num1 + num2;
+        } else if (sign == '*') {
+            return num1 * num2;
+        } else if (sign == '/' && num2 != 0) {
+            return num1 / num2;
+        } else {
+            throw new CalculateException("Sorry, something is wrong!");
         }
     }
 }
