58db6ede9971b6e0c0e409b62d95b076352ef98c
ana-dav <opa.super.op@gmail.com>
2020-06-01 19:16:47 +0300

    calc

---
+++ src/main/java/core/basesyntax/Calculator.java
@@ -0,0 +1,6 @@
+package core.basesyntax;
+
+@FunctionalInterface
+public interface Calculator {
+    Double calculate( Double operand1, Double operand2);
+}
---
+++ src/main/java/core/basesyntax/CalculatorImpl.java
@@ -0,0 +1,32 @@
+package core.basesyntax;
+
+public class CalculatorImpl {
+    public Double addition(Double operand1, Double operand2) {
+        Calculator calc = (op1, op2) -> operand1 + operand2;
+        return calculate(operand1, operand2, calc);
+    }
+
+    public Double removal(Double operand1, Double operand2) {
+        Calculator calc = (op1, op2) -> operand1 - operand2;
+        return calculate(operand1, operand2, calc);
+    }
+
+    public Double division(Double operand1, Double operand2) {
+        Calculator calc = (op1, op2) -> operand1 / operand2;
+        return calculate(operand1, operand2, calc);
+    }
+
+    public Double multiplication(Double operand1, Double operand2) {
+        Calculator calc = (op1, op2) -> operand1 * operand2;
+        return calculate(operand1, operand2, calc);
+    }
+
+    public Double pow(Double operand1, Double operand2) {
+        Calculator calc = (op1, op2) -> Math.pow(operand1, operand2);
+        return calculate(operand1, operand2, calc);
+    }
+
+    private double calculate (Double operand1, Double operand2, Calculator calculator) {
+        return calculator.calculate(operand1, operand2);
+    }
+}
