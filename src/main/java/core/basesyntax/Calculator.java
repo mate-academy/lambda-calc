273b06cf3d975b108896c9abe9f8fd071f7ac41f
Andrii Borozdykh <aborozdykh@gmail.com>
2020-06-01 22:26:37 +0300

    Added solution

---
+++ src/main/java/core/basesyntax/Calculator.java
@@ -0,0 +1,27 @@
+package core.basesyntax;
+
+/**
+ * Feel free to remove this class and create your own.
+ */
+public class Calculator {
+    public static Integer startCalc(int a, int b, char sign) {
+        switch (sign) {
+            case ('-'):
+                return a - b;
+            case ('+'):
+                return a + b;
+            case ('*'):
+                return a * b;
+            case ('/'):
+                if (b != 0) {
+                    return a / b;
+                } else {
+                    throw new IllegalArgumentException("Error!");
+                }
+            case ('^'):
+                return (int) Math.pow(a, b);
+            default:
+                throw new IllegalArgumentException("Error!");
+        }
+    }
+}
---
+++ src/test/java/core/basesyntax/CalculatorTest.java
@@ -0,0 +1,52 @@
+package core.basesyntax;
+
+import org.junit.Assert;
+import org.junit.Test;
+/**
+ * Feel free to remove this class and create your own.
+ */
+public class CalculatorTest {
+    @Test
+    public void addNumbers() {
+        Assert.assertEquals(Integer.valueOf(9), Calculator.startCalc(2, 7, '+'));
+    }
+
+    @Test
+    public void addNegativeNumbers() throws IllegalArgumentException {
+        Assert.assertEquals(Integer.valueOf(-10), Calculator.startCalc(-3, -7, '+'));
+    }
+
+    @Test
+    public void subtractNumbers() {
+        Assert.assertEquals(Integer.valueOf(3), Calculator.startCalc(5, 2, '-'));
+    }
+    @Test
+    public void subtractNegativeNumbers() {
+        Assert.assertEquals(Integer.valueOf(-3), Calculator.startCalc(-5, -2, '-'));
+    }
+
+    @Test
+    public void multiplySimpleNumbers() {
+        Assert.assertEquals(Integer.valueOf(4), Calculator.startCalc(2, 2, '*'));
+    }
+
+    @Test
+    public void divideSimpleNumbers() {
+        Assert.assertEquals(Integer.valueOf(2), Calculator.startCalc(4, 2, '/'));
+    }
+
+    @Test
+    public void levelOfNumber() {
+        Assert.assertEquals(Integer.valueOf(4), Calculator.startCalc(2, 2, '^'));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void divideByZero() {
+        Calculator.startCalc(5, 0, '/');
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void setNotValidSign() {
+        Calculator.startCalc(5, 3, 's');
+    }
+}
