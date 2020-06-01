d84527d80f232d13d76bb7d38766235eebef5770
vladpetrovskyi <v.petrovskyi98@gmail.com>
2020-06-01 14:59:07 +0300

    updated solution

--- src/test/java/core/basesyntax/CalculatorTest.java
+++ src/test/java/core/basesyntax/CalculatorTest.java
@@ -5,42 +5,47 @@ import org.junit.Test;
 
 public class CalculatorTest {
     @Test
-    public void sum() {
+    public void addition() {
         Assert.assertEquals(3, Calculator.calculate(1, 2, '+'));
+        Assert.assertEquals(-3, Calculator.calculate(-1, -2, '+'));
+        Assert.assertEquals(1, Calculator.calculate(-1, 2, '+'));
     }
 
     @Test
-    public void substract() {
+    public void subtraction() {
         Assert.assertEquals(1, Calculator.calculate(3, 2, '-'));
+        Assert.assertEquals(-1, Calculator.calculate(-3, -2, '-'));
+        Assert.assertEquals(-5, Calculator.calculate(-3, 2, '-'));
     }
 
     @Test
-    public void multiply() {
+    public void multiplication() {
         Assert.assertEquals(6, Calculator.calculate(3, 2, '*'));
+        Assert.assertEquals(6, Calculator.calculate(-3, -2, '*'));
+        Assert.assertEquals(-6, Calculator.calculate(-3, 2, '*'));
     }
 
     @Test
-    public void divide() {
+    public void division() {
         Assert.assertEquals(2, Calculator.calculate(6, 3, '/'));
+        Assert.assertEquals(2, Calculator.calculate(-6, -3, '/'));
+        Assert.assertEquals(-2, Calculator.calculate(-6, 3, '/'));
     }
 
     @Test
-    public void divisionByZero() {
-        try {
-            Calculator.calculate(3, 0, '/');
-        } catch (ArithmeticException e) {
-            return;
-        }
-        Assert.fail("ArithmeticException expected!");
+    public void exponentiation() {
+        Assert.assertEquals(4, Calculator.calculate(2, 2, '^'));
+        Assert.assertEquals(4, Calculator.calculate(-2, 2, '^'));
+        Assert.assertEquals(0, Calculator.calculate(2, -2, '^'));
     }
 
-    @Test
-    public void unavailableOperation() {
-        try {
-            Calculator.calculate(3, 2, '%');
-        } catch (IllegalArgumentException e) {
-            return;
-        }
-        Assert.fail("IllegalArgumentException expected!");
+    @Test(expected = ArithmeticException.class)
+    public void divisionByZero() throws ArithmeticException {
+        Calculator.calculate(3, 0, '/');
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void unavailableOperation() throws IllegalArgumentException {
+        Calculator.calculate(3, 2, '%');
     }
 }
