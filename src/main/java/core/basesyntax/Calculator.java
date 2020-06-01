33f7b7e2d1cec4aa480366511749f1687eed0f93
Богдан Демьянчук <t0974447144@gmail.com>
2020-06-01 23:30:58 +0300

    style

--- src/main/java/core/basesyntax/Calculator.java
+++ src/main/java/core/basesyntax/Calculator.java
@@ -4,19 +4,19 @@ import java.util.function.DoubleBinaryOperator;
 
 public class Calculator {
 
-    public static double calculate(double firstArgument, double secondArgument, Character operation) {
+    public static double calculate(double firstArgument,
+                                   double secondArgument, Character operation) {
         if (!operation.toString().matches("[-+*/^]")) {
             throw new ArithmeticException("Wrong operation");
-        } else if(operation == '/' && secondArgument == 0){
+        } else if (operation == '/' && secondArgument == 0) {
             throw new ArithmeticException("Division by zero");
         }
         DoubleBinaryOperator binaryOperator =
-                  operation == '+' ? Double::sum
-                : operation == '-' ? (first, second) -> first - second
-                : operation == '*' ? (first, second) -> first * second
-                : operation == '/' ? (first, second) -> first / second
-                : Math::pow;
+                operation == '+' ? Double::sum
+                        : operation == '-' ? (first, second) -> first - second
+                        : operation == '*' ? (first, second) -> first * second
+                        : operation == '/' ? (first, second) -> first / second
+                        : Math::pow;
         return binaryOperator.applyAsDouble(firstArgument, secondArgument);
     }
-
 }
