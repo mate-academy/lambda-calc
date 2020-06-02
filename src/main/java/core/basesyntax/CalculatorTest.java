a841123f38a217991b11d5dd86a7d45d241d338e
Andrew <tolstuy1996@gmail.com>
2020-06-02 16:47:40 +0300

    fixed CS

--- src/main/java/core/basesyntax/CalculatorTest.java
+++ src/main/java/core/basesyntax/CalculatorTest.java
@@ -34,7 +34,7 @@ public class CalculatorTest {
     @Test
     public void checkMultiplication() {
         Assert.assertEquals(4, Calculator.calculate(-2, -2, '*'), DELTA);
-        Assert.assertEquals(Double.MAX_VALUE, Calculator.calculate(Double.MAX_VALUE, 1,'*'),  DELTA);
+        Assert.assertEquals(Double.MAX_VALUE, Calculator.calculate(Double.MAX_VALUE, 1,'*'), DELTA);
 
     }
 
@@ -42,20 +42,24 @@ public class CalculatorTest {
     public void checkAddition() {
         Assert.assertEquals(0, Calculator.calculate(-2, 2, '+'), DELTA);
         Assert.assertEquals(-7, Calculator.calculate(0, -7, '+'), DELTA);
-        Assert.assertEquals(Double.MAX_VALUE, Calculator.calculate(0, Double.MAX_VALUE, '+'), DELTA);
+        Assert.assertEquals(Double.MAX_VALUE,
+                Calculator.calculate(0, Double.MAX_VALUE, '+'), DELTA);
 
     }
 
     @Test
     public void checkSubtraction() {
-        Assert.assertEquals(0, Calculator.calculate(Double.MAX_VALUE, Double.MAX_VALUE, '-'), DELTA);
-        Assert.assertEquals( Double.MAX_VALUE, Calculator.calculate(Double.MAX_VALUE, 0, '-'), DELTA);
+        Assert.assertEquals(0,
+                Calculator.calculate(Double.MAX_VALUE, Double.MAX_VALUE, '-'), DELTA);
+        Assert.assertEquals(Double.MAX_VALUE,
+                Calculator.calculate(Double.MAX_VALUE, 0, '-'), DELTA);
     }
 
     @Test
     public void checkExponentiation() {
         Assert.assertEquals(1, Calculator.calculate(3, 0, '^'), DELTA);
-        Assert.assertEquals(Double.MAX_VALUE, Calculator.calculate(Double.MAX_VALUE, 1, '^'), DELTA);
+        Assert.assertEquals(Double.MAX_VALUE,
+                Calculator.calculate(Double.MAX_VALUE, 1, '^'), DELTA);
         Assert.assertEquals(74.088, Calculator.calculate(4.2, 3, '^'), DELTA);
     }
 }
