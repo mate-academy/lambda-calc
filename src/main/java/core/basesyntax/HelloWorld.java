9868257c4c1611a17cf738f77fe943a44582fbfb
Laszlo Petechuk <frostpv@gmail.com>
2020-08-18 15:07:10 +0300

    my solution

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -4,5 +4,47 @@ package core.basesyntax;
  * Feel free to remove this class and create your own.
  */
 public class HelloWorld {
+    public double calculate(double firstDigit, double secondDigit, char operand) {
+        switch (operand) {
+            case ('+'):
+                return addition(firstDigit, secondDigit);
+            case ('-'):
+                return subtraction(firstDigit, secondDigit);
+            case ('/'):
+                return division(firstDigit, secondDigit);
+            case ('*'):
+                return multiplication(firstDigit, secondDigit);
+            case ('^'):
+                return raisingToPower(firstDigit, secondDigit);
+            default:
+                throw new IllegalArgumentException();
+        }
+    }
 
+    private double addition(double firstDigit, double secondDigit) {
+        return firstDigit + secondDigit;
+    }
+
+    private double subtraction(double firstDigit, double secondDigit) {
+        return firstDigit - secondDigit;
+    }
+
+    private double division(double firstDigit, double secondDigit) {
+        if (secondDigit == 0) {
+            throw new ArithmeticException();
+        }
+        return firstDigit / secondDigit;
+    }
+
+    private double multiplication(double firstDigit, double secondDigit) {
+        return firstDigit * secondDigit;
+    }
+
+    private double raisingToPower(double firstDigit, double n) {
+        double result = 1;
+        for (double i = 1; i <= n; i++) {
+            result = result * firstDigit;
+        }
+        return result;
+    }
 }
--- src/test/java/core/basesyntax/HelloWorldTest.java
+++ src/test/java/core/basesyntax/HelloWorldTest.java
@@ -1,8 +1,77 @@
 package core.basesyntax;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * Feel free to remove this class and create your own.
  */
 public class HelloWorldTest {
+    private HelloWorld helloWorld;
+    private static final double DELTA = 0.01D;
+
+    @Test
+    public void additionWithNehativeAndPositive() {
+        helloWorld = new HelloWorld();
+        Assert.assertEquals(5, helloWorld.calculate(2.00, 3.00, '+'), DELTA);
+        Assert.assertEquals(-5, helloWorld.calculate(-15, 10, '+'), DELTA);
+        Assert.assertEquals(-1000, helloWorld.calculate(-999, -1, '+'), DELTA);
+    }
+
+    @Test
+    public void subtractionWithNehativeAndPositive() {
+        helloWorld = new HelloWorld();
+        Assert.assertEquals(1, helloWorld.calculate(3, 2, '-'), DELTA);
+        Assert.assertEquals(-25, helloWorld.calculate(-15, 10, '-'), DELTA);
+        Assert.assertEquals(-998, helloWorld.calculate(-999, -1, '-'), DELTA);
+    }
+
+    @Test
+    public void divisionWithNehativeAndPositive() {
+        helloWorld = new HelloWorld();
+        Assert.assertEquals(1.5, helloWorld.calculate(3, 2, '/'), DELTA);
+        Assert.assertEquals(-1.5, helloWorld.calculate(-15, 10, '/'), DELTA);
+        Assert.assertEquals(999, helloWorld.calculate(-999, -1, '/'), DELTA);
+    }
+
+    @Test
+    public void divisionByZero() {
+        helloWorld = new HelloWorld();
+        try {
+            helloWorld.calculate(3, 0, '/');
+        } catch (ArithmeticException e) {
+            return;
+        }
+        Assert.fail("ArithmeticException expected");
+    }
+
+
+    @Test
+    public void multiplicationWithNehativeAndPositive() {
+        helloWorld = new HelloWorld();
+        Assert.assertEquals(6, helloWorld.calculate(3, 2, '*'), DELTA);
+        Assert.assertEquals(-150, helloWorld.calculate(-15, 10, '*'), DELTA);
+        Assert.assertEquals(999, helloWorld.calculate(-999, -1, '*'), DELTA);
+    }
+
+    @Test
+    public void raisingToPowerWithNehativeAndPositive() {
+        helloWorld = new HelloWorld();
+        Assert.assertEquals(81, helloWorld.calculate(9, 2, '^'), DELTA);
+        Assert.assertEquals(-3125, helloWorld.calculate(-5, 5, '^'), DELTA);
+        Assert.assertEquals(1, helloWorld.calculate(-2, -1, '^'), DELTA);
+    }
+
+    @Test
+    public void illegalArgumenUse(){
+        helloWorld = new HelloWorld();
+        try {
+            helloWorld.calculate(3, 0, '$');
+        } catch (IllegalArgumentException e) {
+            return;
+        }
+        Assert.fail("IllegalArgumentException expected");
+    }
+
 
 }
