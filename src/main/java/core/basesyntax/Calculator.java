707645f8226d0ae6ec30dda2b7385e7413e344fd
ROMASHSX <andriiromash@gmail.com>
2020-08-18 13:16:53 +0300

    tests for subtraction and addition are made

--- src/main/java/core/basesyntax/Calculator.java
+++ src/main/java/core/basesyntax/Calculator.java
@@ -1,4 +1,10 @@
 package core.basesyntax;
 
 public class Calculator {
+
+    public double calculate(double value1, double value2, char operation) {
+        double result = 0.00;
+
+        return result;
+    }
 }
--- src/test/java/core/basesyntax/CalculatorTest.java
+++ src/test/java/core/basesyntax/CalculatorTest.java
@@ -1,7 +1,82 @@
 package core.basesyntax;
 
-import static org.junit.Assert.*;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
 public class CalculatorTest {
+    private static final double DELTA = 0D;
+    private Calculator calculator;
+    private double result;
 
+    @Before
+    public void setUp() throws Exception {
+        calculator = new Calculator();
+    }
+
+    @Test
+    public void normalAdditionTest() {
+        result = calculator.calculate(5.2, 5.8, '+');
+        Assert.assertEquals(11, result, DELTA);
+    }
+
+    @Test
+    public void negativeAdditionTest() {
+        result = calculator.calculate(-5.2, -5.8, '+');
+        Assert.assertEquals(-11, result, DELTA);
+    }
+
+    @Test
+    public void negativeAndPositiveAdditionTest() {
+        result = calculator.calculate(-5.2, 5.8, '+');
+        Assert.assertEquals(0.6, result, DELTA);
+    }
+
+    @Test
+    public void additionWithOneZero() {
+        result = calculator.calculate(0, 5.8, '+');
+        Assert.assertEquals(5.8, result, DELTA);
+    }
+
+    @Test
+    public void additionWithBothZero() {
+        result = calculator.calculate(0, 0, '+');
+        Assert.assertEquals(0, result, DELTA);
+    }
+
+    @Test
+    public void normalSubtraction() {
+        result = calculator.calculate(10, 6.2, '-');
+        Assert.assertEquals(3.8, result, DELTA);
+    }
+
+    @Test
+    public void subtractionWithFirstNegative() {
+        result = calculator.calculate(-10, 6.2, '-');
+        Assert.assertEquals(-16.2, result, DELTA);
+    }
+
+    @Test
+    public void subtractionWithSecondNegative() {
+        result = calculator.calculate(10, -6.2, '-');
+        Assert.assertEquals(16.2, result, DELTA);
+    }
+
+    @Test
+    public void subtractionWithFirstZero() {
+        result = calculator.calculate(0, 6.2, '-');
+        Assert.assertEquals(-6.2, result, DELTA);
+    }
+
+    @Test
+    public void subtractionWithSecondZero() {
+        result = calculator.calculate(10, 0, '-');
+        Assert.assertEquals(10, result, DELTA);
+    }
+
+    @Test
+    public void subtractionWithBothZero() {
+        result = calculator.calculate(0, 0, '-');
+        Assert.assertEquals(0, result, DELTA);
+    }
 }
