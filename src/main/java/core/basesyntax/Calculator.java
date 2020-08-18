7cdd4ce2439e75e08fdcc40af6e6ad10d57f3757
Kostian <pyteec.ua@i.ua>
2020-08-18 18:06:29 +0300

    added solution

---
+++ src/main/java/core/basesyntax/Calculator.java
@@ -0,0 +1,26 @@
+package core.basesyntax;
+
+/**
+ * Feel free to remove this class and create your own.
+ */
+public class Calculator {
+    public double calculate(char operation, double fistDigit, double secondDigit) {
+        switch (operation) {
+            case '+':
+                return fistDigit + secondDigit;
+            case '-':
+                return fistDigit - secondDigit;
+            case '*':
+                return fistDigit * secondDigit;
+            case '/':
+                if (secondDigit == 0) {
+                    throw new ArithmeticException("Cannot be divided by zero");
+                }
+                return fistDigit / secondDigit;
+            case '^':
+                return Math.pow(fistDigit, secondDigit);
+            default:
+                throw new IllegalArgumentException("Enter correct operation");
+        }
+    }
+}
