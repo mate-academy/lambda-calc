b52b6ec6abd79a96081c3ed6d66b9e6d26fc9bde
Fedia <fedia.rudiachenko@gmail.com>
2020-08-18 19:08:43 +0300

    added solution with 36 tests (100% lines covered);

---
+++ src/main/java/core/basesyntax/Calculator.java
@@ -0,0 +1,50 @@
+package core.basesyntax;
+
+public class Calculator {
+    private static final char ADDITION = '+';
+    private static final char SUBTRACTION = '-';
+    private static final char DIVISION = '/';
+    private static final char MULTIPLICATION = '*';
+    private static final char POWER = '^';
+
+    public static double calculator(double value1, double value2, char operation) {
+        switch (operation) {
+            case ADDITION:
+                return addition(value1, value2);
+            case SUBTRACTION:
+                return subtraction(value1, value2);
+            case DIVISION:
+                return division(value1, value2);
+            case MULTIPLICATION:
+                return multiplication(value1, value2);
+            case POWER:
+                return power(value1, value2);
+            default:
+                throw new IllegalArgumentException("Operation with "
+                        + operation + "is not supported");
+        }
+    }
+
+    private static double addition(double value1, double value2) {
+        return value1 + value2;
+    }
+
+    private static double subtraction(double value1, double value2) {
+        return value1 - value2;
+    }
+
+    private static double division(double value1, double value2) {
+        if (value2 == 0) {
+            throw new ArithmeticException("/ by zero");
+        }
+        return value1 / value2;
+    }
+
+    private static double multiplication(double value1, double value2) {
+        return value1 * value2;
+    }
+
+    private static double power(double value1, double value2) {
+        return Math.pow(value1, value2);
+    }
+}
---
+++ src/test/java/core/basesyntax/CalculatorTest.java
@@ -0,0 +1,246 @@
+package core.basesyntax;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static core.basesyntax.Calculator.calculator;
+
+public class CalculatorTest {
+    private static final double DELTA = 1e-10;
+    private static final char ADDITION = '+';
+    private static final char SUBTRACTION = '-';
+    private static final char DIVISION = '/';
+    private static final char MULTIPLICATION = '*';
+    private static final char POWER = '^';
+    private static final double POSITIVE_VALUE1 = 4;
+    private static final double POSITIVE_VALUE2 = 2;
+    private static final double NEGATIVE_VALUE1 = -2;
+    private static final double NEGATIVE_VALUE2 = -4;
+    private static final double POSITIVE_FRACTION1 = 2.5;
+    private static final double POSITIVE_FRACTION2 = 1.5;
+    private static final double NEGATIVE_FRACTION1 = -2.5;
+    private static final double NEGATIVE_FRACTION2 = -1.5;
+    private static final double ZERO = 0;
+    private static double result;
+
+
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+        Calculator calculator = new Calculator();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void illegalOperation() {
+        calculator(POSITIVE_VALUE1, POSITIVE_VALUE2, '!');
+    }
+
+    @Test
+    public void positiveAddition() {
+        result = calculator(POSITIVE_VALUE1, POSITIVE_VALUE2, ADDITION);
+        Assert.assertEquals(6, result, DELTA);
+    }
+
+    @Test
+    public void negativeAddition() {
+        result = calculator(NEGATIVE_VALUE1, NEGATIVE_VALUE2, ADDITION);
+        Assert.assertEquals(-6, result, DELTA);
+    }
+
+    @Test
+    public void oneNegativeAddition() {
+        result = calculator(NEGATIVE_VALUE1, POSITIVE_VALUE2, ADDITION);
+        Assert.assertEquals(0, result, DELTA);
+    }
+
+    @Test
+    public void positiveFractionAddition() {
+        result = calculator(POSITIVE_FRACTION1, POSITIVE_FRACTION2, ADDITION);
+        Assert.assertEquals(4, result, DELTA);
+    }
+
+    @Test
+    public void negativeFractionAddition() {
+        result = calculator(NEGATIVE_FRACTION1, NEGATIVE_FRACTION2, ADDITION);
+        Assert.assertEquals(-4, result, DELTA);
+    }
+
+    @Test
+    public void oneNegativeFractionAddition() {
+        result = calculator(NEGATIVE_FRACTION1, POSITIVE_FRACTION2, ADDITION);
+        Assert.assertEquals(-1, result, DELTA);
+    }
+
+    @Test
+    public void positiveSubtraction() {
+        result = calculator(POSITIVE_VALUE1, POSITIVE_VALUE2, SUBTRACTION);
+        Assert.assertEquals(2, result, DELTA);
+    }
+
+    @Test
+    public void negativeSubtraction() {
+        result = calculator(NEGATIVE_VALUE1, NEGATIVE_VALUE2, SUBTRACTION);
+        Assert.assertEquals(2, result, DELTA);
+    }
+
+    @Test
+    public void oneNegativeSubtraction() {
+        result = calculator(NEGATIVE_VALUE1, POSITIVE_VALUE2, SUBTRACTION);
+        Assert.assertEquals(-4, result, DELTA);
+    }
+
+    @Test
+    public void positiveFractionSubtraction() {
+        result = calculator(POSITIVE_FRACTION1, POSITIVE_FRACTION2, SUBTRACTION);
+        Assert.assertEquals(1, result, DELTA);
+    }
+
+    @Test
+    public void negativeFractionSubtraction() {
+        result = calculator(NEGATIVE_FRACTION1, NEGATIVE_FRACTION2, SUBTRACTION);
+        Assert.assertEquals(-1, result, DELTA);
+    }
+
+    @Test
+    public void oneNegativeFractionSubtraction() {
+        result = calculator(NEGATIVE_FRACTION1, POSITIVE_FRACTION2, SUBTRACTION);
+        Assert.assertEquals(-4, result, DELTA);
+    }
+
+    @Test
+    public void positiveDivision() {
+        result = calculator(POSITIVE_VALUE1, POSITIVE_VALUE2, DIVISION);
+        Assert.assertEquals(2, result, DELTA);
+    }
+
+    @Test
+    public void negativeDivision() {
+        result = calculator(NEGATIVE_VALUE1, NEGATIVE_VALUE2, DIVISION);
+        Assert.assertEquals(0.5, result, DELTA);
+    }
+
+    @Test
+    public void oneNegativeDivision() {
+        result = calculator(NEGATIVE_VALUE1, POSITIVE_VALUE2, DIVISION);
+        Assert.assertEquals(-1, result, DELTA);
+    }
+
+    @Test
+    public void positiveFractionDivision() {
+        result = calculator(POSITIVE_FRACTION1, POSITIVE_VALUE2, DIVISION);
+        Assert.assertEquals(1.25, result, DELTA);
+    }
+
+    @Test
+    public void negativeFractionDivision() {
+        result = calculator(NEGATIVE_FRACTION1, NEGATIVE_VALUE2, DIVISION);
+        Assert.assertEquals(0.625, result, DELTA);
+    }
+
+    @Test
+    public void oneNegativeFractionDivision() {
+        result = calculator(NEGATIVE_FRACTION1, POSITIVE_VALUE2, DIVISION);
+        Assert.assertEquals(-1.25, result, DELTA);
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void arithmeticException() {
+        calculator(POSITIVE_VALUE1, ZERO, DIVISION);
+    }
+
+    @Test
+    public void zeroDevided() {
+        result = calculator(ZERO, POSITIVE_VALUE2, DIVISION);
+        Assert.assertEquals(0, result, DELTA);
+    }
+
+    @Test
+    public void positiveMultiplication() {
+        result = calculator(POSITIVE_VALUE1, POSITIVE_VALUE2, MULTIPLICATION);
+        Assert.assertEquals(8, result, DELTA);
+    }
+
+    @Test
+    public void negativeMultiplication() {
+        result = calculator(NEGATIVE_VALUE1, NEGATIVE_VALUE2, MULTIPLICATION);
+        Assert.assertEquals(8, result, DELTA);
+    }
+
+    @Test
+    public void oneNegativeMultiplication() {
+        result = calculator(NEGATIVE_VALUE1, POSITIVE_VALUE2, MULTIPLICATION);
+        Assert.assertEquals(-4, result, DELTA);
+    }
+
+    @Test
+    public void positiveFractionMultiplication() {
+        result = calculator(POSITIVE_FRACTION1, POSITIVE_VALUE2, MULTIPLICATION);
+        Assert.assertEquals(5, result, DELTA);
+    }
+
+    @Test
+    public void negativeFractionMultiplication() {
+        result = calculator(NEGATIVE_FRACTION1, NEGATIVE_VALUE2, MULTIPLICATION);
+        Assert.assertEquals(10, result, DELTA);
+    }
+
+    @Test
+    public void oneNegativeFractionMultiplication() {
+        result = calculator(NEGATIVE_FRACTION1, POSITIVE_VALUE2, MULTIPLICATION);
+        Assert.assertEquals(-5, result, DELTA);
+    }
+
+    @Test
+    public void zeroMultiplication() {
+        result = calculator(ZERO, POSITIVE_VALUE2, MULTIPLICATION);
+        Assert.assertEquals(0, result, DELTA);
+    }
+
+    @Test
+    public void positivePower() {
+        result = calculator(POSITIVE_VALUE1, POSITIVE_VALUE2, POWER);
+        Assert.assertEquals(16, result, DELTA);
+    }
+
+    @Test
+    public void negativePower() {
+        result = calculator(NEGATIVE_VALUE1, NEGATIVE_VALUE2, POWER);
+        Assert.assertEquals(0.0625, result, DELTA);
+    }
+
+    @Test
+    public void oneNegativePower() {
+        result = calculator(NEGATIVE_VALUE1, POSITIVE_VALUE2, POWER);
+        Assert.assertEquals(4, result, DELTA);
+    }
+
+    @Test
+    public void positiveFractionPower() {
+        result = calculator(POSITIVE_FRACTION1, POSITIVE_VALUE2, POWER);
+        Assert.assertEquals(6.25, result, DELTA);
+    }
+
+    @Test
+    public void negativeFractionPower() {
+        result = calculator(NEGATIVE_FRACTION1, NEGATIVE_VALUE2, POWER);
+        Assert.assertEquals(0.0256, result, DELTA);
+    }
+
+    @Test
+    public void oneNegativeFractionPower() {
+        result = calculator(NEGATIVE_FRACTION1, POSITIVE_VALUE2, POWER);
+        Assert.assertEquals(6.25, result, DELTA);
+    }
+
+    @Test
+    public void zeroPower() {
+        result = calculator(ZERO, POSITIVE_VALUE2, POWER);
+        Assert.assertEquals(0, result, DELTA);
+    }
+
+    @Test
+    public void powByZero() {
+        result = calculator(POSITIVE_VALUE1, ZERO, POWER);
+        Assert.assertEquals(1, result, DELTA);
+    }
+}
