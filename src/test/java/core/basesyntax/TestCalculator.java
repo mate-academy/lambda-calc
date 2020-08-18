f766a258b0a59d5fa9d32aa01607b6a642f0006f
dariiapikul <dariyapikul@gmail.com>
2020-08-18 21:33:07 +0300

    Tests order changed

--- src/test/java/core/basesyntax/TestCalculator.java
+++ src/test/java/core/basesyntax/TestCalculator.java
@@ -33,10 +33,10 @@ public class TestCalculator {
     public void additionTestOK() {
         result = calculator.calculate(POSITIVE_OPERAND, POSITIVE_OPERAND, PLUS);
         Assert.assertEquals(22, result, DELTA);
-        result = calculator.calculate(POSITIVE_OPERAND, NEGATIVE_OPERAND, PLUS);
-        Assert.assertEquals(9, result, DELTA);
         result = calculator.calculate(NEGATIVE_OPERAND, NEGATIVE_OPERAND, PLUS);
         Assert.assertEquals(-4, result, DELTA);
+        result = calculator.calculate(POSITIVE_OPERAND, NEGATIVE_OPERAND, PLUS);
+        Assert.assertEquals(9, result, DELTA);
         result = calculator.calculate(ZERO, POSITIVE_OPERAND, PLUS);
         Assert.assertEquals(POSITIVE_OPERAND, result, DELTA);
         result = calculator.calculate(ZERO, NEGATIVE_OPERAND, PLUS);
@@ -49,10 +49,10 @@ public class TestCalculator {
     public void subtractionTest() {
         result = calculator.calculate(POSITIVE_OPERAND, POSITIVE_OPERAND, MINUS);
         Assert.assertEquals(ZERO, result, DELTA);
-        result = calculator.calculate(POSITIVE_OPERAND, NEGATIVE_OPERAND, MINUS);
-        Assert.assertEquals(13, result, DELTA);
         result = calculator.calculate(NEGATIVE_OPERAND, NEGATIVE_OPERAND, MINUS);
         Assert.assertEquals(ZERO, result, DELTA);
+        result = calculator.calculate(POSITIVE_OPERAND, NEGATIVE_OPERAND, MINUS);
+        Assert.assertEquals(13, result, DELTA);
         result = calculator.calculate(NEGATIVE_OPERAND, POSITIVE_OPERAND, MINUS);
         Assert.assertEquals(-13, result, DELTA);
         result = calculator.calculate(ZERO, POSITIVE_OPERAND, MINUS);
@@ -66,12 +66,12 @@ public class TestCalculator {
 
     @Test
     public void multiplicationTest() {
-        result = calculator.calculate(POSITIVE_OPERAND, NEGATIVE_OPERAND, MULTIPLICATION);
-        Assert.assertEquals(-22, result, DELTA);
         result = calculator.calculate(POSITIVE_OPERAND, POSITIVE_OPERAND, MULTIPLICATION);
         Assert.assertEquals(121, result, DELTA);
         result = calculator.calculate(NEGATIVE_OPERAND, NEGATIVE_OPERAND, MULTIPLICATION);
         Assert.assertEquals(4, result, DELTA);
+        result = calculator.calculate(POSITIVE_OPERAND, NEGATIVE_OPERAND, MULTIPLICATION);
+        Assert.assertEquals(-22, result, DELTA);
         result = calculator.calculate(POSITIVE_OPERAND, ZERO, MULTIPLICATION);
         Assert.assertEquals(ZERO, result, DELTA);
         result = calculator.calculate(NEGATIVE_OPERAND, ZERO, MULTIPLICATION);
@@ -82,14 +82,14 @@ public class TestCalculator {
 
     @Test
     public void divisionWithValidOperandsTest() {
-        result = calculator.calculate(POSITIVE_OPERAND, NEGATIVE_OPERAND, DIVISION);
-        Assert.assertEquals(-5.5, result, DELTA);
         result = calculator.calculate(POSITIVE_OPERAND, POSITIVE_OPERAND, DIVISION);
         Assert.assertEquals(1, result, DELTA);
-        result = calculator.calculate(NEGATIVE_OPERAND, POSITIVE_OPERAND, DIVISION);
-        Assert.assertEquals(-0.18181818181818182, result, DELTA);
         result = calculator.calculate(NEGATIVE_OPERAND, NEGATIVE_OPERAND, DIVISION);
         Assert.assertEquals(1, result, DELTA);
+        result = calculator.calculate(POSITIVE_OPERAND, NEGATIVE_OPERAND, DIVISION);
+        Assert.assertEquals(-5.5, result, DELTA);
+        result = calculator.calculate(NEGATIVE_OPERAND, POSITIVE_OPERAND, DIVISION);
+        Assert.assertEquals(-0.18181818181818182, result, DELTA);
         result = calculator.calculate(ZERO, POSITIVE_OPERAND, DIVISION);
         Assert.assertEquals(ZERO, result, DELTA);
         result = calculator.calculate(ZERO, NEGATIVE_OPERAND, DIVISION);
@@ -103,19 +103,19 @@ public class TestCalculator {
 
     @Test
     public void raisingToPowerTest() {
-        result = calculator.calculate(POSITIVE_OPERAND, NEGATIVE_OPERAND, POWER);
-        Assert.assertEquals(0.008264462809917356, result, DELTA);
         result = calculator.calculate(POSITIVE_OPERAND, POSITIVE_OPERAND, POWER);
         Assert.assertEquals(2.85311670611E11, result, DELTA);
         result = calculator.calculate(NEGATIVE_OPERAND, NEGATIVE_OPERAND, POWER);
         Assert.assertEquals(0.25, result, DELTA);
-        result = calculator.calculate(ZERO, POSITIVE_OPERAND, POWER);
-        Assert.assertEquals(ZERO, result, DELTA);
+        result = calculator.calculate(POSITIVE_OPERAND, NEGATIVE_OPERAND, POWER);
+        Assert.assertEquals(0.008264462809917356, result, DELTA);
         result = calculator.calculate(POSITIVE_OPERAND, ZERO, POWER);
         Assert.assertEquals(1, result, DELTA);
         result = calculator.calculate(NEGATIVE_OPERAND, ZERO, POWER);
         Assert.assertEquals(1, result, DELTA);
         result = calculator.calculate(ZERO, ZERO, POWER);
         Assert.assertEquals(1, result, DELTA);
+        result = calculator.calculate(ZERO, POSITIVE_OPERAND, POWER);
+        Assert.assertEquals(ZERO, result, DELTA);
     }
 }
