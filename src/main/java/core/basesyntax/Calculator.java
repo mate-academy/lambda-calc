e415124d46ac5911341c4ad99bfec9aaffce6608
ROMASHSX <andriiromash@gmail.com>
2020-08-18 14:26:56 +0300

    whitespace near brackets added

--- src/main/java/core/basesyntax/Calculator.java
+++ src/main/java/core/basesyntax/Calculator.java
@@ -2,7 +2,7 @@ package core.basesyntax;
 
 public class Calculator {
 
-    public double calculate(double value1, double value2, char operation){
+    public double calculate(double value1, double value2, char operation) {
         switch (operation) {
             case '-':
                 return value1 - value2;
