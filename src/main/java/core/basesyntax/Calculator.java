6e9dcc9c577234c6c9ee7d7884506007fcb7d809
Martynice <marticiy@ukr.net>
2020-08-18 20:57:27 +0300

    added solution

---
+++ src/main/java/core/basesyntax/Calculator.java
@@ -0,0 +1,37 @@
+package core.basesyntax;
+
+/**
+ * Feel free to remove this class and create your own.
+ */
+public class Calculator {
+
+    private static final char ADDITION = '+';
+    private static final char SUBTRACTION = '-';
+    private static final char MULTIPLICATION = '*';
+    private static final char DIVISION = '/';
+    private static final char POWER = '^';
+
+    public static double calculate(double number1, double number2, char action) {
+        switch (action) {
+            case ADDITION:
+                return number1 + number2;
+            case SUBTRACTION:
+                return number1 - number2;
+            case MULTIPLICATION:
+                return number1 * number2;
+            case DIVISION:
+                return division(number1, number2);
+            case POWER:
+                return Math.pow(number1, number2);
+            default:
+                throw new IllegalArgumentException("Action " + action + " is illegal");
+        }
+    }
+
+    private static double division(double number1, double number2) {
+        if (number2 == 0) {
+            throw new ArithmeticException("Division by 0");
+        }
+        return number1 / number2;
+    }
+}
---
+++ src/test/java/core/basesyntax/CalculatorTest.java
@@ -0,0 +1,77 @@
+package core.basesyntax;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Feel free to remove this class and create your own.
+ */
+public class CalculatorTest {
+
+    private static final char ADDITION = '+';
+    private static final char SUBTRACTION = '-';
+    private static final char MULTIPLICATION = '*';
+    private static final char DIVISION = '/';
+    private static final char POWER = '^';
+    private static final char ILLEGAL_ACTION = '&';
+    private static final int ZERO = 0;
+    private static final double NUMBER_1 = 2.0;
+    private static final double NUMBER_2 = 3.0;
+    private static final double NEGATIVE_NUMBER_1 = -2.0;
+    private static final double NEGATIVE_NUMBER_2 = -3.0;
+    private static final double DELTA = 0.01;
+
+    private static Calculator calculator;
+
+    @Before
+    public void setUp() {
+        calculator = new Calculator();
+    }
+
+    @Test
+    public void additionIsOk() {
+        Assert.assertEquals(5.0, Calculator.calculate(NUMBER_1, NUMBER_2, ADDITION), DELTA);
+        Assert.assertEquals(-5.0, Calculator.calculate(NEGATIVE_NUMBER_1, NEGATIVE_NUMBER_2, ADDITION), DELTA);
+        Assert.assertEquals(-1.0, Calculator.calculate(NUMBER_1, NEGATIVE_NUMBER_2, ADDITION), DELTA);
+    }
+
+    @Test
+    public void subtractionIsOk() {
+        Assert.assertEquals(-1.0, Calculator.calculate(NUMBER_1, NUMBER_2, SUBTRACTION), DELTA);
+        Assert.assertEquals(1.0, Calculator.calculate(NEGATIVE_NUMBER_1, NEGATIVE_NUMBER_2, SUBTRACTION), DELTA);
+        Assert.assertEquals(5.0, Calculator.calculate(NUMBER_1, NEGATIVE_NUMBER_2, SUBTRACTION), DELTA);
+    }
+
+    @Test
+    public void multiplicationIsOk() {
+        Assert.assertEquals(6.0, Calculator.calculate(NUMBER_1, NUMBER_2, MULTIPLICATION), DELTA);
+        Assert.assertEquals(6.0, Calculator.calculate(NEGATIVE_NUMBER_1, NEGATIVE_NUMBER_2, MULTIPLICATION), DELTA);
+        Assert.assertEquals(-6.0, Calculator.calculate(NUMBER_1, NEGATIVE_NUMBER_2, MULTIPLICATION), DELTA);
+    }
+
+    @Test
+    public void divisionIsOk() {
+        Assert.assertEquals(1.5, Calculator.calculate(NUMBER_2, NUMBER_1, DIVISION), DELTA);
+        Assert.assertEquals(1.5, Calculator.calculate(NEGATIVE_NUMBER_2, NEGATIVE_NUMBER_1, DIVISION), DELTA);
+        Assert.assertEquals(-1.5, Calculator.calculate(NUMBER_2, NEGATIVE_NUMBER_1, DIVISION), DELTA);
+    }
+
+    @Test
+    public void raisingToPowerIsOk() {
+        Assert.assertEquals(8.0, Calculator.calculate(NUMBER_1, NUMBER_2, POWER), DELTA);
+        Assert.assertEquals(-0.125, Calculator.calculate(NEGATIVE_NUMBER_1, NEGATIVE_NUMBER_2, POWER), DELTA);
+        Assert.assertEquals(0.125, Calculator.calculate(NUMBER_1, NEGATIVE_NUMBER_2, POWER), DELTA);
+        Assert.assertEquals(1, Calculator.calculate(NUMBER_1, ZERO, POWER), DELTA);
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void divisionByZero() {
+        Calculator.calculate(NUMBER_1, ZERO, DIVISION);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void illegalAction() {
+        Calculator.calculate(NUMBER_1, ZERO, ILLEGAL_ACTION);
+    }
+}
