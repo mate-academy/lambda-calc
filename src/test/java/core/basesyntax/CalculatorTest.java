9fd6eb5395624ccaa5d7df50f9db619865c72d5c
m.mikhajlov <lizappy31@gmail.com>
2020-08-19 15:49:16 +0300

    created calculator, added tests

---
+++ src/test/java/core/basesyntax/CalculatorTest.java
@@ -0,0 +1,121 @@
+package core.basesyntax;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * Feel free to remove this class and create your own.
+ */
+public class CalculatorTest {
+    private static final char ADD = '+';
+    private static final char SUB = '-';
+    private static final char DIV = '/';
+    private static final char MULT = '*';
+    private static final char POW = '^';
+    private static final double ZERO = 0.0;
+    private static final double FIRST_OPERAND = 5;
+    private static final double SECOND_OPERAND = 8;
+    private static final double FIRST_NEGATIVE_OPERAND = -6;
+    private static final double SECOND_NEGATIVE_OPERAND = -10;
+    private static final char ILLEGAL_OPERATOR = '!';
+    private static final double DELTA = 0.001;
+    private static Calculator calculator;
+
+    @BeforeClass
+    public static void setUp() {
+        calculator = new Calculator();
+    }
+
+    @Test
+    public void addition_ok() {
+        double expected = FIRST_OPERAND + SECOND_OPERAND;
+        double actual = calculator.calculate(FIRST_OPERAND, ADD, SECOND_OPERAND);
+        Assert.assertEquals(expected, actual, DELTA);
+    }
+
+    @Test
+    public void negativeAddition_ok() {
+        double expected = FIRST_NEGATIVE_OPERAND + SECOND_NEGATIVE_OPERAND;
+        double actual = calculator.calculate(FIRST_NEGATIVE_OPERAND, ADD, SECOND_NEGATIVE_OPERAND);
+        Assert.assertEquals(expected, actual, DELTA);
+    }
+
+    @Test
+    public void subtraction_ok() {
+        double expected = FIRST_OPERAND - SECOND_OPERAND;
+        double actual = calculator.calculate(FIRST_OPERAND, SUB, SECOND_OPERAND);
+        Assert.assertEquals(expected, actual, DELTA);
+    }
+
+    @Test
+    public void negativeSubtraction_ok() {
+        double expected = FIRST_NEGATIVE_OPERAND - SECOND_NEGATIVE_OPERAND;
+        double actual = calculator.calculate(FIRST_NEGATIVE_OPERAND, SUB, SECOND_NEGATIVE_OPERAND);
+        Assert.assertEquals(expected, actual, DELTA);
+    }
+
+    @Test
+    public void division_ok() {
+        double expected = FIRST_OPERAND / SECOND_OPERAND;
+        double actual = calculator.calculate(FIRST_OPERAND, DIV, SECOND_OPERAND);
+        Assert.assertEquals(expected, actual, DELTA);
+    }
+
+    @Test
+    public void negativeDivision_ok() {
+        double expected = FIRST_NEGATIVE_OPERAND / SECOND_NEGATIVE_OPERAND;
+        double actual = calculator.calculate(FIRST_NEGATIVE_OPERAND, DIV, SECOND_NEGATIVE_OPERAND);
+        Assert.assertEquals(expected, actual, DELTA);
+    }
+
+    @Test
+    public void multiplication_ok() {
+        double expected = FIRST_OPERAND * SECOND_OPERAND;
+        double actual = calculator.calculate(FIRST_OPERAND, MULT, SECOND_OPERAND);
+        Assert.assertEquals(expected, actual, DELTA);
+    }
+
+    @Test
+    public void negativeMultiplication_ok() {
+        double expected = FIRST_NEGATIVE_OPERAND * SECOND_NEGATIVE_OPERAND;
+        double actual = calculator.calculate(FIRST_NEGATIVE_OPERAND, MULT, SECOND_NEGATIVE_OPERAND);
+        Assert.assertEquals(expected, actual, DELTA);
+    }
+
+    @Test
+    public void risingToPower_ok() {
+        double expected = Math.pow(FIRST_OPERAND, SECOND_OPERAND);
+        double actual = calculator.calculate(FIRST_OPERAND, POW, SECOND_OPERAND);
+        Assert.assertEquals(expected, actual, DELTA);
+    }
+
+    @Test
+    public void negativeRisingToPower_ok() {
+        double expected = Math.pow(FIRST_NEGATIVE_OPERAND, SECOND_NEGATIVE_OPERAND);
+        double actual = calculator.calculate(FIRST_NEGATIVE_OPERAND, POW, SECOND_NEGATIVE_OPERAND);
+        Assert.assertEquals(expected, actual, DELTA);
+    }
+
+    @Test
+    public void divisionByZero() {
+        String expectedMessage = "Cannot divide by 0";
+        try {
+            calculator.calculate(FIRST_OPERAND, DIV, ZERO);
+            Assert.fail("Expected ArithmeticException");
+        } catch (ArithmeticException e) {
+            Assert.assertEquals(e.getMessage(), expectedMessage);
+        }
+    }
+
+    @Test
+    public void illegalOperatorTest() {
+        String expectedMessage = "Unsupported operator";
+        try {
+            calculator.calculate(FIRST_OPERAND, ILLEGAL_OPERATOR, SECOND_OPERAND);
+            Assert.fail("Expected IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            Assert.assertEquals(e.getMessage(), expectedMessage);
+        }
+    }
+}
