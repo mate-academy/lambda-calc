a78e07bcd0eaed24489338b4b99cb3dab39af181
Artem Vremere <vremere_a@ukr.net>
2020-08-19 12:14:57 +0300

    changed solution

--- src/test/java/core/basesyntax/CalculatorTest.java
+++ src/test/java/core/basesyntax/CalculatorTest.java
@@ -1,161 +1,148 @@
 package core.basesyntax;
 
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
-/**
- * Feel free to remove this class and create your own.
- */
 public class CalculatorTest {
+    private static Calculator calculator;
     private final double EPS = 1e-9;
+    private double x = 2;
+    private double y = 3;
 
-    @Test
-    public void simplePositiveAddedTest() {
-        Calculator calculator = new Calculator(2, 3, '+');
-        Assert.assertEquals(5, calculator.calculate(), EPS);
+    @BeforeClass
+    public static void createNewCalc() {
+        calculator = new Calculator();
     }
 
     @Test
-    public void simpleNegativeAddedTest() {
-        Calculator calculator = new Calculator(-2, -3, '+');
-        Assert.assertEquals(-5, calculator.calculate(), EPS);
+    public void positiveAddedTest() {
+        Assert.assertEquals(5, calculator.calculate(2, 3, '+'), EPS);
     }
 
     @Test
-    public void simpleAllZeroAddedTest() {
-        Calculator calculator = new Calculator(0, 0, '+');
-        Assert.assertEquals(0, calculator.calculate(), EPS);
+    public void negativeAddedTest() {
+        Assert.assertEquals(-5, calculator.calculate(-2, -3, '+'), EPS);
     }
 
     @Test
-    public void simpleXZeroAddedTest() {
-        Calculator calculator = new Calculator(0, 3, '+');
-        Assert.assertEquals(3, calculator.calculate(), EPS);
+    public void allZeroAddedTest() {
+        Assert.assertEquals(0, calculator.calculate(0, 0, '+'), EPS);
     }
 
     @Test
-    public void simpleYZeroAddedTest() {
-        Calculator calculator = new Calculator(2, 0, '+');
-        Assert.assertEquals(2, calculator.calculate(), EPS);
+    public void xZeroAddedTest() {
+        Assert.assertEquals(3, calculator.calculate(0, 3, '+'), EPS);
     }
 
     @Test
-    public void simplePositiveSubtractionTest() {
-        Calculator calculator = new Calculator(2, 3, '-');
-        Assert.assertEquals(-1, calculator.calculate(), EPS);
+    public void yZeroAddedTest() {
+        Assert.assertEquals(2, calculator.calculate(2, 0, '+'), EPS);
     }
 
     @Test
-    public void simpleNegativeSubtractionTest() {
-        Calculator calculator = new Calculator(-2, -3, '-');
-        Assert.assertEquals(1, calculator.calculate(), EPS);
+    public void positiveSubtractionTest() {
+        Assert.assertEquals(-1, calculator.calculate(2, 3, '-'), EPS);
     }
 
     @Test
-    public void simpleZeroSubtractionTest() {
-        Calculator calculator = new Calculator(0, 0, '-');
-        Assert.assertEquals(0, calculator.calculate(), EPS);
+    public void negativeSubtractionTest() {
+        Assert.assertEquals(1, calculator.calculate(-2, -3, '-'), EPS);
     }
 
     @Test
-    public void simpleXZeroSubtractionTest() {
-        Calculator calculator = new Calculator(0, 3, '-');
-        Assert.assertEquals(-3, calculator.calculate(), EPS);
+    public void zeroSubtractionTest() {
+        Assert.assertEquals(0, calculator.calculate(0, 0, '-'), EPS);
     }
 
     @Test
-    public void simpleYZeroSubtractionTest() {
-        Calculator calculator = new Calculator(2, 0, '-');
-        Assert.assertEquals(2, calculator.calculate(), EPS);
+    public void xZeroSubtractionTest() {
+        Assert.assertEquals(-3, calculator.calculate(0, 3, '-'), EPS);
     }
 
     @Test
-    public void simplePositiveDivisionTest() {
-        Calculator calculator = new Calculator(2, 3, '/');
-        Assert.assertEquals(0.6666666666666666, calculator.calculate(), EPS);
+    public void yZeroSubtractionTest() {
+        Assert.assertEquals(2, calculator.calculate(2, 0, '-'), EPS);
     }
 
     @Test
-    public void simpleNegativeDivisionTest() {
-        Calculator calculator = new Calculator(-2, -3, '/');
-        Assert.assertEquals(0.6666666666666666, calculator.calculate(), EPS);
+    public void positiveDivisionTest() {
+        Assert.assertEquals(0.6666666666666666, calculator.calculate(2, 3, '/'), EPS);
+    }
+
+    @Test
+    public void negativeDivisionTest() {
+        Assert.assertEquals(0.6666666666666666, calculator.calculate(-2, -3, '/'), EPS);
     }
 
     @Test(expected = ArithmeticException.class)
-    public void simpleZeroDivisionTest() {
-        Calculator calculator = new Calculator(0, 0, '/');
-        calculator.calculate();
+    public void zeroDivisionTest() {
+        calculator.calculate(0, 0, '/');
     }
 
     @Test
-    public void simpleXZeroDivisionTest() {
-        Calculator calculator = new Calculator(0, 3, '/');
-        Assert.assertEquals(0, calculator.calculate(), EPS);
+    public void xZeroDivisionTest() {
+        Assert.assertEquals(0, calculator.calculate(0, 3, '/'), EPS);
     }
 
     @Test(expected = ArithmeticException.class)
-    public void simpleYZeroDivisionTest() {
-        Calculator calculator = new Calculator(2, 0, '/');
-        calculator.calculate();
+    public void yZeroDivisionTest() {
+        calculator.calculate(2, 0, '/');
     }
 
     @Test
-    public void simplePositiveMultiplicationTest() {
-        Calculator calculator = new Calculator(2, 3, '*');
-        Assert.assertEquals(6, calculator.calculate(), EPS);
+    public void positiveMultiplicationTest() {
+        Assert.assertEquals(6, calculator.calculate(2, 3, '*'), EPS);
     }
 
     @Test
-    public void simpleNegativeMultiplicationTest() {
-        Calculator calculator = new Calculator(-2, -3, '*');
-        Assert.assertEquals(6, calculator.calculate(), EPS);
+    public void negativeMultiplicationTest() {
+        Assert.assertEquals(6, calculator.calculate(-2, -3, '*'), EPS);
     }
 
     @Test
-    public void simpleZeroMultiplicationTest() {
-        Calculator calculator = new Calculator(0, 0, '*');
-        Assert.assertEquals(0, calculator.calculate(), EPS);
+    public void zeroMultiplicationTest() {
+        Assert.assertEquals(0, calculator.calculate(0, 0, '*'), EPS);
     }
 
     @Test
-    public void simpleXZeroMultiplicationTest() {
-        Calculator calculator = new Calculator(0, 3, '*');
-        Assert.assertEquals(0, calculator.calculate(), EPS);
+    public void xZeroMultiplicationTest() {
+        Assert.assertEquals(0, calculator.calculate(0, 3, '*'), EPS);
     }
 
     @Test
-    public void simpleYZeroMultiplicationTest() {
-        Calculator calculator = new Calculator(2, 0, '*');
-        Assert.assertEquals(0, calculator.calculate(), EPS);
+    public void yZeroMultiplicationTest() {
+        Assert.assertEquals(0, calculator.calculate(2, 0, '*'), EPS);
     }
 
     @Test
-    public void simplePositiveRaisingToThePowerTest() {
-        Calculator calculator = new Calculator(2, 3, '^');
-        Assert.assertEquals(8, calculator.calculate(), EPS);
+    public void positiveRaisingToThePowerTest() {
+        Assert.assertEquals(8, calculator.calculate(2, 3, '^'), EPS);
     }
 
     @Test
-    public void simpleNegativeRaisingToThePowerTest() {
-        Calculator calculator = new Calculator(-2, -3, '^');
-        Assert.assertEquals(-0.125, calculator.calculate(), EPS);
+    public void negativeRaisingToThePowerTest() {
+        Assert.assertEquals(-0.125, calculator.calculate(-2, -3, '^'), EPS);
     }
 
     @Test
-    public void simpleZeroRaisingToThePowerTest() {
-        Calculator calculator = new Calculator(0, 0, '^');
-        Assert.assertEquals(1, calculator.calculate(), EPS);
+    public void zeroRaisingToThePowerTest() {
+        Assert.assertEquals(1, calculator.calculate(0, 0, '^'), EPS);
     }
 
     @Test
-    public void simpleXZeroRaisingToThePowerTest() {
-        Calculator calculator = new Calculator(0, 3, '^');
-        Assert.assertEquals(0, calculator.calculate(), EPS);
+    public void xZeroRaisingToThePowerTest() {
+        Assert.assertEquals(0, calculator.calculate(0, 3, '^'), EPS);
     }
 
     @Test
-    public void simpleYZeroRaisingToThePowerTest() {
-        Calculator calculator = new Calculator(2, 0, '^');
-        Assert.assertEquals(1, calculator.calculate(), EPS);
+    public void yZeroRaisingToThePowerTest() {
+        Assert.assertEquals(1, calculator.calculate(2, 0, '^'), EPS);
     }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void wrongOperatorTest() {
+        calculator.calculate(2, 3, ')');
+    }
+
 }
