955f8caba806051fd66169786430b7bed9f587a6
tkhambir <tatiana.khambir@gmail.com>
2020-08-19 18:03:15 +0300

    review rocommendation implemented

--- src/test/java/core/basesyntax/CalculatorTest.java
+++ src/test/java/core/basesyntax/CalculatorTest.java
@@ -1,19 +1,17 @@
 package core.basesyntax;
 
 import org.junit.Assert;
-import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
 
-@RunWith(JUnit4.class)
+
 public class CalculatorTest {
 
-    private Calculator calculator;
+    private static Calculator calculator;
 
-    @Before
-    public void init() {
-        this.calculator = new Calculator();
+    @BeforeClass
+    public static void init() {
+        calculator = new Calculator();
     }
 
     @Test(expected = IllegalArgumentException.class)
