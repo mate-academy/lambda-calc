beacaf5e55d053d3f0e01d274b181a29baad190f
Victoria-Moiseienko <teasevic@gmail.com>
2020-08-19 06:11:23 +0300

    update after review: remove main, use return instead of result, use Math.pow() instead of doRaising()

--- src/main/java/core/basesyntax/Calculator.java
+++ src/main/java/core/basesyntax/Calculator.java
@@ -1,70 +1,17 @@
 package core.basesyntax;
 
-import java.util.Scanner;
-
 public class Calculator {
-    static Scanner scanner = new Scanner(System.in);
-
-    public static void main(String[] args) {
-        double num1 = getNum();
-        char operation = getOperation();
-        double num2 = getNum();
-
-        double result = calc(num1, num2, operation);
-        System.out.println("Result: " + result);
-    }
-
-    public static double getNum() {
-        System.out.println("Enter the number :");
-        double num;
-        if (scanner.hasNextDouble()) {
-            num = scanner.nextDouble();
-        } else {
-            System.out.println("Error. Number can contain [,] and digital");
-            scanner.next();
-            num = getNum();
-        }
-        return num;
-    }
-
-    public static char getOperation() {
-        System.out.println("Math operation:");
-        char operation;
-        if (scanner.hasNext()) {
-            operation = scanner.next().charAt(0);
-        } else {
-            System.out.println("Error. Enter the math.operation:");
-            scanner.next();
-            operation = getOperation();
-        }
-        return operation;
-    }
-
-    public static double calc(double num1, double num2, char operation) {
-        double result = 0;
+    public double calc(double num1, double num2, char operation) {
         switch (operation) {
-            case '+':
-                result = num1 + num2;
-                break;
-            case '-':
-                result = num1 - num2;
-                break;
-            case '*':
-                result = num1 * num2;
-                break;
-            case '/': {
-                result = doDivision(num1, num2);
-                break;
-            }
-            case '^': {
-                result = doRaising(num1, num2);
-                break;
-            }
+            case '+': return num1 + num2;
+            case '-': return num1 - num2;
+            case '*': return num1 * num2;
+            case '/': return doDivision(num1, num2);
+            case '^': return Math.pow(num1, num2);
             default:
                 throw new IllegalArgumentException(
                         "Unsupported math operation. Enter from [+ - * / ^]");
         }
-        return result;
     }
 
     private static double doDivision(double num1, double num2) {
@@ -72,16 +19,5 @@ public class Calculator {
             throw new ArithmeticException("Division by 0");
         }
         return num1 / num2;
-    }
-
-    private static double doRaising(double num1, double num2) {
-        if (num2 < 0) {
-            throw new IllegalArgumentException("[" + num2 + "] raising is incorrect");
-        }
-        double result = 1;
-        for (int i = 1; i <= num2; i++) {
-            result = result * num1;
-        }
-        return result;
     }
 }
--- src/test/java/core/basesyntax/CalculatorTest.java
+++ src/test/java/core/basesyntax/CalculatorTest.java
@@ -2,11 +2,10 @@
 package core.basesyntax;
 
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 
 public class CalculatorTest {
-    public final ExpectedException exception = ExpectedException.none();
     private static final char PLUS = '+';
     private static final char MINUS = '-';
     private static final char MULTIPLY = '*';
@@ -21,50 +20,57 @@ public class CalculatorTest {
     private static final double NEGATIVE_VALUE2 = -5.0;
     private static final double ZERO = 0.0;
 
-    @Test
-    public void testAdditionCorrectWork() {
-        Assert.assertEquals(7.0, Calculator.calc(POSITIVE_VALUE1, POSITIVE_VALUE2, PLUS), DELTA);
-        Assert.assertEquals(0, Calculator.calc(POSITIVE_VALUE1, NEGATIVE_VALUE1, PLUS), DELTA);
+    private static Calculator calculatorInstance;
+
+    @BeforeClass
+    public static void setCalculator() {
+        calculatorInstance = new Calculator();
     }
 
     @Test
+    public void testAdditionCorrectWork() {
+        Assert.assertEquals(7.0, calculatorInstance.calc(POSITIVE_VALUE1, POSITIVE_VALUE2, PLUS), DELTA);
+        Assert.assertEquals(0, calculatorInstance.calc(POSITIVE_VALUE1, NEGATIVE_VALUE1, PLUS), DELTA);
+        Assert.assertEquals(POSITIVE_VALUE1, calculatorInstance.calc(POSITIVE_VALUE1, ZERO, PLUS), DELTA);
+        Assert.assertEquals(NEGATIVE_VALUE1, calculatorInstance.calc(NEGATIVE_VALUE1, ZERO, PLUS), DELTA);
+    }
+
+   @Test
     public void testSubtractionCorrectWork() {
-        Assert.assertEquals(3, Calculator.calc(POSITIVE_VALUE2, POSITIVE_VALUE1, MINUS), DELTA);
-        Assert.assertEquals(-3, Calculator.calc(NEGATIVE_VALUE2, NEGATIVE_VALUE1, MINUS), DELTA);
+        Assert.assertEquals(3, calculatorInstance.calc(POSITIVE_VALUE2, POSITIVE_VALUE1, MINUS), DELTA);
+        Assert.assertEquals(-3, calculatorInstance.calc(NEGATIVE_VALUE2, NEGATIVE_VALUE1, MINUS), DELTA);
     }
 
     @Test
     public void testDivisionCorrectWork() {
-        Assert.assertEquals(2.5, Calculator.calc(POSITIVE_VALUE2, POSITIVE_VALUE1, DIVIDE), DELTA);
-        Assert.assertEquals(-2.5, Calculator.calc(NEGATIVE_VALUE2, POSITIVE_VALUE1, DIVIDE), DELTA);
-        Assert.assertEquals(2.5, Calculator.calc(NEGATIVE_VALUE2, NEGATIVE_VALUE1, DIVIDE), DELTA);
+        Assert.assertEquals(2.5, calculatorInstance.calc(POSITIVE_VALUE2, POSITIVE_VALUE1, DIVIDE), DELTA);
+        Assert.assertEquals(-2.5, calculatorInstance.calc(NEGATIVE_VALUE2, POSITIVE_VALUE1, DIVIDE), DELTA);
+        Assert.assertEquals(2.5, calculatorInstance.calc(NEGATIVE_VALUE2, NEGATIVE_VALUE1, DIVIDE), DELTA);
     }
 
     @Test(expected = ArithmeticException.class)
-    public void testDivisionBy0() {
-        Calculator.calc(POSITIVE_VALUE1, ZERO,'/');
+    public void testDivisionByZero() {
+        calculatorInstance.calc(POSITIVE_VALUE1, ZERO, DIVIDE);
     }
 
     @Test
     public void testMultiplicationCorrectWork() {
-        Assert.assertEquals(10.0, Calculator.calc(POSITIVE_VALUE2, POSITIVE_VALUE1, MULTIPLY), DELTA);
-        Assert.assertEquals(-4.0, Calculator.calc(NEGATIVE_VALUE1, POSITIVE_VALUE1, MULTIPLY), DELTA);
-        Assert.assertEquals(10.0, Calculator.calc(NEGATIVE_VALUE1, NEGATIVE_VALUE2, MULTIPLY), DELTA);
+        Assert.assertEquals(10.0, calculatorInstance.calc(POSITIVE_VALUE2, POSITIVE_VALUE1, MULTIPLY), DELTA);
+        Assert.assertEquals(-4.0, calculatorInstance.calc(NEGATIVE_VALUE1, POSITIVE_VALUE1, MULTIPLY), DELTA);
+        Assert.assertEquals(10.0, calculatorInstance.calc(NEGATIVE_VALUE1, NEGATIVE_VALUE2, MULTIPLY), DELTA);
+        Assert.assertEquals(0.0, calculatorInstance.calc(POSITIVE_VALUE1, ZERO, MULTIPLY), DELTA);
     }
 
     @Test
     public void testRaisingToThePowerCorrectWork() {
-        Assert.assertEquals(25.0, Calculator.calc(POSITIVE_VALUE2, POSITIVE_VALUE1, RAISE), DELTA);
-        Assert.assertEquals(1, Calculator.calc(POSITIVE_VALUE1, ZERO, RAISE), DELTA);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testRaisingToTheNegativePower() {
-        Calculator.calc(POSITIVE_VALUE1, NEGATIVE_VALUE1, RAISE);
+        Assert.assertEquals(25.0, calculatorInstance.calc(POSITIVE_VALUE2, POSITIVE_VALUE1, RAISE), DELTA);
+        Assert.assertEquals(1, calculatorInstance.calc(POSITIVE_VALUE1, ZERO, RAISE), DELTA);
+        Assert.assertEquals(0.04, calculatorInstance.calc(POSITIVE_VALUE2, NEGATIVE_VALUE1, RAISE), DELTA);
+        Assert.assertEquals(4, calculatorInstance.calc(NEGATIVE_VALUE1, POSITIVE_VALUE1, RAISE), DELTA);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testWrongOperation() {
-        Calculator.calc(POSITIVE_VALUE1, POSITIVE_VALUE1, WRONG_OPERATOR);
+        calculatorInstance.calc(POSITIVE_VALUE1, POSITIVE_VALUE1, WRONG_OPERATOR);
     }
 }
