5f58aa9167d39cc5ab3869cae99c64f0a53e6c2a
SetMyDream <56842438+SetMyDream@users.noreply.github.com>
2020-08-19 11:19:14 +0300

    som minor changes

--- src/test/java/core/basesyntax/SimpleCalculatorTest.java
+++ src/test/java/core/basesyntax/SimpleCalculatorTest.java
@@ -2,9 +2,6 @@ package core.basesyntax;
 
 import org.junit.Assert;
 import org.junit.Test;
-
-import java.io.IOException;
-
 import static java.lang.Float.NaN;
 
 public class SimpleCalculatorTest {
