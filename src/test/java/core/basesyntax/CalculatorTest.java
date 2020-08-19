599ad95f67a1a19dd1756b6f6a0541673355d776
Pavlo Khomiak <pavlo.khomiak93@gmail.com>
2020-08-19 13:24:54 +0300

    Refactor code

--- src/test/java/core/basesyntax/CalculatorTest.java
+++ src/test/java/core/basesyntax/CalculatorTest.java
@@ -23,87 +23,104 @@ public class CalculatorTest {
         calculator = new Calculator();
     }
 
-    @AfterClass
-    public static void deleteCalculatorInstance() {
-        calculator = null;
+    @Test
+    public void getAddingResultInPositiveCase() {
+        double actualFirstPlusSecond = calculator.calculate(FIRST_VALUE, ADDITION_SIGN, SECOND_VALUE);
+        double expectedFirstPlusSecond = 16.;
+        Assert.assertEquals(expectedFirstPlusSecond, actualFirstPlusSecond, DELTA);
     }
 
     @Test
-    public void getAddingResultInDifferentCases() {
-        double actualFirstPlusSecond = calculator.calculate(FIRST_VALUE, ADDITION_SIGN, SECOND_VALUE);
+    public void getAddingResultInNegativeCase() {
         double actualFirstPlusNegative = calculator.calculate(FIRST_VALUE, ADDITION_SIGN, NEGATIVE_VALUE);
-
-        double expectedFirstPlusSecond = 16.;
         double expectedFirstPlusNegative = -4.;
-
-        Assert.assertEquals(expectedFirstPlusSecond, actualFirstPlusSecond, DELTA);
         Assert.assertEquals(expectedFirstPlusNegative, actualFirstPlusNegative, DELTA);
     }
 
     @Test
-    public void getSubtractingResultInDifferentCases() {
+    public void getSubtractingResultInPositiveCase() {
         double actualFirstMinusSecond = calculator.calculate(FIRST_VALUE, SUBTRACTION_SIGN, SECOND_VALUE);
-        double actualFirstMinusNegative = calculator.calculate(FIRST_VALUE, SUBTRACTION_SIGN, NEGATIVE_VALUE);
-
         double expectedFirstMinusSecond = -8.;
-        double expectedFirstMinusNegative = 12.;
-
         Assert.assertEquals(expectedFirstMinusSecond, actualFirstMinusSecond, DELTA);
+    }
+
+    @Test
+    public void getSubtractingResultInNegativeCase() {
+        double actualFirstMinusNegative = calculator.calculate(FIRST_VALUE, SUBTRACTION_SIGN, NEGATIVE_VALUE);
+        double expectedFirstMinusNegative = 12.;
         Assert.assertEquals(expectedFirstMinusNegative, actualFirstMinusNegative, DELTA);
     }
 
     @Test
-    public void getDivisionResultInDifferentCases() {
+    public void getDivisionResultInPositiveCase() {
         double actualSecondDivideFirst = calculator.calculate(SECOND_VALUE, DIVISION_SIGN, FIRST_VALUE);
-        double actualNegativeDivideFirst = calculator.calculate(NEGATIVE_VALUE, DIVISION_SIGN, FIRST_VALUE);
-        double actualZeroDivideFirst = calculator.calculate(ZERO_VALUE, DIVISION_SIGN, FIRST_VALUE);
-
         double expectedSecondDivideFirst = 3.;
-        double expectedNegativeDivideFirst = -2.;
-        double expectedZeroDivideFirst = 0.;
-
         Assert.assertEquals(expectedSecondDivideFirst, actualSecondDivideFirst, DELTA);
-        Assert.assertEquals(expectedNegativeDivideFirst, actualNegativeDivideFirst, DELTA);
-        Assert.assertEquals(expectedZeroDivideFirst, actualZeroDivideFirst, DELTA);
-    }
-
-    @Test(expected = ArithmeticException.class)
-    public void getDivisionResultInDivideByZeroCase() {
-        calculator.calculate(FIRST_VALUE, DIVISION_SIGN, ZERO_VALUE);
     }
 
     @Test
-    public void getMultiplicationResultInDifferentCases() {
+    public void getDivisionResultInNegativeCase() {
+        double actualNegativeDivideFirst = calculator.calculate(NEGATIVE_VALUE, DIVISION_SIGN, FIRST_VALUE);
+        double expectedNegativeDivideFirst = -2.;
+        Assert.assertEquals(expectedNegativeDivideFirst, actualNegativeDivideFirst, DELTA);
+    }
+
+    @Test
+    public void getDivisionResultInZeroCase() {
+        double actualZeroDivideFirst = calculator.calculate(ZERO_VALUE, DIVISION_SIGN, FIRST_VALUE);
+        double expectedZeroDivideFirst = 0.;
+        Assert.assertEquals(expectedZeroDivideFirst, actualZeroDivideFirst, DELTA);
+    }
+
+    @Test
+    public void getDivisionResultInDivideByZeroCase() {
+        Assert.assertTrue(Double.isInfinite(calculator.calculate(FIRST_VALUE, DIVISION_SIGN, ZERO_VALUE)));
+    }
+
+    @Test
+    public void getMultiplicationResultInPositiveCase() {
         double actualFirstMultiplySecond = calculator.calculate(FIRST_VALUE, MULTIPLICATION_SIGN, SECOND_VALUE);
-        double actualFirstMultiplyNegative = calculator.calculate(FIRST_VALUE, MULTIPLICATION_SIGN, NEGATIVE_VALUE);
-        double actualZeroMultiplyFirst = calculator.calculate(ZERO_VALUE, MULTIPLICATION_SIGN, FIRST_VALUE);
-
         double expectedFirstMultiplySecond = 48.;
-        double expectedFirstMultiplyNegative = -32.;
-        double expectedZeroMultiplyFirst = 0.;
-
         Assert.assertEquals(expectedFirstMultiplySecond, actualFirstMultiplySecond, DELTA);
+    }
+
+    @Test
+    public void getMultiplicationResultInNegativeCase() {
+        double actualFirstMultiplyNegative = calculator.calculate(FIRST_VALUE, MULTIPLICATION_SIGN, NEGATIVE_VALUE);
+        double expectedFirstMultiplyNegative = -32.;
         Assert.assertEquals(expectedFirstMultiplyNegative, actualFirstMultiplyNegative, DELTA);
+    }
+
+    @Test
+    public void getMultiplicationResultInZeroCase() {
+        double actualZeroMultiplyFirst = calculator.calculate(ZERO_VALUE, MULTIPLICATION_SIGN, FIRST_VALUE);
+        double expectedZeroMultiplyFirst = 0.;
         Assert.assertEquals(expectedZeroMultiplyFirst, actualZeroMultiplyFirst, DELTA);
     }
 
     @Test
-    public void getRaisingToThePowerResultInDifferentCases() {
+    public void getRaisingToThePowerResultInPositiveCase() {
         double actualFirstRaisingSecond = calculator.calculate(FIRST_VALUE, RAISING_TO_THE_POWER_SIGN, SECOND_VALUE);
+        double expectedFirstRaisingSecond = 16777216.;
+        Assert.assertEquals(expectedFirstRaisingSecond, actualFirstRaisingSecond, DELTA);
+    }
+
+    @Test
+    public void getRaisingToThePowerResultInNegativeCases() {
         double actualFirstRaisingNegative = calculator.calculate(FIRST_VALUE, RAISING_TO_THE_POWER_SIGN, NEGATIVE_VALUE);
         double actualNegativeRaisingFirst = calculator.calculate(NEGATIVE_VALUE, RAISING_TO_THE_POWER_SIGN, FIRST_VALUE);
-        double actualZeroRaisingFirst = calculator.calculate(ZERO_VALUE, RAISING_TO_THE_POWER_SIGN, FIRST_VALUE);
-        double actualFirstRaisingZero = calculator.calculate(FIRST_VALUE, RAISING_TO_THE_POWER_SIGN, ZERO_VALUE);
-
-        double expectedFirstRaisingSecond = 16777216.;
         double expectedFirstRaisingNegative = .0000152587890625;
         double expectedNegativeRaisingFirst = 4096.;
-        double expectedZeroRaisingFirst = 0.;
-        double expectedFirstRaisingZero = 1.;
-
-        Assert.assertEquals(expectedFirstRaisingSecond, actualFirstRaisingSecond, DELTA);
         Assert.assertEquals(expectedFirstRaisingNegative, actualFirstRaisingNegative, DELTA);
         Assert.assertEquals(expectedNegativeRaisingFirst, actualNegativeRaisingFirst, DELTA);
+    }
+
+    @Test
+    public void getRaisingToThePowerResultInZeroCases() {
+        double actualZeroRaisingFirst = calculator.calculate(ZERO_VALUE, RAISING_TO_THE_POWER_SIGN, FIRST_VALUE);
+        double actualFirstRaisingZero = calculator.calculate(FIRST_VALUE, RAISING_TO_THE_POWER_SIGN, ZERO_VALUE);
+        double expectedZeroRaisingFirst = 0.;
+        double expectedFirstRaisingZero = 1.;
         Assert.assertEquals(expectedZeroRaisingFirst, actualZeroRaisingFirst, DELTA);
         Assert.assertEquals(expectedFirstRaisingZero, actualFirstRaisingZero, DELTA);
     }
