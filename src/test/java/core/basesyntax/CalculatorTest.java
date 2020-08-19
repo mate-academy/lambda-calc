73c9bdf325fce83eea49f3c90f24761c42c3cd4d
romanHreits <romagreic@gmail.com>
2020-08-19 13:11:43 +0300

    added tests methods for every operation

--- src/test/java/core/basesyntax/CalculatorTest.java
+++ src/test/java/core/basesyntax/CalculatorTest.java
@@ -14,6 +14,7 @@ public class CalculatorTest {
     private static final String MULT_NULL_STRING = "0 * -30";
     private static final String POW_STRING = "2 ^ 7";
     private static final String SUBTR_STRING = "33 - 8";
+    private static final String SUBTR_NEGATIVE_STRING = "-5 - 8";
     private static final String POW_STRING_NEGATIVE = "-2 ^ 7";
     private static final String NULL_POW = "-2 ^ 0";
     private static final double DELTA = 1e-9;
@@ -26,7 +27,6 @@ public class CalculatorTest {
 
     @Test
     public void isWeCallTheRightClass() {
-        Assert.assertNull(calculator.getLastOperation());
         calculator.calculate(ADD_STRING);
         Assert.assertSame(calculator.getLastOperation().getClass(), AddOperation.class);
         calculator.calculate(DIV_STRING);
@@ -69,28 +69,46 @@ public class CalculatorTest {
     }
 
     @Test
-    public void isCorrectArithmeticOperations() {
+    public void okSubtractOperations() {
+        double subtrNegativeNumResult = -13.0;
+        Assert.assertEquals(subtrNegativeNumResult, calculator.calculate(SUBTR_NEGATIVE_STRING), DELTA);
+        double subtrResult = 25.0;
+        Assert.assertEquals(subtrResult, calculator.calculate(SUBTR_STRING), DELTA);
+    }
+
+    @Test
+    public void okAddOperations() {
         double addResult = 55.0;
         Assert.assertEquals(addResult, calculator.calculate(ADD_STRING), DELTA);
         double addNegativeNumResult = -11.0;
         Assert.assertEquals(addNegativeNumResult, calculator.calculate(ADD_NEGATIVE_STRING), DELTA);
+    }
+
+    @Test
+    public void okDivideOperations() {
         double divResult = 2.0;
         Assert.assertEquals(divResult, calculator.calculate(DIV_STRING), DELTA);
         double divNegativeNumResult = -5.0;
         Assert.assertEquals(divNegativeNumResult, calculator.calculate(DIV_NEGATIVE_STRING), DELTA);
-        double multResult = 750.0;
-        Assert.assertEquals(multResult, calculator.calculate(MULT_STRING), DELTA);
-        double multNegativeNumResult = -750.0;
-        Assert.assertEquals(multNegativeNumResult, calculator.calculate(MULT_NEGATIVE_STRING), DELTA);
-        double multNullResult = 0.0;
-        Assert.assertEquals(multNullResult, calculator.calculate(MULT_NULL_STRING), DELTA);
-        double subtrResult = 25.0;
-        Assert.assertEquals(subtrResult, calculator.calculate(SUBTR_STRING), DELTA);
+    }
+
+    @Test
+    public void okElevationToTheDegreeOperations() {
         double powResult = 128.0;
         Assert.assertEquals(powResult, calculator.calculate(POW_STRING), DELTA);
         double powNegativeNumResult = -128.0;
         Assert.assertEquals(powNegativeNumResult, calculator.calculate(POW_STRING_NEGATIVE), DELTA);
         double powNullResult = 1.0;
         Assert.assertEquals(powNullResult, calculator.calculate(NULL_POW), DELTA);
+    }
+
+    @Test
+    public void okMultiplicationOperations() {
+        double multResult = 750.0;
+        Assert.assertEquals(multResult, calculator.calculate(MULT_STRING), DELTA);
+        double multNegativeNumResult = -750.0;
+        Assert.assertEquals(multNegativeNumResult, calculator.calculate(MULT_NEGATIVE_STRING), DELTA);
+        double multNullResult = 0.0;
+        Assert.assertEquals(multNullResult, calculator.calculate(MULT_NULL_STRING), DELTA);
     }
 }
