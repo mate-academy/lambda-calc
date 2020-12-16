23c26a1a38c07d44f38106bd62e125f25c45b141
Oleh Kravets <oleg.kravets92@gmail.com>
2020-12-16 22:16:53 +0200

    added multiplication tests

--- src/test/java/core/basesyntax/MagicalTest.java
+++ src/test/java/core/basesyntax/MagicalTest.java
@@ -11,6 +11,12 @@ class MagicalTest {
     private static Magical magic;
     private static final int MAX_VALUE = Integer.MAX_VALUE;
     private static final int MIN_VALUE = Integer.MIN_VALUE;
+    private static final String ADD = "+";
+    private static final String SUB = "-";
+    private static final String DIV = "/";
+    private static final String MUL = "*";
+    private static final String POW = "p";
+    private static final String POW_UPPERCASE = "p";
 
     @BeforeEach
     void setUp() {
@@ -20,7 +26,7 @@ class MagicalTest {
     @Test
     void case_insensitive_operation_input_NotOk() {
         try {
-            magic.calculate(0, 0, "P");
+            magic.calculate(0, 0, POW_UPPERCASE);
         } catch (RuntimeException e) {
             fail("Method should be case insensitive");
         }
@@ -41,78 +47,96 @@ class MagicalTest {
     @Test
     void divide_by_zero_Ok() {
         Assertions.assertThrows(ArithmeticException.class,
-                () -> magic.calculate(5, 0, "/"));
+                () -> magic.calculate(5, 0, DIV));
     }
 
     @Test
     void divide_max_positives_Ok() {
         Assertions.assertEquals((double) MAX_VALUE / MAX_VALUE,
-                magic.calculate(MAX_VALUE, MAX_VALUE, "/"));
+                magic.calculate(MAX_VALUE, MAX_VALUE, DIV));
     }
 
     @Test
     void divide_min_negatives_Ok() {
         Assertions.assertEquals((double) MIN_VALUE / MIN_VALUE,
-                magic.calculate(MIN_VALUE, MIN_VALUE, "/"));
+                magic.calculate(MIN_VALUE, MIN_VALUE, DIV));
     }
 
     @Test
     void divide_max_positive_by_negative_Ok() {
         Assertions.assertEquals((double) MAX_VALUE / MIN_VALUE,
-                magic.calculate(MAX_VALUE, MIN_VALUE, "/"));
+                magic.calculate(MAX_VALUE, MIN_VALUE, DIV));
     }
 
     @Test
     void add_max_positive_Ok() {
         Assertions.assertEquals((double) MAX_VALUE + MAX_VALUE,
-                magic.calculate(MAX_VALUE, MAX_VALUE, "+"));
+                magic.calculate(MAX_VALUE, MAX_VALUE, ADD));
     }
 
     @Test
     void add_max_positive_NotOk() {
         Assertions.assertNotEquals(MAX_VALUE + MAX_VALUE,
-                magic.calculate(MAX_VALUE, MAX_VALUE, "+"));
+                magic.calculate(MAX_VALUE, MAX_VALUE, ADD));
     }
 
     @Test
     void add_max_positive_and_negative_Ok() {
         Assertions.assertEquals((double) MAX_VALUE + MIN_VALUE,
-                magic.calculate(MAX_VALUE, MIN_VALUE, "+"));
+                magic.calculate(MAX_VALUE, MIN_VALUE, ADD));
     }
 
     @Test
     void add_min_negative_Ok() {
         Assertions.assertEquals((double) MIN_VALUE + MIN_VALUE,
-                magic.calculate(MIN_VALUE, MIN_VALUE, "+"));
+                magic.calculate(MIN_VALUE, MIN_VALUE, ADD));
     }
 
     @Test
     void add_min_NotOk() {
         Assertions.assertNotEquals(MIN_VALUE + MIN_VALUE,
-                magic.calculate(MIN_VALUE, MIN_VALUE, "+"));
+                magic.calculate(MIN_VALUE, MIN_VALUE, ADD));
     }
 
     @Test
     void add_negative_Ok() {
         Assertions.assertEquals((double) MAX_VALUE + MIN_VALUE,
-                magic.calculate(MAX_VALUE, MIN_VALUE, "+"));
+                magic.calculate(MAX_VALUE, MIN_VALUE, ADD));
     }
 
     @Test
     void subtract_max_positives_Ok() {
         Assertions.assertEquals((double) MAX_VALUE - MAX_VALUE,
-                magic.calculate(MAX_VALUE, MAX_VALUE, "-"));
+                magic.calculate(MAX_VALUE, MAX_VALUE, SUB));
     }
 
     @Test
     void subtract_min_negatives_Ok() {
         Assertions.assertEquals((double) MIN_VALUE - MIN_VALUE,
-                magic.calculate(MAX_VALUE, MAX_VALUE, "-"));
+                magic.calculate(MAX_VALUE, MAX_VALUE, SUB));
     }
 
     @Test
     void subtract_max_positive_and_negative() {
         Assertions.assertEquals((double) MAX_VALUE - MIN_VALUE,
-                magic.calculate(MAX_VALUE, MIN_VALUE, "-"));
+                magic.calculate(MAX_VALUE, MIN_VALUE, SUB));
+    }
+
+    @Test
+    void multiply_max_positives_Ok() {
+        Assertions.assertEquals((double) MAX_VALUE * MAX_VALUE,
+                magic.calculate(MAX_VALUE, MAX_VALUE, MUL));
+    }
+
+    @Test
+    void multiply_min_positives_Ok() {
+        Assertions.assertEquals((double) MIN_VALUE * MIN_VALUE,
+                magic.calculate(MIN_VALUE, MIN_VALUE, MUL));
+    }
+
+    @Test
+    void multiply_max_positive_and_negative_Ok() {
+        Assertions.assertEquals((double) MAX_VALUE * MIN_VALUE,
+                magic.calculate(MAX_VALUE, MIN_VALUE, MUL));
     }
 }
