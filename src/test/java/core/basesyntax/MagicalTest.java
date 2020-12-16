0fed032316265d573aae957b7b850dc90b1ed219
Oleh Kravets <oleg.kravets92@gmail.com>
2020-12-16 20:33:55 +0200

    added incorrectInputOperationExceptionExpected test for runtime exception

--- src/test/java/core/basesyntax/MagicalTest.java
+++ src/test/java/core/basesyntax/MagicalTest.java
@@ -1,7 +1,24 @@
 package core.basesyntax;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MagicalTest {
+    static Magical magic;
+    @BeforeAll
+    static void beforeAll() {
+        magic = new Magical();
+    }
 
+    @Test
+    void incorrectInputOperationExceptionExpected() {
+        try {
+            magic.calculate(0, 0, 'c');
+        } catch (RuntimeException e) {
+            return;
+        }
+        fail("RuntimeException is expected with incorrect input");
+    }
 }
