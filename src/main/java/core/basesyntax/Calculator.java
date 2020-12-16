0c7cc1d51b791a7c0d6523bb0e68857962212ce0
Rostyslav Mandybur <mandyburostyk@gmail.com>
2020-12-16 16:05:03 +0200

    tests was added

---
+++ src/main/java/core/basesyntax/Calculator.java
@@ -0,0 +1,7 @@
+package core.basesyntax;
+
+public class Calculator {
+    public double calculate(int a, int b, String operate) {
+        return 0;
+    }
+}
---
+++ src/main/java/core/basesyntax/IncorrectOperationException.java
@@ -0,0 +1,7 @@
+package core.basesyntax;
+
+public class IncorrectOperationException extends RuntimeException {
+    public IncorrectOperationException(String message) {
+        super(message);
+    }
+}
