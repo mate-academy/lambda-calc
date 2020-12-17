1cb6e7f0e3d347cef2406d9c7175b1061c258f67
Okulov Vadim <Vedroidua@gmail.com>
2020-12-17 22:47:37 +0200

    Deleted HelloWorld and HelloWorldTest Created empty Calculator and CalculatorTest

---
+++ src/main/java/core/basesyntax/Calculator.java
@@ -0,0 +1,7 @@
+package core.basesyntax;
+
+public class Calculator {
+    public double calculate(int x, int y, char operation) {
+        return 0.00;
+    }
+}
---
+++ src/test/java/core/basesyntax/CalculatorTest.java
@@ -0,0 +1,17 @@
+package core.basesyntax;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+    
+    @BeforeEach
+    void setUp() {
+    }
+    
+    @Test
+    void calculate() {
+    }
+}
