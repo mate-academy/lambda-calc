482cada4ff10d1bd2457e5e751544f57417c9574
Rostyslav Mandybur <mandyburostyk@gmail.com>
2020-12-20 13:59:00 +0200

    switch was changed

--- src/main/java/core/basesyntax/Calculator.java
+++ src/main/java/core/basesyntax/Calculator.java
@@ -2,9 +2,6 @@ package core.basesyntax;
 
 public class Calculator {
     public double calculate(int a, int b, char operate) {
-        if (String.valueOf(operate).matches("[^*/+\\-^]")) {
-            throw new IncorrectOperationException("You try to do incorrect operation");
-        }
         if (b == 0 && operate == '/') {
             throw new ArithmeticException("You can't division by zero");
         }
@@ -14,7 +11,7 @@ public class Calculator {
             case '*': return (double) a * b;
             case '/': return (double) a / b;
             case '^': return Math.pow(a, b);
-            default: return 0.0;
+            default: throw new IncorrectOperationException("You try to do incorrect operation");
         }
     }
 }
