1c33b729d7d15bd36cb25a7160fd659261fe814d
Alex <65593125+Artificial-Friend@users.noreply.github.com>
2020-12-20 14:33:23 +0200

    checking if result in integer bounds moved to separate method

--- src/main/java/core/basesyntax/Calculator.java
+++ src/main/java/core/basesyntax/Calculator.java
@@ -11,32 +11,36 @@ public class Calculator implements Calculable {
         switch (operator) {
             case '+':
                 result = (long) operand1 + (long) operand2;
-                if (result > Integer.MAX_VALUE) {
-                    throw new ArithmeticException("Result is out of integer capacity");
-                }
+                checkIfInBounds(result);
                 return (int) result;
             case '-':
                 result = (long) operand1 - (long) operand2;
-                if (result < Integer.MIN_VALUE) {
-                    throw new ArithmeticException("Result is out of integer capacity");
-                }
+                checkIfInBounds(result);
                 return (int) result;
             case '/':
                 return operand1 / operand2;
             case '*':
                 result = (long) operand1 * (long) operand2;
-                if (result > Integer.MAX_VALUE || result < Integer.MIN_VALUE) {
-                    throw new ArithmeticException("Result is out of integer capacity");
-                }
+                checkIfInBounds(result);
                 return (int) result;
             case '^':
                 double doubleResult = Math.pow(operand1, operand2);
-                if (doubleResult > Integer.MAX_VALUE || doubleResult < Integer.MIN_VALUE) {
-                    throw new ArithmeticException("Result is out of integer capacity");
-                }
+                checkIfInBounds(doubleResult);
                 return (int) doubleResult;
             default:
                 throw new RuntimeException("ERROR impossible operand detected: " + operator);
+        }
+    }
+
+    private void checkIfInBounds(long value) {
+        if (value > Integer.MAX_VALUE || value < Integer.MIN_VALUE) {
+            throw new ArithmeticException("Result is out of integer capacity");
+        }
+    }
+
+    private void checkIfInBounds(double value) {
+        if (value > Integer.MAX_VALUE || value < Integer.MIN_VALUE) {
+            throw new ArithmeticException("Result is out of integer capacity");
         }
     }
 }
