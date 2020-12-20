18481958bde20bde96f82c568bbca03b9066cb66
Oleh Kravets <oleg.kravets92@gmail.com>
2020-12-20 13:33:57 +0200

    Small refactoring

--- src/main/java/core/basesyntax/Calculator.java
+++ src/main/java/core/basesyntax/Calculator.java
@@ -9,37 +9,30 @@ public class Calculator {
     private static final String DIV = "/";
     private static final String MUL = "*";
     private static final String POW = "p";
+    private static final String POW_UPPERCASE = "P";
 
-    /*
-    + second adds to first
-    - second subtracts from fist
-    / first is divisor and second is divider
-    * second times first
-    p first in the power of second
-     */
     public double calculate(int first, int second, String operation) {
         if (operation == null) {
             throw new NullPointerException();
         }
-        if (operation.equals(ADD)) {
-            return (double) first + second;
+        switch (operation) {
+            case ADD:
+                return (double) first + second;
+            case SUB:
+                return (double) first - second;
+            case MUL:
+                return (double) first * second;
+            case DIV:
+                if (second == 0) {
+                    throw new ArithmeticException("Can't divide by 0");
+                }
+                return (double) first / second;
+            case POW:
+            case POW_UPPERCASE:
+                return Math.pow(first, second);
+            default:
+                throw new RuntimeException("Invalid operation input! Was" + operation
+                        + " but + - * / p expected");
         }
-        if (operation.equals(SUB)) {
-            return (double) first - second;
-        }
-        if (operation.equals(DIV)) {
-            if (second == 0) {
-                throw new ArithmeticException("Can't divide by 0");
-            }
-            return (double) first / second;
-        }
-        if (operation.equals(MUL)) {
-            return (double) first * second;
-        }
-        if (operation.equals(POW) || operation.equals(POW.toUpperCase())) {
-            return Math.pow(first, second);
-        }
-        throw new RuntimeException("Invalid operation input! Was" + operation
-        + " but + - * / p expected");
     }
 }
