f3e58d383350a3168ebf2f212a05aea175367034
Alex <65593125+Artificial-Friend@users.noreply.github.com>
2020-12-20 12:01:39 +0200

    Created tests for exceptions, arithmetics and integer overflow. Checks with exceptions created in Calculator

--- src/main/java/core/basesyntax/Calculator.java
+++ src/main/java/core/basesyntax/Calculator.java
@@ -7,17 +7,34 @@ public class Calculator implements Calculable{
 
     @Override
     public int calculate(int operand1, int operand2, char operator) {
+        long result;
         switch(operator) {
             case '+':
-                return operand1 + operand2;
+                result = (long) operand1 + (long) operand2;
+                if (result > Integer.MAX_VALUE){
+                    throw new ArithmeticException("Result is out of integer capacity");
+                }
+                return (int) result;
             case '-':
-                return operand1 - operand2;
+                result = (long) operand1 - (long) operand2;
+                if (result < Integer.MIN_VALUE){
+                    throw new ArithmeticException("Result is out of integer capacity");
+                }
+                return (int) result;
             case '/':
                 return operand1 / operand2;
             case '*':
-                return operand1 * operand2;
+                result = (long) operand1 * (long) operand2;
+                if (result > Integer.MAX_VALUE || result < Integer.MIN_VALUE){
+                    throw new ArithmeticException("Result is out of integer capacity");
+                }
+                return (int) result;
             case '^':
-                return (int) Math.pow(operand1, operand2);
+                double doubleResult = Math.pow(operand1, operand2);
+                if (doubleResult > Integer.MAX_VALUE || doubleResult < Integer.MIN_VALUE){
+                    throw new ArithmeticException("Result is out of integer capacity");
+                }
+                return (int) doubleResult;
             default:
                 throw new RuntimeException("ERROR impossible operand detected: " + operator);
         }
--- src/test/java/core/basesyntax/CalculatorTest.java
+++ src/test/java/core/basesyntax/CalculatorTest.java
@@ -8,11 +8,83 @@ class CalculatorTest {
     Calculator calculator = new Calculator();
 
     @Test
-    void additionTest() {
+    void integerOverflow_ThrowsException() {
+        assertThrows(ArithmeticException.class, () -> calculator.calculate(Integer.MAX_VALUE, Integer.MAX_VALUE, '*'));
+        assertThrows(ArithmeticException.class, () -> calculator.calculate(Integer.MIN_VALUE, 1, '-'));
+        assertThrows(ArithmeticException.class, () -> calculator.calculate(Integer.MAX_VALUE, 1, '+'));
+        assertThrows(ArithmeticException.class, () -> calculator.calculate(Integer.MAX_VALUE, Integer.MAX_VALUE, '^'));
+        assertThrows(ArithmeticException.class, () -> calculator.calculate(Integer.MAX_VALUE, 2, '^'));
+    }
+
+    @Test
+    void additionTest_Correct() {
         int sum1and0 = calculator.calculate(1, 0, '+');
         int sum0and1 = calculator.calculate(0, 1, '+');
         int sum2and2 = calculator.calculate(2, 2, '+');
 
         assertEquals(4,sum2and2);
+        assertEquals(1, sum0and1);
+        assertEquals(1, sum1and0);
+
+    }
+
+    @Test
+    void subtractionTest_Correct() {
+        int sub1and0 = calculator.calculate(1, 0, '-');
+        int sub0and1 = calculator.calculate(0, 1, '-');
+        int sub2and2 = calculator.calculate(2, 2, '-');
+
+        assertEquals(0,sub2and2);
+        assertEquals(-1, sub0and1);
+        assertEquals(1, sub1and0);
+    }
+
+    @Test
+    void divisionByZeroTest_ThrowsException() {
+        assertThrows(ArithmeticException.class, () -> calculator.calculate(0, 0, '/'));
+        assertThrows(ArithmeticException.class, () -> calculator.calculate(21, 0, '/'));
+    }
+
+    @Test
+    void divisionTest_Correct() {
+        int div4and2 = calculator.calculate(4, 2, '/');
+        int div2and2 = calculator.calculate(2, 2, '/');
+        int div21and1 = calculator.calculate(21, 1, '/');
+
+        assertEquals(2,div4and2);
+        assertEquals(1, div2and2);
+        assertEquals(21, div21and1);
+    }
+
+    @Test
+    void multiplicationTest_Correct() {
+        int multi4and1 = calculator.calculate(4, 1, '*');
+        int multi2and2 = calculator.calculate(2, 2, '*');
+        int multi21and0 = calculator.calculate(21, 0, '*');
+
+        assertEquals(4,multi4and1);
+        assertEquals(4, multi2and2);
+        assertEquals(0, multi21and0);
+    }
+
+    @Test
+    void raisingToPowerTest_Correct() {
+        int power0to21 = calculator.calculate(0, 21, '^');
+        int power0to0 = calculator.calculate(0, 0, '^');
+        int power1to21 = calculator.calculate(1, 21, '^');
+        int power5to5 = calculator.calculate(5, 5, '^');
+
+        assertEquals(0,power0to21);
+        assertEquals(1,power0to0);
+        assertEquals(1, power1to21);
+        assertEquals(3125, power5to5);
+    }
+
+    @Test
+    void invalidOperatorTest_ThrowsException() {
+        assertThrows(RuntimeException.class, () -> calculator.calculate(1, 0, 'a'));
+        assertThrows(RuntimeException.class, () -> calculator.calculate(0, 1, '\\'));
+        assertThrows(RuntimeException.class, () -> calculator.calculate(2, 2, '.'));
+        assertThrows(RuntimeException.class, () -> calculator.calculate(2, 2, '\0'));
     }
 }
