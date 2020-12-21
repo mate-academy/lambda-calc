6b90da7c2dabd1853b46af94a690bc5a25e2326b
Rostyslav Mandybur <mandyburostyk@gmail.com>
2020-12-21 11:11:53 +0200

    cheching for div bt zero was changed

--- src/main/java/core/basesyntax/Calculator.java
+++ src/main/java/core/basesyntax/Calculator.java
@@ -2,14 +2,14 @@ package core.basesyntax;
 
 public class Calculator {
     public double calculate(int a, int b, char operate) {
-        if (b == 0 && operate == '/') {
-            throw new ArithmeticException("You can't division by zero");
-        }
         switch (operate) {
             case '+': return (double) a + b;
             case '-': return (double) a - b;
             case '*': return (double) a * b;
-            case '/': return (double) a / b;
+            case '/': if (b == 0) {
+                    throw new ArithmeticException("You can't division by zero");
+                }
+            return (double) a / b;
             case '^': return Math.pow(a, b);
             default: throw new IncorrectOperationException("You try to do incorrect operation");
         }
