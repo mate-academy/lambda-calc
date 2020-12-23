1c13d58bf45a7961df43a753cf2f6c622ee2fd22
Okulov Vadim <Vedroidua@gmail.com>
2020-12-23 15:40:58 +0200

    Added more tests

--- src/test/java/core/basesyntax/CalculatorTest.java
+++ src/test/java/core/basesyntax/CalculatorTest.java
@@ -21,47 +21,89 @@ class CalculatorTest {
     
     @Test
     void calculate_addition() {
-        assertEquals(0.1005, calculator.calculate(0.1, OPERATION_ADD, 0.0005));
-        assertEquals(0, calculator.calculate(10, OPERATION_ADD, -10));
-        assertEquals(444, calculator.calculate(123, OPERATION_ADD, 321));
         assertEquals(1_001_000, calculator.calculate(1_000_000, OPERATION_ADD, 1000));
-        assertEquals(49, calculator.calculate(47, OPERATION_ADD, 2));
+        assertEquals(444, calculator.calculate(123, OPERATION_ADD, 321));
+        assertEquals(-20, calculator.calculate(-10, OPERATION_ADD, -10));
+        assertEquals(-90, calculator.calculate(10, OPERATION_ADD, -100));
+        assertEquals(90, calculator.calculate(-10, OPERATION_ADD, 100));
+        assertEquals(123, calculator.calculate(123, OPERATION_ADD, 0));
+        assertEquals(234, calculator.calculate(0, OPERATION_ADD, 234));
+        assertEquals(0, calculator.calculate(0, OPERATION_ADD, 0));
+        assertEquals(Double.POSITIVE_INFINITY, calculator.calculate(Double.MAX_VALUE,
+                OPERATION_ADD, Double.MAX_VALUE));
+        assertEquals(Double.MAX_VALUE, calculator.calculate(Double.MAX_VALUE,
+                OPERATION_ADD, Double.MIN_VALUE));
     }
     
     @Test
     void calculate_subtraction() {
-        assertEquals(0.0995, calculator.calculate(0.1, OPERATION_SUB, 0.0005));
-        assertEquals(20, calculator.calculate(10, OPERATION_SUB, -10));
-        assertEquals(-198, calculator.calculate(123, OPERATION_SUB, 321));
         assertEquals(999_000, calculator.calculate(1_000_000, OPERATION_SUB, 1000));
-        assertEquals(45, calculator.calculate(47, OPERATION_SUB, 2));
+        assertEquals(-198, calculator.calculate(123, OPERATION_SUB, 321));
+        assertEquals(0, calculator.calculate(-10, OPERATION_SUB, -10));
+        assertEquals(110, calculator.calculate(10, OPERATION_SUB, -100));
+        assertEquals(-110, calculator.calculate(-10, OPERATION_SUB, 100));
+        assertEquals(123, calculator.calculate(123, OPERATION_SUB, 0));
+        assertEquals(-234, calculator.calculate(0, OPERATION_SUB, 234));
+        assertEquals(0, calculator.calculate(0, OPERATION_SUB, 0));
+        assertEquals(0, calculator.calculate(Double.MAX_VALUE,
+                OPERATION_SUB, Double.MAX_VALUE));
+        assertEquals(-100, calculator.calculate(Double.MIN_VALUE,
+                OPERATION_SUB, 100));
+    }
+    
+    @Test
+    void calculate_divisionByZero() {
+        assertThrows(ArithmeticException.class, () -> calculator.calculate(0, '/', 0));
+        assertThrows(ArithmeticException.class, () -> calculator.calculate(234, '/', 0));
+        assertThrows(ArithmeticException.class, () -> calculator.calculate(-234, '/', 0));
     }
     
     @Test
     void calculate_division() {
-        assertEquals(200, calculator.calculate(0.1, OPERATION_DIV, 0.0005));
-        assertEquals(-1, calculator.calculate(10, OPERATION_DIV, -10));
-        assertEquals(41, calculator.calculate(123, OPERATION_DIV, 3));
         assertEquals(1000, calculator.calculate(1_000_000, OPERATION_DIV, 1000));
-        assertEquals(23.5, calculator.calculate(47, OPERATION_DIV, 2));
+        assertEquals(3, calculator.calculate(333, OPERATION_DIV, 111));
+        assertEquals(1, calculator.calculate(-10, OPERATION_DIV, -10));
+        assertEquals(-0.1, calculator.calculate(10, OPERATION_DIV, -100));
+        assertEquals(-0.1, calculator.calculate(-10, OPERATION_DIV, 100));
+        assertEquals(0, calculator.calculate(0, OPERATION_DIV, 234));
+        assertEquals(1, calculator.calculate(Double.MAX_VALUE,
+                OPERATION_DIV, Double.MAX_VALUE));
+        assertEquals(Double.POSITIVE_INFINITY, calculator.calculate(Double.MAX_VALUE,
+                OPERATION_DIV, Double.MIN_VALUE));
     }
     
     @Test
     void calculate_multiplication() {
-        assertEquals(0.5, calculator.calculate(0.1, OPERATION_MUL, 5));
-        assertEquals(-100, calculator.calculate(10, OPERATION_MUL, -10));
-        assertEquals(42435, calculator.calculate(123, OPERATION_MUL, 345));
         assertEquals(1_000_000, calculator.calculate(1000, OPERATION_MUL, 1000));
-        assertEquals(94, calculator.calculate(47, OPERATION_MUL, 2));
+        assertEquals(39483, calculator.calculate(123, OPERATION_MUL, 321));
+        assertEquals(100, calculator.calculate(-10, OPERATION_MUL, -10));
+        assertEquals(-1000, calculator.calculate(10, OPERATION_MUL, -100));
+        assertEquals(-1000, calculator.calculate(-10, OPERATION_MUL, 100));
+        assertEquals(0, calculator.calculate(123, OPERATION_MUL, 0));
+        assertEquals(0, calculator.calculate(0, OPERATION_MUL, 234));
+        assertEquals(0, calculator.calculate(0, OPERATION_MUL, 0));
+        assertEquals(Double.POSITIVE_INFINITY, calculator.calculate(Double.MAX_VALUE,
+                OPERATION_MUL, Double.MAX_VALUE));
+        double expected = Double.MAX_VALUE * Double.MIN_VALUE;
+        assertEquals(expected, calculator.calculate(Double.MAX_VALUE,
+                OPERATION_MUL, Double.MIN_VALUE));
     }
     
     @Test
     void calculate_power() {
-        assertEquals(0.25, calculator.calculate(0.5, OPERATION_POW, 2));
-        assertEquals(1_000_000, calculator.calculate(0.001, OPERATION_POW, -2));
-        assertEquals(1860867, calculator.calculate(123, OPERATION_POW, 3));
-        assertEquals(1_000_000, calculator.calculate(10, OPERATION_POW, 6));
         assertEquals(2209, calculator.calculate(47, OPERATION_POW, 2));
+        assertEquals(25, calculator.calculate(5, OPERATION_POW, 2));
+        assertEquals(0.04, calculator.calculate(-5, OPERATION_POW, -2));
+        assertEquals(1_000_000, calculator.calculate(0.001, OPERATION_POW, -2));
+        assertEquals(-15625, calculator.calculate(-25, OPERATION_POW, 3));
+        assertEquals(1, calculator.calculate(123, OPERATION_POW, 0));
+        assertEquals(0, calculator.calculate(0, OPERATION_POW, 6));
+        assertEquals(Double.POSITIVE_INFINITY, calculator.calculate(Double.MAX_VALUE,
+                OPERATION_POW, Double.MAX_VALUE));
+        assertEquals(1, calculator.calculate(Double.MAX_VALUE,
+                OPERATION_POW, Double.MIN_VALUE));
+        assertEquals(1, calculator.calculate(Double.MIN_VALUE,
+                OPERATION_POW, Double.MIN_VALUE));
     }
     
     @Test
@@ -71,12 +113,9 @@ class CalculatorTest {
         assertThrows(ArithmeticException.class, () -> calculator.calculate(0, '&', 10));
         assertThrows(ArithmeticException.class, () -> calculator.calculate(0, '1', 10));
         assertThrows(ArithmeticException.class, () -> calculator.calculate(0, '_', 10));
-    }
-    
-    @Test
-    void calculate_divisionByZero() {
-        assertThrows(ArithmeticException.class, () -> calculator.calculate(0, '/', 0));
-        assertThrows(ArithmeticException.class, () -> calculator.calculate(234, '/', 0));
-        assertThrows(ArithmeticException.class, () -> calculator.calculate(-234, '/', 0));
+        assertThrows(ArithmeticException.class, () -> calculator.calculate(0, 'a', 10));
+        assertThrows(ArithmeticException.class, () -> calculator.calculate(0, 'x', 10));
+        assertThrows(ArithmeticException.class, () -> calculator.calculate(0, '~', 10));
+        assertThrows(ArithmeticException.class, () -> calculator.calculate(0, '_', 10));
     }
 }
