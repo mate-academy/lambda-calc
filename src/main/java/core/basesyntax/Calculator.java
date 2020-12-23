d7c4be1f3af497c80ad838ba9d3cc2fba0a70c10
Okulov Vadim <Vedroidua@gmail.com>
2020-12-23 20:36:06 +0200

    Renamed 'x' to 'firstNumber' and 'y' to 'secondNumber'

--- src/main/java/core/basesyntax/Calculator.java
+++ src/main/java/core/basesyntax/Calculator.java
@@ -1,21 +1,21 @@
 package core.basesyntax;
 
 public class Calculator {
-    public double calculate(double x, char operation, double y) {
+    public double calculate(double firstNumber, char operation, double secondNumber) {
         switch (operation) {
             case '+':
-                return x + y;
+                return firstNumber + secondNumber;
             case '-':
-                return x - y;
+                return firstNumber - secondNumber;
             case '*':
-                return x * y;
+                return firstNumber * secondNumber;
             case '/':
-                if (y == 0) {
+                if (secondNumber == 0) {
                     throw new ArithmeticException("Division by zero");
                 }
-                return x / y;
+                return firstNumber / secondNumber;
             case '^':
-                return Math.pow(x, y);
+                return Math.pow(firstNumber, secondNumber);
             default:
                 throw new ArithmeticException("Invalid math operator");
         }
