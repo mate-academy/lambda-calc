702c6ec266e53807e4ae45dd4ff0568685ec6075
ivlytvynets <ivlytvynets@users.noreply.github.com>
2020-12-23 10:35:00 +0200

    implemented class Calculator

--- src/main/java/core/basesyntax/Calculator.java
+++ src/main/java/core/basesyntax/Calculator.java
@@ -1,32 +1,53 @@
 package core.basesyntax;
 
-import core.basesyntax.exception.BigValueException;
+import core.basesyntax.exception.*;
 
 public class Calculator {
-    public static int add(int a, int b) {
-        if (a == Integer.MAX_VALUE && b > 0 || b == Integer.MAX_VALUE && a > 0) {
-            throw new BigValueException("Big value");
+    public double calculate(double a, double b, char operator) {
+        isValidOperator(operator);
+        return operator == '+' ? add(a, b) :
+               operator == '-' ? subtract(a, b) :
+               operator == '/' ? divide(a, b) :
+               operator == '*' ? multiply(a, b) :
+               raiseToPower(a, b);
+    }
+
+    private boolean isValidOperator(char operator) {
+        String operators = "+-*/^";
+        if (!operators.contains(String.valueOf(operator))) {
+            throw new IllegalOperatorException("Illegal operator");
+        }
+        return true;
+    }
+
+    private double add(double a, double b) {
+        if (a == Integer.MAX_VALUE && b > 0 || b == Integer.MAX_VALUE && a > 0
+                || a == Integer.MIN_VALUE && b < 0 || b == Integer.MIN_VALUE && a < 0) {
+            throw new InvalidValuesException("Out of range Integer. Operation - addition");
         }
         return a + b;
     }
 
-    public static int subtract(int a, int b) {
+    private double subtract(double a, double b) {
+        if (a == Integer.MAX_VALUE && b < 0 || b == Integer.MAX_VALUE && a < 0
+                || a == Integer.MIN_VALUE && b > 0 || b == Integer.MIN_VALUE && a > 0) {
+            throw new InvalidValuesException("Out of range Integer. Operation - subtraction");
+        }
         return a - b;
     }
 
-    public static double divide(int a, int b) {
+    private double divide(double a, double b) {
+        if (b == 0) {
+            throw new DivisionByZeroException("Division by zero");
+        }
         return a / b;
     }
 
-    public static int multiply(int a, int b) {
+    private double multiply(double a, double b) {
         return a * b;
     }
 
-    public static int raiseToPower(int a, int power) {
-        for (int i = 1; i <= power; i++) {
-            a *= a;
-        }
-        return a;
+    private double raiseToPower(double a, double power) {
+        return Math.pow(a, power);
     }
-
 }
--- src/main/java/core/basesyntax/exception/BigValueException.java
+++
@@ -1,7 +0,0 @@
-package core.basesyntax.exception;
-
-public class BigValueException extends RuntimeException {
-    public BigValueException(String message) {
-        super(message);
-    }
-}
