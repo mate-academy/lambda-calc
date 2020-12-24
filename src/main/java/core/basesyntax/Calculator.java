9b511285a45e332ab7e3934aac95827a98fb29f4
Sashk52 <a.tsven@gmail.com>
2020-12-24 14:59:51 +0200

    made correction after mentor's reject

--- src/main/java/core/basesyntax/Calculator.java
+++ src/main/java/core/basesyntax/Calculator.java
@@ -2,35 +2,28 @@ package core.basesyntax;
 
 public class Calculator {
     public double calculate(double firstDigit, double secondDigit, char operator) {
-        double result;
         switch (operator) {
             case '+':
-                result = firstDigit + secondDigit;
-                break;
+                return firstDigit + secondDigit;
             case '-':
-                result = firstDigit - secondDigit;
-                break;
+                return firstDigit - secondDigit;
             case '*':
-                result = firstDigit * secondDigit;
-                break;
+                return firstDigit * secondDigit;
             case '/':
                 if (secondDigit == 0) {
                     throw new ArithmeticException("Dividing by zero forbidden");
                 } else {
-                    result = firstDigit / secondDigit;
+                    return firstDigit / secondDigit;
                 }
-                break;
             case '^':
                 if (firstDigit < 0 && secondDigit < 1) {
                     throw new ArithmeticException("Negative number can't "
                             + "be raised to power less than 1 ");
                 } else {
-                    result = Math.pow(firstDigit, secondDigit);
-                    break;
+                    return Math.pow(firstDigit, secondDigit);
                 }
             default:
                 throw new UnsupportedOperationException("Operator was not defined properly");
         }
-        return result;
     }
 }
--- src/test/java/core/basesyntax/CalculatorTest.java
+++ src/test/java/core/basesyntax/CalculatorTest.java
@@ -8,58 +8,58 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-class CalculatorTest {
+public class CalculatorTest {
     private static Calculator calculator;
 
     @BeforeAll
-    static void beforeAll() {
+    public static void beforeAll() {
         calculator = new Calculator();
     }
 
     @Test
-    void additingTwoPositiveNumber_Ok() {
+    public void additingTwoPositiveNumber_Ok() {
         double actual = calculator.calculate(100.5, 200.3, '+');
         double expected = 300.8;
         assertEquals(actual, expected, "Failed additing two positive numbers");
     }
 
     @Test
-    void additingOnePositiveAndOneNegative_Ok() {
+    public void additingOnePositiveAndOneNegative_Ok() {
         double actual = calculator.calculate(-10.1, 50.4, '+');
         double expected = 40.3;
         assertEquals(actual, expected, "Failed additing negative and positive numbers");
     }
 
     @Test
-    void additingTwoNegativeNumber_Ok() {
+    public void additingTwoNegativeNumber_Ok() {
         double actual = calculator.calculate(-15.55, -5000.45, '+');
         double expected = -5016;
         assertEquals(actual, expected, "Failed additing two negative numbers");
     }
 
     @Test
-    void additingFirstZeroTest_Ok() {
+    public void additingFirstZeroTest_Ok() {
         double actual = calculator.calculate(0, 356, '+');
         double expected = 356;
         assertEquals(actual, expected, "Failed additing first zero positive number");
     }
 
     @Test
-    void additingSecondZeroTest_Ok() {
+    public void additingSecondZeroTest_Ok() {
         double actual = calculator.calculate(2000.89, 0, '+');
         double expected = 2000.89;
         assertEquals(actual, expected, "Failed additing number and zero");
     }
 
     @Test
-    void additingOneInfinityTest_Ok() {
+    public void additingOneInfinityTest_Ok() {
         double actual = calculator.calculate(86.0, Double.POSITIVE_INFINITY, '+');
         double expected = Double.POSITIVE_INFINITY;
         assertEquals(actual, expected, "Failed additing number and positive infinity");
     }
 
     @Test
-    void additingTwoInfinityTest_NotOk() {
+    public void additingTwoInfinityTest_NotOk() {
         double actual = calculator.calculate(Double.POSITIVE_INFINITY,
                  Double.POSITIVE_INFINITY, '+');
         double expected = 0.0;
@@ -67,56 +67,56 @@ class CalculatorTest {
     }
 
     @Test
-    void additingMinAndMaxVaueTest_Ok() {
+    public void additingMinAndMaxVaueTest_Ok() {
         double actual = calculator.calculate(Double.MIN_VALUE, Double.MIN_VALUE, '+');
-        double exprected = 1.0E-323;
-        assertEquals(actual, exprected, "Failed additing MinValue and MaxValue");
+        double expected = 1.0E-323;
+        assertEquals(actual, expected, "Failed adding MinValue and MaxValue");
     }
 
     @Test
-    void substractionTwoPositiveNumberTest_Ok() {
+    public void substractionTwoPositiveNumberTest_Ok() {
         double actual = calculator.calculate(300.3, 200.3, '-');
         double expected = 100;
         assertEquals(actual, expected, "Failed substraction two positive numbers");
     }
 
     @Test
-    void substractionOnePositiveAndOneNegativeTest_Ok() {
+    public void substractionOnePositiveAndOneNegativeTest_Ok() {
         double actual = calculator.calculate(-10.1, 50.4, '-');
         double expected = -60.5;
         assertEquals(actual, expected, "Failed substraction negative and positive numbers");
     }
 
     @Test
-    void substractionTwoNegativeNumberTest_Ok() {
+    public void substractionTwoNegativeNumberTest_Ok() {
         double actual = calculator.calculate(-15.55, -5000.45, '-');
         double expected = 4984.9;
         assertEquals(actual, expected, "Failed substraction two negative numbers");
     }
 
     @Test
-    void substractionFirstZeroTest_Ok() {
+    public void substractionFirstZeroTest_Ok() {
         double actual = calculator.calculate(0, 356, '-');
         double expected = -356;
         assertEquals(actual, expected, "Failed substraction first zero positive number");
     }
 
     @Test
-    void substractionNumberAndSecondZeroTest_Ok() {
+    public void substractionNumberAndSecondZeroTest_Ok() {
         double expected = 2000.89;
         double actual = calculator.calculate(2000.89, 0.0, '-');
         assertEquals(actual, expected, "Failed substraction number and zero");
     }
 
     @Test
-    void substractionOneInfinityTest_Ok() {
+    public void substractionOneInfinityTest_Ok() {
         double actual = calculator.calculate(86.0, Double.POSITIVE_INFINITY, '-');
         double expected = Double.NEGATIVE_INFINITY;
         assertEquals(actual, expected, "Failed substraction number and positive infinity");
     }
 
     @Test
-    void substractionTwoInfinityTest_NotOk() {
+    public void substractionTwoInfinityTest_NotOk() {
         double actual = calculator.calculate(Double.POSITIVE_INFINITY,
                  Double.POSITIVE_INFINITY, '-');
         double expected = 0.0;
@@ -124,21 +124,21 @@ class CalculatorTest {
     }
 
     @Test
-    void substractionMinAndMaxVaueTest_Ok() {
+    public void substractionMinAndMaxVaueTest_Ok() {
         double actual = calculator.calculate(Double.MIN_VALUE, Double.MIN_VALUE, '-');
         double exprected = 0.0;
         assertEquals(actual, exprected, "Failed substraction MinValue and MaxValue");
     }
 
     @Test
-    void multiplyTwoPositiveNumberTest_Ok() {
+    public void multiplyTwoPositiveNumberTest_Ok() {
         double actual = calculator.calculate(100.5, 200.3, '*');
         double expected = 20130.15;
         assertEquals(actual, expected, "Failed multiplying two positive numbers");
     }
 
     @Test
-    void multiplyOnePositiveAndOneNegativeTest_Ok() {
+    public void multiplyOnePositiveAndOneNegativeTest_Ok() {
         double actual = calculator.calculate(-3.2, 50.5, '*');
         double expected = -161.6;
         double delta = 0.1;
@@ -146,35 +146,35 @@ class CalculatorTest {
     }
 
     @Test
-    void multiplyTwoNegativeNumberTest_Ok() {
+    public void multiplyTwoNegativeNumberTest_Ok() {
         double actual = calculator.calculate(-17.1, -521.4, '*');
         double expected = 8915.94;
         assertEquals(actual, expected, "Failed multiplying two negative numbers");
     }
 
     @Test
-    void multiplyFirstZeroTest_Ok() {
+    public void multiplyFirstZeroTest_Ok() {
         double actual = calculator.calculate(0, 356, '*');
         double expected = 0;
         assertEquals(actual, expected, "Failed multiplying first zero positive number");
     }
 
     @Test
-    void multiplySecondZeroTest_Ok() {
+    public void multiplySecondZeroTest_Ok() {
         double actual = calculator.calculate(2000.89, 0.0, '*');
         double expected = 0;
         assertEquals(actual, expected, "Failed multiplying number and zero");
     }
 
     @Test
-    void multiplyOneInfinityTest_Ok() {
+    public void multiplyOneInfinityTest_Ok() {
         double actual = calculator.calculate(86.0, Double.POSITIVE_INFINITY, '*');
         double expected = Double.POSITIVE_INFINITY;
         assertEquals(actual, expected, "Failed multiplying number and positive infinity");
     }
 
     @Test
-    void multiplyTwoInfinityTest_NotOk() {
+    public void multiplyTwoInfinityTest_NotOk() {
         double actual = calculator.calculate(Double.POSITIVE_INFINITY,
                  Double.POSITIVE_INFINITY, '*');
         double expected = Double.NEGATIVE_INFINITY;
@@ -182,49 +182,56 @@ class CalculatorTest {
     }
 
     @Test
-    void multiplyMinAndMaxVaueTest_Ok() {
+    public void multiplyMinAndMaxVaueTest_Ok() {
         double actual = calculator.calculate(Double.MIN_VALUE, Double.MIN_VALUE, '*');
         double exprected = 0.0;
         assertEquals(actual, exprected, "Failed multiplying MinValue and MaxValue");
     }
 
     @Test
-    void divideTwoPositiveNumberTest_Ok() {
+    public void divideTwoPositiveNumberTest_Ok() {
         double actual = calculator.calculate(186.85, 10.1, '/');
         double expected = 18.5;
         assertEquals(actual, expected, "Failed dividing two positive numbers");
     }
 
     @Test
-    void divideOnePositiveAndOneNegativeTest_Ok() {
+    public void divideOnePositiveAndOneNegativeTest_Ok() {
         double actual = calculator.calculate(-75.4, 14.5, '/');
         double expected = -5.2;
         assertEquals(actual, expected, "Failed dividing negative and positive numbers");
     }
 
     @Test
-    void divideTwoNegativeNumberTest_Ok() {
+    public void divideTwoNegativeNumberTest_Ok() {
         double actual = calculator.calculate(-5000.45, -100.009, '/');
         double expected = 50;
         assertEquals(actual, expected, "Failed dividing two negative numbers");
     }
 
     @Test
-    void divideFirstZeroTestTest_Ok() {
+    public void divideFirstZeroTestTest_Ok() {
         double actual = calculator.calculate(0, 356, '/');
         double expected = 0;
         assertEquals(actual, expected, "Failed dividing first zero positive number");
     }
 
     @Test
-    void divideOneInfinityTestTest_Ok() {
+    public void dividePositiveNumberOnZeroTest_Ok() {
+        assertThrows(ArithmeticException.class, () -> {
+            calculator.calculate(356, 0, '/');
+        });
+    }
+
+    @Test
+    public void divideOneInfinityTestTest_Ok() {
         double actual = calculator.calculate(86.0, Double.POSITIVE_INFINITY, '/');
         double expected = 0.0;
         assertEquals(actual, expected, "Failed dividing number and positive infinity");
     }
 
     @Test
-    void divideTwoInfinityTestTest_NotOk() {
+    public void divideTwoInfinityTestTest_NotOk() {
         double actual = calculator.calculate(Double.POSITIVE_INFINITY,
                  Double.POSITIVE_INFINITY, '/');
         double expected = 0.0;
@@ -232,14 +239,14 @@ class CalculatorTest {
     }
 
     @Test
-    void divideMinAndMaxVaueTest_Ok() {
+    public void divideMinAndMaxVaueTest_Ok() {
         double actual = calculator.calculate(Double.MIN_VALUE, Double.MIN_VALUE, '/');
         double exprected = 1.0;
         assertEquals(actual, exprected, "Failed dividing MinValue and MaxValue");
     }
 
     @Test
-    void raisingtoPowerTwoPositiveNumberTest_Ok() {
+    public void raisingtoPowerTwoPositiveNumberTest_Ok() {
         double actual = calculator.calculate(10, 2, '^');
         double expected = 100;
         assertEquals(actual, expected, "Failed raising one positive "
@@ -247,7 +254,7 @@ class CalculatorTest {
     }
 
     @Test
-    void raisingOnePositiveToPowerNegativeTest_Ok() {
+    public void raisingOnePositiveToPowerNegativeTest_Ok() {
         double actual = calculator.calculate(10, -2, '^');
         double expected = 0.01;
         assertEquals(actual, expected, "Failed raising one positive "
@@ -255,28 +262,28 @@ class CalculatorTest {
     }
 
     @Test
-    void raisingZeroToPowerPositiveNumberTest_Ok() {
+    public void raisingZeroToPowerPositiveNumberTest_Ok() {
         double actual = calculator.calculate(0, 3, '^');
         double expected = 0;
         assertEquals(actual, expected, "Failed raising zero to positive power");
     }
 
     @Test
-    void raisingNumberToZeroPowerTest_Ok() {
+    public void raisingNumberToZeroPowerTest_Ok() {
         double actual = calculator.calculate(125.1, 0, '^');
         double expected = 1;
         assertEquals(actual, expected, "Failed raising number to zero power");
     }
 
     @Test
-    void raisingNumberToInfinityPowerTest_Ok() {
+    public void raisingNumberToInfinityPowerTest_Ok() {
         double actual = calculator.calculate(86.0, Double.POSITIVE_INFINITY, '^');
         double expected = Double.POSITIVE_INFINITY;
         assertEquals(actual, expected, "Failed raising number to infinity power");
     }
 
     @Test
-    void raisingInfinityToInfinityPowerTest_NotOk() {
+    public void raisingInfinityToInfinityPowerTest_NotOk() {
         double actual = calculator.calculate(Double.POSITIVE_INFINITY,
                  Double.POSITIVE_INFINITY, '^');
         double expected = 0.0;
@@ -284,14 +291,14 @@ class CalculatorTest {
     }
 
     @Test
-    void raisingMinValueToPowerMaxVaueTest_Ok() {
+    public void raisingMinValueToPowerMaxVaueTest_Ok() {
         double actual = calculator.calculate(Double.MIN_VALUE, Double.MIN_VALUE, '^');
         double exprected = 1.0;
         assertEquals(actual, exprected, "Failed additing MinValue and MaxValue");
     }
 
     @Test
-    void edgeCasesTests() {
+    public void edgeCasesTests() {
         assertThrows(ArithmeticException.class, () -> {
             calculator.calculate(2000.89, 0, '/');
         });
@@ -310,21 +317,21 @@ class CalculatorTest {
     }
 
     @Test
-    void falseCasesTests_NotOk() {
-        double actual = calculator.calculate(123, 456, '+');
-        double notExperted = 123456;
-        assertFalse(actual == notExperted);
-        double actualExcrection = calculator.calculate(123456, 123, '-');
-        double notExpertedExctraction = 456;
-        assertFalse(actualExcrection == notExpertedExctraction);
+    public void falseCasesTests_NotOk() {
+        double actualAddition = calculator.calculate(123, 456, '+');
+        double notExpertedAddition = 123456;
+        assertFalse(actualAddition == notExpertedAddition);
+        double actualSubtraction = calculator.calculate(123456, 123, '-');
+        double notExpectedSubtraction = 456;
+        assertFalse(actualSubtraction == notExpectedSubtraction);
         double actualMultiply = calculator.calculate(3, 45, '*');
-        double notExpertedMutliply = 48;
-        assertFalse(actualMultiply == notExpertedMutliply);
+        double notExpectedMutliply = 48;
+        assertFalse(actualMultiply == notExpectedMutliply);
         double actualDivide = calculator.calculate(30, 3, '/');
-        double notExpertedDivide = 15;
-        assertFalse(actualDivide == notExpertedDivide);
+        double notExpectedDivide = 15;
+        assertFalse(actualDivide == notExpectedDivide);
         double actualRisePower = calculator.calculate(2, 3, '^');
-        double notExpertedRisePower = 6;
-        assertFalse(actualRisePower == notExpertedRisePower);
+        double notExpectedRisePowe = 6;
+        assertFalse(actualRisePower == notExpectedRisePowe);
     }
 }
