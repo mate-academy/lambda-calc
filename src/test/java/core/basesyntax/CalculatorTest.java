8f1798b146bc431796d216d9c5568eb5a0e3bbd1
Fedenko Maksym <fedenko.maxim.a@gmail.com>
2020-12-24 12:53:33 +0200

    removed target folder, added access modifier, added check for 0 raise in power, added check on illegal argument

--- src/test/java/core/basesyntax/CalculatorTest.java
+++ src/test/java/core/basesyntax/CalculatorTest.java
@@ -3,15 +3,16 @@ package core.basesyntax;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
-    static final double FIRST_CASE_A = 21;
-    static final double SECOND_CASE_A = -21;
-    static final double THIRD_CASE_B = 0;
-    static final double FORTH_CASE_A = Double.MAX_VALUE;
-    static final double FIFTH_CASE_A = Double.MIN_VALUE;
+    private static final double POSITIVE_VALUE = 21;
+    private static final double NEGATIVE_VALUE = -21;
+    private static final double ZERO_VALUE = 0;
+    private static final double MAX_VALUE = Double.MAX_VALUE;
+    private static final double MIN_VALUE = Double.MIN_VALUE;
     private static Calculator calculator;
 
     @BeforeAll
@@ -19,112 +20,97 @@ public class CalculatorTest {
         calculator = new Calculator();
     }
 
+    @AfterAll
+    static void afterAll() {
+        assertThrows(UnsupportedOperationException.class, () -> calculator.calculate(1, 2, 'd'));
+        assertThrows(UnsupportedOperationException.class, () -> calculator.calculate(1, 2, '%'));
+        assertThrows(UnsupportedOperationException.class, () -> calculator.calculate(1, 2, '9'));
+        assertThrows(UnsupportedOperationException.class, () -> calculator.calculate(1, 2, '('));
+        assertThrows(UnsupportedOperationException.class, () -> calculator.calculate(1, 2, '&'));
+        assertThrows(UnsupportedOperationException.class, () -> calculator.calculate(1, 2, '#'));
+        assertThrows(UnsupportedOperationException.class, () -> calculator.calculate(1, 2, '>'));
+    }
+
     @Test
     public void addition_Ok() {
-        final double expectedFirst = 42;
-        final double expectedSecond = -42;
-        final double expectedThird = 0;
-        final double expectedForth = Double.POSITIVE_INFINITY;
-        final double expectedFifth = 21;
-        final double expectedSixth = 1.7976931348623157E308;
-        final double actualFirst = calculator.calculate(FIRST_CASE_A, FIRST_CASE_A, '+');
-        final double actualSecond = calculator.calculate(SECOND_CASE_A, SECOND_CASE_A, '+');
-        final double actualThird = calculator.calculate(FIRST_CASE_A, SECOND_CASE_A, '+');
-        final double actualForth = calculator.calculate(FORTH_CASE_A, FORTH_CASE_A, '+');
-        final double actualFifth = calculator.calculate(FIRST_CASE_A, THIRD_CASE_B, '+');
-        final double actualSixth = calculator.calculate(FORTH_CASE_A, FIFTH_CASE_A, '+');
-        assertEquals(expectedFirst, actualFirst);
-        assertEquals(expectedSecond, actualSecond);
-        assertEquals(expectedThird, actualThird);
-        assertEquals(expectedForth, actualForth);
-        assertEquals(expectedFifth, actualFifth);
-        assertEquals(expectedSixth, actualSixth);
+        double actualFirst = calculator.calculate(POSITIVE_VALUE, POSITIVE_VALUE, '+');
+        assertEquals(42, actualFirst);
+        double actualSecond = calculator.calculate(NEGATIVE_VALUE, NEGATIVE_VALUE, '+');
+        assertEquals(-42, actualSecond);
+        double actualThird = calculator.calculate(POSITIVE_VALUE, NEGATIVE_VALUE, '+');
+        assertEquals(0, actualThird);
+        double actualForth = calculator.calculate(MAX_VALUE, MAX_VALUE, '+');
+        assertEquals(Double.POSITIVE_INFINITY, actualForth);
+        double actualFifth = calculator.calculate(POSITIVE_VALUE, ZERO_VALUE, '+');
+        assertEquals(21, actualFifth);
+        double actualSixth = calculator.calculate(MAX_VALUE, MIN_VALUE, '+');
+        assertEquals(1.7976931348623157E308, actualSixth);
     }
 
     @Test
     public void subtraction_Ok() {
-        final double expectedFirst = 0;
-        final double expectedSecond = 0;
-        final double expectedThird = 42;
-        final double expectedForth = 0;
-        final double expectedFifth = 21;
-        final double expectedSixth = 1.7976931348623157E308;
-        final double actualFirst = calculator.calculate(FIRST_CASE_A, FIRST_CASE_A, '-');
-        final double actualSecond = calculator.calculate(SECOND_CASE_A, SECOND_CASE_A, '-');
-        final double actualThird = calculator.calculate(FIRST_CASE_A, SECOND_CASE_A, '-');
-        final double actualForth = calculator.calculate(FORTH_CASE_A, FORTH_CASE_A, '-');
-        final double actualFifth = calculator.calculate(FIRST_CASE_A, THIRD_CASE_B, '-');
-        final double actualSixth = calculator.calculate(FORTH_CASE_A, FIFTH_CASE_A, '-');
-        assertEquals(expectedFirst, actualFirst);
-        assertEquals(expectedSecond, actualSecond);
-        assertEquals(expectedThird, actualThird);
-        assertEquals(expectedForth, actualForth);
-        assertEquals(expectedFifth, actualFifth);
-        assertEquals(expectedSixth, actualSixth);
+        double actualFirst = calculator.calculate(POSITIVE_VALUE, POSITIVE_VALUE, '-');
+        assertEquals(0, actualFirst);
+        double actualSecond = calculator.calculate(NEGATIVE_VALUE, NEGATIVE_VALUE, '-');
+        assertEquals(0, actualSecond);
+        double actualThird = calculator.calculate(POSITIVE_VALUE, NEGATIVE_VALUE, '-');
+        assertEquals(42, actualThird);
+        double actualForth = calculator.calculate(MAX_VALUE, MAX_VALUE, '-');
+        assertEquals(0, actualForth);
+        double actualFifth = calculator.calculate(POSITIVE_VALUE, ZERO_VALUE, '-');
+        assertEquals(21, actualFifth);
+        double actualSixth = calculator.calculate(MAX_VALUE, MIN_VALUE, '-');
+        assertEquals(1.7976931348623157E308, actualSixth);
     }
 
     @Test
     public void division_Ok() {
-        final double expectedFirst = 1;
-        final double expectedSecond = 1;
-        final double expectedThird = -1;
-        final double expectedForth = 1;
-        final double expectedFifth = Double.POSITIVE_INFINITY;
-        final double actualFirst = calculator.calculate(FIRST_CASE_A, FIRST_CASE_A, '/');
-        final double actualSecond = calculator.calculate(SECOND_CASE_A, SECOND_CASE_A, '/');
-        final double actualThird = calculator.calculate(FIRST_CASE_A, SECOND_CASE_A, '/');
-        final double actualForth = calculator.calculate(FORTH_CASE_A, FORTH_CASE_A, '/');
-        final double actualFifth = calculator.calculate(FORTH_CASE_A, FIFTH_CASE_A, '/');
-        assertEquals(expectedFirst, actualFirst);
-        assertEquals(expectedSecond, actualSecond);
-        assertEquals(expectedThird, actualThird);
-        assertEquals(expectedForth, actualForth);
-        assertEquals(expectedFifth, actualFifth);
+        double actualFirst = calculator.calculate(POSITIVE_VALUE, POSITIVE_VALUE, '/');
+        assertEquals(1, actualFirst);
+        double actualSecond = calculator.calculate(NEGATIVE_VALUE, NEGATIVE_VALUE, '/');
+        assertEquals(1, actualSecond);
+        double actualThird = calculator.calculate(POSITIVE_VALUE, NEGATIVE_VALUE, '/');
+        assertEquals(-1, actualThird);
+        double actualForth = calculator.calculate(MAX_VALUE, MAX_VALUE, '/');
+        assertEquals(1, actualForth);
+        double actualFifth = calculator.calculate(MAX_VALUE, MIN_VALUE, '/');
+        assertEquals(Double.POSITIVE_INFINITY, actualFifth);
         assertThrows(ArithmeticException.class, () -> calculator
-                .calculate(FIRST_CASE_A, THIRD_CASE_B, '/'), "Can't division on zero!");
+                .calculate(POSITIVE_VALUE, ZERO_VALUE, '/'), "Can't division on zero!");
     }
 
     @Test
     public void multiplication_Ok() {
-        final double expectedFirst = 441;
-        final double expectedSecond = 441;
-        final double expectedThird = -441;
-        final double expectedForth = Double.POSITIVE_INFINITY;
-        final double expectedFifth = 0;
-        final double expectedSixth = 8.881784197001251E-16;
-        final double actualFirst = calculator.calculate(FIRST_CASE_A, FIRST_CASE_A, '*');
-        final double actualSecond = calculator.calculate(SECOND_CASE_A, SECOND_CASE_A, '*');
-        final double actualThird = calculator.calculate(FIRST_CASE_A, SECOND_CASE_A, '*');
-        final double actualForth = calculator.calculate(FORTH_CASE_A, FORTH_CASE_A, '*');
-        final double actualFifth = calculator.calculate(FIRST_CASE_A, THIRD_CASE_B, '*');
-        final double actualSixth = calculator.calculate(FORTH_CASE_A, FIFTH_CASE_A, '*');
-        assertEquals(expectedFirst, actualFirst);
-        assertEquals(expectedSecond, actualSecond);
-        assertEquals(expectedThird, actualThird);
-        assertEquals(expectedForth, actualForth);
-        assertEquals(expectedFifth, actualFifth);
-        assertEquals(expectedSixth, actualSixth);
+        double actualFirst = calculator.calculate(POSITIVE_VALUE, POSITIVE_VALUE, '*');
+        assertEquals(441, actualFirst);
+        double actualSecond = calculator.calculate(NEGATIVE_VALUE, NEGATIVE_VALUE, '*');
+        assertEquals(441, actualSecond);
+        double actualThird = calculator.calculate(POSITIVE_VALUE, NEGATIVE_VALUE, '*');
+        assertEquals(-441, actualThird);
+        double actualForth = calculator.calculate(MAX_VALUE, MAX_VALUE, '*');
+        assertEquals(Double.POSITIVE_INFINITY, actualForth);
+        double actualFifth = calculator.calculate(POSITIVE_VALUE, ZERO_VALUE, '*');
+        assertEquals(0, actualFifth);
+        double actualSixth = calculator.calculate(MAX_VALUE, MIN_VALUE, '*');
+        assertEquals(8.881784197001251E-16, actualSixth);
     }
 
     @Test
     public void raisingToAPower_Ok() {
-        final double expectedFirst = 5.842587018385982E27;
-        final double expectedSecond = -1.7115705711410192E-28;
-        final double expectedThird = 1.7115705711410192E-28;
-        final double expectedForth = Double.POSITIVE_INFINITY;
-        final double expectedFifth = 1;
-        final double expectedSixth = 1;
-        final double actualFirst = calculator.calculate(FIRST_CASE_A, FIRST_CASE_A, '^');
-        final double actualSecond = calculator.calculate(SECOND_CASE_A, SECOND_CASE_A, '^');
-        final double actualThird = calculator.calculate(FIRST_CASE_A, SECOND_CASE_A, '^');
-        final double actualForth = calculator.calculate(FORTH_CASE_A, FORTH_CASE_A, '^');
-        final double actualFifth = calculator.calculate(FIRST_CASE_A, THIRD_CASE_B, '^');
-        final double actualSixth = calculator.calculate(FORTH_CASE_A, FIFTH_CASE_A, '^');
-        assertEquals(expectedFirst, actualFirst);
-        assertEquals(expectedSecond, actualSecond);
-        assertEquals(expectedThird, actualThird);
-        assertEquals(expectedForth, actualForth);
-        assertEquals(expectedFifth, actualFifth);
-        assertEquals(expectedSixth, actualSixth);
+        double actualFirst = calculator.calculate(POSITIVE_VALUE, POSITIVE_VALUE, '^');
+        assertEquals(5.842587018385982E27, actualFirst);
+        double actualSecond = calculator.calculate(NEGATIVE_VALUE, NEGATIVE_VALUE, '^');
+        assertEquals(-1.7115705711410192E-28, actualSecond);
+        double actualThird = calculator.calculate(POSITIVE_VALUE, NEGATIVE_VALUE, '^');
+        assertEquals(1.7115705711410192E-28, actualThird);
+        double actualForth = calculator.calculate(MAX_VALUE, MAX_VALUE, '^');
+        assertEquals(Double.POSITIVE_INFINITY, actualForth);
+        double actualFifth = calculator.calculate(POSITIVE_VALUE, ZERO_VALUE, '^');
+        assertEquals(1, actualFifth);
+        double actualSixth = calculator.calculate(MAX_VALUE, MIN_VALUE, '^');
+        assertEquals(1, actualSixth);
+        double actualSeventh = calculator.calculate(ZERO_VALUE, POSITIVE_VALUE, '^');
+        assertEquals(0, actualSeventh);
+
     }
 }
