b6de52de32790e9c7384776ee48263101c1e5e75
egorka <entertainment.veks@gmail.com>
2020-12-24 12:54:21 +0200

    first commit

--- src/test/java/core/basesyntax/CalculatorTest.java
+++ src/test/java/core/basesyntax/CalculatorTest.java
@@ -1,7 +1,9 @@
 package core.basesyntax;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorTest {
     Calculator calculator = new Calculator();
