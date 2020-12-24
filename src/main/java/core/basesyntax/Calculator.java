cc3809d27cbfa44792bf24203ccf92e6ce74ac60
AndreiYazadzhi <belator251000@gmail.com>
2020-12-24 11:24:32 +0200

    Move repeated logic to private method

--- src/main/java/core/basesyntax/Calculator.java
+++ src/main/java/core/basesyntax/Calculator.java
@@ -4,39 +4,39 @@ public class Calculator {
     public double calculate(double firstVariable, double secondVariable, char operation) {
         switch (operation) {
             case '+':
-                if (firstVariable == Double.MAX_VALUE
-                        || secondVariable == Double.MAX_VALUE) {
-                    throw new ValueOutOfBoundException("Your value more than Double max value.");
-                }
+                isResultInBoundForMax(firstVariable, secondVariable);
                 return firstVariable + secondVariable;
             case '-':
-                if (firstVariable == Double.MIN_VALUE
-                        || secondVariable == Double.MIN_VALUE) {
-                    throw new ValueOutOfBoundException("Your value more than Double min value.");
-                }
+                isResultInBoundForMin(firstVariable, secondVariable);
                 return firstVariable - secondVariable;
             case '*':
-                if (firstVariable == Double.MAX_VALUE
-                        || secondVariable == Double.MAX_VALUE) {
-                    throw new ValueOutOfBoundException("Your value more than Double max value.");
-                }
+                isResultInBoundForMax(firstVariable, secondVariable);
                 return firstVariable * secondVariable;
             case '/':
                 if (secondVariable == 0) {
                     throw new ArithmeticException("You can`t divide by zero");
-                } else if (firstVariable == Double.MIN_VALUE
-                        || secondVariable == Double.MIN_VALUE) {
-                    throw new ValueOutOfBoundException("Your value more than Double min value.");
                 }
+                isResultInBoundForMin(firstVariable, secondVariable);
                 return firstVariable / secondVariable;
             case '^':
-                if (firstVariable == Double.MAX_VALUE
-                        || secondVariable == Double.MAX_VALUE) {
-                    throw new ValueOutOfBoundException("Your value more than Double max value.");
-                }
+                isResultInBoundForMax(firstVariable, secondVariable);
                 return Math.pow(firstVariable, secondVariable);
             default:
                 throw new NotValidOperationException("Not valid operation!");
+        }
+    }
+
+    private void isResultInBoundForMax(double firstVariable, double secondVariable) {
+        if (firstVariable == Double.MAX_VALUE
+                || secondVariable == Double.MAX_VALUE) {
+            throw new ValueOutOfBoundException("Your result is out of bound.");
+        }
+    }
+
+    private void isResultInBoundForMin(double firstVariable, double secondVariable) {
+        if (firstVariable == Double.MIN_VALUE
+                || secondVariable == Double.MIN_VALUE) {
+            throw new ValueOutOfBoundException("Your result is out of bound.");
         }
     }
 }
