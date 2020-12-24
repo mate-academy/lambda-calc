708bce5c14e08e300e9fa9f2e0cc6b558f7514dc
egorka <entertainment.veks@gmail.com>
2020-12-24 16:37:14 +0200

    code refactored

--- src/test/java/core/basesyntax/CalculatorTest.java
+++ src/test/java/core/basesyntax/CalculatorTest.java
@@ -1,11 +1,10 @@
 package core.basesyntax;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
 
