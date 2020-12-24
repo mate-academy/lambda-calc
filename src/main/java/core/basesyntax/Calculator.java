7a25994d9de6144aa94bbabf96d6bb64b3a8f1f1
Ivan Marchenko <51205960+IwalkInSilence@users.noreply.github.com>
2020-12-24 13:39:47 +0200

    change if on switch

--- src/main/java/core/basesyntax/Calculator.java
+++ src/main/java/core/basesyntax/Calculator.java
@@ -2,25 +2,22 @@ package core.basesyntax;
 
 public class Calculator {
     public double calculate(double firstValue, double secondValue, String operation) {
-        if (operation.equals("+")) {
-            return firstValue + secondValue;
+        switch (operation) {
+            case "+":
+                return firstValue + secondValue;
+            case "-":
+                return firstValue - secondValue;
+            case "*":
+                return firstValue * secondValue;
+            case "/":
+                if (secondValue == 0) {
+                    throw new ArithmeticException("You can't division on zero!");
+                }
+                return firstValue / secondValue;
+            case "^":
+                return Math.pow(firstValue, secondValue);
+            default:
+                throw new ArithmeticException("You type wrong operator, plz try again");
         }
-        if (operation.equals("-")) {
-            return firstValue - secondValue;
-        }
-        if (operation.equals("*")) {
-            return firstValue * secondValue;
-        }
-        if (operation.equals("/")) {
-            if (secondValue == 0) {
-                throw new ArithmeticException("You can't division on zero!");
-            }
-            return firstValue / secondValue;
-        }
-        if (operation.equals("^")) {
-            return Math.pow(firstValue, secondValue);
-        }
-
-        throw new ArithmeticException("You type wrong operator, plz try again");
     }
 }
