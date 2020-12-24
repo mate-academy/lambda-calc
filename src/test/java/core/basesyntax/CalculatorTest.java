d054ffe46503bd0e31ca85dd228934daff833775
ivlytvynets <ivlytvynets@users.noreply.github.com>
2020-12-24 16:48:28 +0200

    edited unit tests

--- src/test/java/core/basesyntax/CalculatorTest.java
+++ src/test/java/core/basesyntax/CalculatorTest.java
@@ -9,8 +9,8 @@ import core.basesyntax.exception.InvalidValuesException;
 import org.junit.jupiter.api.Test;
 
 class CalculatorTest {
-    private static final int DEFAULT_A = 5;
-    private static final int DEFAULT_B = 17;
+    private static final int POSITIVE_A = 5;
+    private static final int POSITIVE_B = 17;
     private static final double ADD_A_B = 22;
     private static final double SUBTRACT_A_B = -12;
     private static final double MULTIPLY_A_B = 5 * 17;
@@ -22,71 +22,83 @@ class CalculatorTest {
     private Calculator calculator = new Calculator();
 
     @Test
-    void add_Ok() {
+    void addition_Ok() {
         double expected = ADD_A_B;
-        double actual = calculator.calculate(DEFAULT_A, DEFAULT_B, '+');
+        double actual = calculator.calculate(POSITIVE_A, POSITIVE_B, '+');
+        assertEquals(expected, actual);
+        expected = -10;
+        actual = calculator.calculate(-2.8, -7.2, '+');
         assertEquals(expected, actual);
     }
 
     @Test
-    void add_NotOk() {
+    void addition_NotOk() {
         assertThrows(InvalidValuesException.class, () -> {
             calculator.calculate(Integer.MAX_VALUE, 1, '+');
         });
     }
 
     @Test
-    void subtract_Ok() {
+    void subtraction_Ok() {
         double expected = SUBTRACT_A_B;
-        double actual = calculator.calculate(DEFAULT_A, DEFAULT_B, '-');
+        double actual = calculator.calculate(POSITIVE_A, POSITIVE_B, '-');
+        assertEquals(expected, actual);
+        expected = 4.4;
+        actual = calculator.calculate(-2.8, -7.2, '-');
         assertEquals(expected, actual);
     }
 
     @Test
-    void subtract_NotOk() {
+    void subtraction_NotOk() {
         assertThrows(InvalidValuesException.class, () -> {
             calculator.calculate(Integer.MAX_VALUE, -1, '-');
         });
     }
 
     @Test
-    void divide_Ok() {
+    void division_Ok() {
         double expected = DIVIDE_A_B;
-        double actual = calculator.calculate(DEFAULT_A, DEFAULT_B, '/');
+        double actual = calculator.calculate(POSITIVE_A, POSITIVE_B, '/');
         assertEquals(expected, actual);
-        expected = DIVIDE_B_A;
-        actual = calculator.calculate(DEFAULT_B, DEFAULT_A, '/');
+        expected = (double) -7 / 9;
+        actual = calculator.calculate(-7, 9, '/');
         assertEquals(expected, actual);
     }
 
     @Test
-    void divide_ByZero() {
+    void division_ByZero() {
         assertThrows(DivisionByZeroException.class, () -> {
-            calculator.calculate(DEFAULT_A, ZERO, '/');
+            calculator.calculate(POSITIVE_A, ZERO, '/');
         });
     }
 
     @Test
-    void multiply_Ok() {
+    void multiplication_Ok() {
         double expected = MULTIPLY_A_B;
-        double actual = calculator.calculate(DEFAULT_A, DEFAULT_B, '*');
+        double actual = calculator.calculate(POSITIVE_A, POSITIVE_B, '*');
+        assertEquals(expected, actual);
+        expected = -8;
+        actual = calculator.calculate(-2, 4, '*');
         assertEquals(expected, actual);
     }
 
     @Test
-    void raiseToPower_Ok() {
+    void raisingToPower_Ok() {
         double expected = RAISE_A_TO_B;
-        double actual = calculator.calculate(DEFAULT_A, DEFAULT_B, '^');
+        double actual = calculator.calculate(POSITIVE_A, POSITIVE_B, '^');
         assertEquals(expected, actual);
         expected = RAISE_B_TO_A;
-        actual = calculator.calculate(DEFAULT_B, DEFAULT_A, '^');
+        actual = calculator.calculate(POSITIVE_B, POSITIVE_A, '^');
+        assertEquals(expected, actual);
+        expected = 0.5;
+        actual = calculator.calculate(2, -1, '^');
         assertEquals(expected, actual);
     }
 
     @Test
     void notValidOperator() {
         assertThrows(IllegalOperatorException.class, () -> {
-            calculator.calculate(DEFAULT_A, DEFAULT_B, '%');
+            calculator.calculate(POSITIVE_A, POSITIVE_B, '%');
         });
     }
 }
