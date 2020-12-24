ab08fd95d45b37a9f495771e2e38cd3cd6c10b91
Baldinini <baldinat063@gmail.com>
2020-12-24 13:08:28 +0200

    bugs fixed

--- src/main/java/core/basesyntax/Calculator.java
+++ src/main/java/core/basesyntax/Calculator.java
@@ -2,30 +2,40 @@ package core.basesyntax;
 
 public class Calculator {
     public static double calculate(double number1, double number2, char operation) {
+        double result;
         switch (operation) {
             case '+':
-                isMaxValue(number1, number2);
-                return number1 + number2;
+                result = number1 + number2;
+                isInfinity(result);
+                return result;
             case '-':
-
-                return number1 - number2;
+                result = number1 - number2;
+                isInfinity(result);
+                return result;
             case '*':
-                isMaxValue(number1, number2);
-                return number1 * number2;
+                result = number1 * number2;
+                isInfinity(result);
+                return result;
             case '/':
                 if (number2 == 0) {
                     throw new ArithmeticException("Can't divide by zero!");
                 }
-                return number1 / number2;
+                result = number1 / number2;
+                return result;
             case '^':
-                return Math.pow(number1, number2);
+                if ((number1 <= 0 && (number2 > 0 && number2 < 1)
+                        || (number1 == 0 && number2 < 0))) {
+                    throw new ArithmeticException("Operation is impossible");
+                }
+                result = Math.pow(number1, number2);
+                return result;
             default:
                 throw new IllegalArgumentException("Wrong operation");
         }
     }
 
-    private static void isMaxValue(double number1, double number2) {
-        if (number1 == Double.MAX_VALUE && number1 == number2) {
+    private static void isInfinity(double value) {
+        if (value == Double.POSITIVE_INFINITY || value == Double.NEGATIVE_INFINITY) {
             throw new ArithmeticException("Infinity");
         }
     }
--- src/test/java/core/basesyntax/CalculatorTest.java
+++ src/test/java/core/basesyntax/CalculatorTest.java
@@ -6,61 +6,63 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
 
 class CalculatorTest {
-    private static final double DELTA = 0.0;
-    private static final double NUMBER1 = 25;
+    private static final double NUMBER1 = 25.003;
     private static final double NUMBER2 = 2;
 
     @Test
     void additionTwoNumbers() {
-        assertEquals(27, Calculator.calculate(NUMBER1, NUMBER2, '+'), DELTA);
-        assertEquals(-27, Calculator.calculate(-NUMBER1, -NUMBER2, '+'), DELTA);
-        assertEquals(23, Calculator.calculate(NUMBER1, -NUMBER2, '+'), DELTA);
-        assertEquals(2, Calculator.calculate(0, NUMBER2, '+'), DELTA);
+        assertEquals(NUMBER1 + NUMBER2, Calculator.calculate(NUMBER1, NUMBER2, '+'));
+        assertEquals(-NUMBER1 + (-NUMBER2), Calculator.calculate(-NUMBER1, -NUMBER2, '+'));
+        assertEquals(NUMBER1 + (-NUMBER2), Calculator.calculate(NUMBER1, -NUMBER2, '+'));
+        assertEquals(0 + NUMBER2, Calculator.calculate(0, NUMBER2, '+'));
         assertThrows(ArithmeticException.class, () ->
                 Calculator.calculate(Double.MAX_VALUE, Double.MAX_VALUE, '+'));
     }
 
     @Test
     void subtractionTwoNumbers() {
-        assertEquals(23, Calculator.calculate(NUMBER1, NUMBER2, '-'),DELTA);
-        assertEquals(-23, Calculator.calculate(-NUMBER1, -NUMBER2, '-'),DELTA);
-        assertEquals(-27, Calculator.calculate(-NUMBER1, NUMBER2, '-'),DELTA);
-        assertEquals(-2, Calculator.calculate(0, NUMBER2, '-'),DELTA);
-        assertEquals(0.0, Calculator.calculate(Double.MIN_VALUE, Double.MIN_VALUE, '-'),DELTA);
+        assertEquals(NUMBER1 - NUMBER2, Calculator.calculate(NUMBER1, NUMBER2, '-'));
+        assertEquals(-NUMBER1 - (-NUMBER2), Calculator.calculate(-NUMBER1, -NUMBER2, '-'));
+        assertEquals(-NUMBER1 - NUMBER2, Calculator.calculate(-NUMBER1, NUMBER2, '-'));
+        assertEquals(0 - NUMBER2, Calculator.calculate(0, NUMBER2, '-'));
+        assertEquals(0.0, Calculator.calculate(Double.MIN_VALUE, Double.MIN_VALUE, '-'));
+        assertThrows(ArithmeticException.class, () ->
+                Calculator.calculate(-Double.MAX_VALUE, Double.MAX_VALUE, '-'));
     }
 
     @Test
     void divisionTwoNumbers() {
-        assertEquals(12.5, Calculator.calculate(NUMBER1, NUMBER2, '/'), DELTA);
-        assertEquals(-0.08, Calculator.calculate(-NUMBER2, NUMBER1, '/'), DELTA);
-        assertEquals(0, Calculator.calculate(0, NUMBER1, '/'), DELTA);
-        assertEquals(1, Calculator.calculate(Double.MAX_VALUE, Double.MAX_VALUE, '/'), DELTA);
+        assertEquals(NUMBER1 / NUMBER2, Calculator.calculate(NUMBER1, NUMBER2, '/'));
+        assertEquals(NUMBER2 / (-NUMBER1), Calculator.calculate(-NUMBER2, NUMBER1, '/'));
+        assertEquals(0 / NUMBER1, Calculator.calculate(0, NUMBER1, '/'));
+        assertEquals(1, Calculator.calculate(Double.MAX_VALUE, Double.MAX_VALUE, '/'));
         assertThrows(ArithmeticException.class, () ->
                 Calculator.calculate(NUMBER2, 0, '/'));
     }
 
     @Test
     void multiplicationTwoNumbers() {
-        assertEquals(50, Calculator.calculate(NUMBER1, NUMBER2, '*'),DELTA);
-        assertEquals(-50, Calculator.calculate(NUMBER1, -NUMBER2, '*'),DELTA);
-        assertEquals(0, Calculator.calculate(0, NUMBER2, '*'),DELTA);
+        assertEquals(NUMBER1 * NUMBER2, Calculator.calculate(NUMBER1, NUMBER2, '*'));
+        assertEquals(NUMBER1 * (-NUMBER2), Calculator.calculate(NUMBER1, -NUMBER2, '*'));
+        assertEquals(0 * NUMBER2, Calculator.calculate(0, NUMBER2, '*'));
         assertThrows(ArithmeticException.class, () ->
                 Calculator.calculate(Double.MAX_VALUE, Double.MAX_VALUE, '*'));
     }
 
     @Test
     void raisingValueToPower() {
-        double division = Calculator.calculate(NUMBER1, -NUMBER2, '/');
-        assertEquals(156.25, Calculator.calculate(division, NUMBER2, '^'), DELTA);
-        assertEquals(Math.pow(division, -NUMBER2),
-                Calculator.calculate(division, -NUMBER2, '^'), DELTA);
-        assertEquals(1, Calculator.calculate(division, 0, '^'), DELTA);
-        assertEquals(0, Calculator.calculate(0, NUMBER1, '^'), DELTA);
+        assertEquals(Math.pow(NUMBER1, NUMBER2), Calculator.calculate(NUMBER1, NUMBER2, '^'));
+        assertEquals(Math.pow(NUMBER1, -NUMBER2),
+                Calculator.calculate(NUMBER1, -NUMBER2, '^'));
+        assertEquals(1, Calculator.calculate(NUMBER1, 0, '^'));
+        assertEquals(0, Calculator.calculate(0, NUMBER1, '^'));
+        assertThrows(ArithmeticException.class, () -> Calculator.calculate(0, -2, '^'));
+        assertThrows(ArithmeticException.class, () -> Calculator.calculate(-NUMBER1, 0.3, '^'));
     }
 
     @Test
     void operationIsOK() {
         assertThrows(IllegalArgumentException.class, () ->
-                Calculator.calculate(NUMBER1, NUMBER2, '5'));
+                Calculator.calculate(NUMBER1, NUMBER2, '%'));
     }
 }
