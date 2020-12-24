f8de7e9f45025e3998a71e64f979a39165902341
Ivan Marchenko <51205960+IwalkInSilence@users.noreply.github.com>
2020-12-24 14:50:30 +0200

    fix

--- src/test/java/core/basesyntax/CalculatorTest.java
+++ src/test/java/core/basesyntax/CalculatorTest.java
@@ -6,17 +6,17 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-class CalculatorTest {
+public class CalculatorTest {
     private static final double DELTA = 0.000001;
     private static Calculator calculator;
 
     @BeforeAll
-    static void setUp() {
+    public static void setUp() {
         calculator = new Calculator();
     }
 
     @Test
-    void additionTests_Ok() {
+    public void additionTests_Ok() {
         assertEquals(70, calculator.calculate(50, 20, "+"), DELTA);
         assertEquals(-140.5, calculator.calculate(-25.5, -115, "+"), DELTA);
         assertEquals(4.37, calculator.calculate(5.35, -0.98, "+"), DELTA);
@@ -29,7 +29,7 @@ class CalculatorTest {
     }
 
     @Test
-    void subtractionTests_Ok() {
+    public void subtractionTests_Ok() {
         assertEquals(30, calculator.calculate(50, 20, "-"), DELTA);
         assertEquals(89.5, calculator.calculate(-25.5, -115, "-"), DELTA);
         assertEquals(6.33, calculator.calculate(5.35, -0.98, "-"), DELTA);
@@ -44,7 +44,7 @@ class CalculatorTest {
     }
 
     @Test
-    void multiplicationTests_Ok() {
+    public void multiplicationTests_Ok() {
         assertEquals(1000, calculator.calculate(50, 20, "*"), DELTA);
         assertEquals(75, calculator.calculate(-5, -15, "*"), DELTA);
         assertEquals(-10.7, calculator.calculate(5.35, -2, "*"), DELTA);
@@ -60,7 +60,7 @@ class CalculatorTest {
     }
 
     @Test
-    void divisionTests_Ok() {
+    public void divisionTests_Ok() {
         assertEquals(2.5, calculator.calculate(50, 20, "/"), DELTA);
         assertEquals(25, calculator.calculate(-50, -2, "/"), DELTA);
         assertEquals(-5.333333, calculator.calculate(16, -3, "/"), DELTA);
@@ -74,29 +74,30 @@ class CalculatorTest {
     }
 
     @Test
-    void divisionByZero() {
+    public void divisionByZero() {
         assertThrows(ArithmeticException.class, () -> calculator.calculate(0, 0, "/"));
         assertThrows(ArithmeticException.class, () -> calculator.calculate(120, 0, "/"));
         assertThrows(ArithmeticException.class, () -> calculator.calculate(-190, 0, "/"));
     }
 
     @Test
-    void powerTest_Ok() {
+    public void powerTest_Ok() {
         assertEquals(4, calculator.calculate(2, 2, "^"), DELTA);
         assertEquals(0.04, calculator.calculate(5, -2, "^"), DELTA);
         assertEquals(-100000, calculator.calculate(-10, 5, "^"), DELTA);
         assertEquals(0.87055, calculator.calculate(2, -0.2, "^"), DELTA);
         assertEquals(0.00390625, calculator.calculate(0.5, 8, "^"), DELTA);
+        assertEquals(1, calculator.calculate(100500, 0, "^"), DELTA);
+        assertEquals(0, calculator.calculate(0, 500, "^"), DELTA);
         assertEquals(Double.POSITIVE_INFINITY, calculator.calculate(
                 Double.MAX_VALUE, Double.MAX_VALUE, "^"), DELTA);
         assertEquals(Double.POSITIVE_INFINITY, calculator.calculate(
                 Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, "^"), DELTA);
         assertEquals(0, calculator.calculate(
                 Double.POSITIVE_INFINITY, Double.NEGATIVE_INFINITY, "^"), DELTA);
-
     }
 
-    void invalidOperators() {
+    public void invalidOperators() {
         assertThrows(ArithmeticException.class, () -> calculator.calculate(1, 1, "&"));
         assertThrows(ArithmeticException.class, () -> calculator.calculate(1, 1, "|"));
         assertThrows(ArithmeticException.class, () -> calculator.calculate(1, 1, "="));
