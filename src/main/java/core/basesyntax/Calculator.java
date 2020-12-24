45f64c57c4b49d09267d0267740115c616fad4d9
Baldinini <baldinat063@gmail.com>
2020-12-24 13:34:24 +0200

    bugs fixed 2

--- src/main/java/core/basesyntax/Calculator.java
+++ src/main/java/core/basesyntax/Calculator.java
@@ -6,37 +6,32 @@ public class Calculator {
         switch (operation) {
             case '+':
                 result = number1 + number2;
-                isInfinity(result);
-                return result;
+                break;
             case '-':
                 result = number1 - number2;
-                isInfinity(result);
-                return result;
+                break;
             case '*':
                 result = number1 * number2;
-                isInfinity(result);
-                return result;
+                break;
             case '/':
                 if (number2 == 0) {
                     throw new ArithmeticException("Can't divide by zero!");
                 }
                 result = number1 / number2;
-                return result;
+                break;
             case '^':
                 if ((number1 <= 0 && (number2 > 0 && number2 < 1)
                         || (number1 == 0 && number2 < 0))) {
                     throw new ArithmeticException("Operation is impossible");
                 }
                 result = Math.pow(number1, number2);
-                return result;
+                break;
             default:
                 throw new IllegalArgumentException("Wrong operation");
         }
-    }
-
-    private static void isInfinity(double value) {
-        if (value == Double.POSITIVE_INFINITY || value == Double.NEGATIVE_INFINITY) {
+        if (result == Double.POSITIVE_INFINITY || result == Double.NEGATIVE_INFINITY) {
             throw new ArithmeticException("Infinity");
         }
+        return result;
     }
 }
