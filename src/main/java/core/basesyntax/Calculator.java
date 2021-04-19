977dc875c43b8cc870df348a5f3b4baa313860f8
Andrii Romaniuk <romaniukandrii7@gmail.com>
2021-04-19 13:04:06 +0300

    suggestion change

--- src/main/java/core/basesyntax/Calculator.java
+++ src/main/java/core/basesyntax/Calculator.java
@@ -29,22 +29,22 @@ public class Calculator {
     private void checkNumbers(double number1, double number2, char operationType) {
         switch (operationType) {
             case ('+'):
-                if (number1 + number2 == Double.MAX_VALUE
-                        || number1 + number2 == -Double.MAX_VALUE) {
+                if (number1 + number2 >= Double.MAX_VALUE
+                        || number1 + number2 <= -Double.MAX_VALUE) {
                     throw new LimitDoubleValueReachedException("Double limit reached, "
                             + "calculations may not be correct");
                 }
                 break;
             case ('-'):
-                if (number1 - number2 == Double.MAX_VALUE
-                        || number1 - number2 == -Double.MAX_VALUE) {
+                if (number1 - number2 >= Double.MAX_VALUE
+                        || number1 - number2 <= -Double.MAX_VALUE) {
                     throw new LimitDoubleValueReachedException("Double limit reached, "
                             + "calculations may not be correct");
                 }
                 break;
             case ('*'):
-                if (number1 * number2 == Double.POSITIVE_INFINITY
-                        || number1 * number2 == Double.NEGATIVE_INFINITY) {
+                if (number1 * number2 >= Double.POSITIVE_INFINITY
+                        || number1 * number2 <= Double.NEGATIVE_INFINITY) {
                     throw new LimitDoubleValueReachedException("Double limit reached, "
                             + "calculations may not be correct");
                 }
@@ -53,15 +53,15 @@ public class Calculator {
                 if (number2 == 0) {
                     throw new ArithmeticException();
                 }
-                if (number1 / number2 == Double.POSITIVE_INFINITY
-                        || number1 / number2 == Double.NEGATIVE_INFINITY) {
+                if (number1 / number2 >= Double.POSITIVE_INFINITY
+                        || number1 / number2 <= Double.NEGATIVE_INFINITY) {
                     throw new LimitDoubleValueReachedException("Double limit reached, "
                             + "calculations may not be correct");
                 }
                 break;
             case ('^'):
-                if (Math.pow(number1, number2) == Double.MAX_VALUE
-                        || (Math.pow(number1, number2) == -Double.MAX_VALUE)) {
+                if (Math.pow(number1, number2) >= Double.MAX_VALUE
+                        || (Math.pow(number1, number2) <= -Double.MAX_VALUE)) {
                     throw new LimitDoubleValueReachedException("Double limit reached, "
                             + "calculations may not be correct");
                 }
