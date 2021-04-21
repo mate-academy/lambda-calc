d0d2d7e0ccfff761de3efb286d775093978ede1e
Stanislav Aleksandrovych <astanislav2361@gmail.com>
2021-04-21 19:36:44 +0300

    fixed checkstyle

--- src/test/java/core/basesyntax/CalculatorTest.java
+++ src/test/java/core/basesyntax/CalculatorTest.java
@@ -1,9 +1,10 @@
 package core.basesyntax;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
     private static final char PLUS = '+';
