68912aac1f1450b64be1d9491af5521f4b6be55a
dant0n <dmibuc@gmail.com>
2021-04-21 16:03:18 +0300

    Make operands and delta constants.

--- src/test/java/core/basesyntax/CalculatorTest.java
+++ src/test/java/core/basesyntax/CalculatorTest.java
@@ -14,222 +14,220 @@ class CalculatorTest {
     public static final char DIVISION_OPERATOR = '/';
     public static final char EXPONENT_OPERATOR = '^';
     public static final char ILLEGAL_OPERATOR = 'h';
+    private static final double FIRST_OPERAND = 123.4567;
+    private static final double SECOND_OPERAND = 8910.12345;
+    private static final double DELTA = 0.0001;
     private static Calculator calculator;
-    private static double firstOperand;
-    private static double secondOperand;
-    private static double delta;
     private double actual;
     private double expected;
 
     @BeforeAll
     static void beforeAll() {
         calculator = new Calculator();
-        firstOperand = 123.4567;
-        secondOperand = 8910.12345;
-        delta = 0.0001;
+
     }
 
     @Test
     void addition_twoPositives_Ok() {
-        expected = firstOperand + secondOperand;
-        actual = calculator.calculate(firstOperand, secondOperand, SUM_OPERATOR);
-        assertEquals(expected, actual, delta);
+        expected = FIRST_OPERAND + SECOND_OPERAND;
+        actual = calculator.calculate(FIRST_OPERAND, SECOND_OPERAND, SUM_OPERATOR);
+        assertEquals(expected, actual, DELTA);
     }
 
     @Test
     void addition_twoNegatives_Ok() {
-        expected = -firstOperand + -secondOperand;
-        actual = calculator.calculate(-firstOperand, -secondOperand, SUM_OPERATOR);
-        assertEquals(expected, actual, delta);
+        expected = -FIRST_OPERAND + -SECOND_OPERAND;
+        actual = calculator.calculate(-FIRST_OPERAND, -SECOND_OPERAND, SUM_OPERATOR);
+        assertEquals(expected, actual, DELTA);
     }
 
     @Test
     void addition_PositiveAndNegative_Ok() {
-        double expected = -firstOperand + secondOperand;
-        double actual = calculator.calculate(-firstOperand, secondOperand, SUM_OPERATOR);
-        assertEquals(expected, actual, delta);
+        double expected = -FIRST_OPERAND + SECOND_OPERAND;
+        double actual = calculator.calculate(-FIRST_OPERAND, SECOND_OPERAND, SUM_OPERATOR);
+        assertEquals(expected, actual, DELTA);
     }
 
     @Test
     void addition_zeroValue_Ok() {
-        expected = firstOperand + ZERO_OPERAND;
-        actual = calculator.calculate(firstOperand, ZERO_OPERAND, SUM_OPERATOR);
-        assertEquals(expected, actual, delta);
+        expected = FIRST_OPERAND + ZERO_OPERAND;
+        actual = calculator.calculate(FIRST_OPERAND, ZERO_OPERAND, SUM_OPERATOR);
+        assertEquals(expected, actual, DELTA);
 
-        expected = ZERO_OPERAND + secondOperand;
-        actual = calculator.calculate(ZERO_OPERAND, secondOperand, SUM_OPERATOR);
-        assertEquals(expected, actual, delta);
+        expected = ZERO_OPERAND + SECOND_OPERAND;
+        actual = calculator.calculate(ZERO_OPERAND, SECOND_OPERAND, SUM_OPERATOR);
+        assertEquals(expected, actual, DELTA);
     }
 
     @Test
     void addition_maxValues_Ok() {
-        expected = Double.MAX_VALUE + secondOperand;
-        actual = calculator.calculate(Double.MAX_VALUE, secondOperand, SUM_OPERATOR);
-        assertEquals(expected, actual, delta);
+        expected = Double.MAX_VALUE + SECOND_OPERAND;
+        actual = calculator.calculate(Double.MAX_VALUE, SECOND_OPERAND, SUM_OPERATOR);
+        assertEquals(expected, actual, DELTA);
     }
 
     @Test
     void subtraction_twoPositives_Ok() {
-        expected = firstOperand - secondOperand;
-        actual = calculator.calculate(firstOperand, secondOperand, SUBTRACTION_OPERATOR);
-        assertEquals(expected, actual, delta);
+        expected = FIRST_OPERAND - SECOND_OPERAND;
+        actual = calculator.calculate(FIRST_OPERAND, SECOND_OPERAND, SUBTRACTION_OPERATOR);
+        assertEquals(expected, actual, DELTA);
     }
 
     @Test
     void subtraction_twoNegatives_Ok() {
-        expected = -firstOperand - -secondOperand;
-        actual = calculator.calculate(-firstOperand, -secondOperand, SUBTRACTION_OPERATOR);
-        assertEquals(expected, actual, delta);
+        expected = -FIRST_OPERAND - -SECOND_OPERAND;
+        actual = calculator.calculate(-FIRST_OPERAND, -SECOND_OPERAND, SUBTRACTION_OPERATOR);
+        assertEquals(expected, actual, DELTA);
     }
 
     @Test
     void subtraction_PositiveAndNegative_Ok() {
-        expected = firstOperand - -secondOperand;
-        actual = calculator.calculate(firstOperand, -secondOperand, SUBTRACTION_OPERATOR);
-        assertEquals(expected, actual, delta);
+        expected = FIRST_OPERAND - -SECOND_OPERAND;
+        actual = calculator.calculate(FIRST_OPERAND, -SECOND_OPERAND, SUBTRACTION_OPERATOR);
+        assertEquals(expected, actual, DELTA);
     }
 
     @Test
     void subtraction_zeroValue_Ok() {
-        expected = firstOperand - ZERO_OPERAND;
-        actual = calculator.calculate(firstOperand, ZERO_OPERAND, SUBTRACTION_OPERATOR);
-        assertEquals(expected, actual, delta);
+        expected = FIRST_OPERAND - ZERO_OPERAND;
+        actual = calculator.calculate(FIRST_OPERAND, ZERO_OPERAND, SUBTRACTION_OPERATOR);
+        assertEquals(expected, actual, DELTA);
 
-        expected = ZERO_OPERAND - secondOperand;
-        actual = calculator.calculate(ZERO_OPERAND, secondOperand, SUBTRACTION_OPERATOR);
-        assertEquals(expected, actual, delta);
+        expected = ZERO_OPERAND - SECOND_OPERAND;
+        actual = calculator.calculate(ZERO_OPERAND, SECOND_OPERAND, SUBTRACTION_OPERATOR);
+        assertEquals(expected, actual, DELTA);
     }
 
     @Test
     void subtraction_maxValues_Ok() {
-        expected = Double.MAX_VALUE - secondOperand;
-        actual = calculator.calculate(Double.MAX_VALUE, secondOperand, SUBTRACTION_OPERATOR);
-        assertEquals(expected, actual, delta);
+        expected = Double.MAX_VALUE - SECOND_OPERAND;
+        actual = calculator.calculate(Double.MAX_VALUE, SECOND_OPERAND, SUBTRACTION_OPERATOR);
+        assertEquals(expected, actual, DELTA);
     }
 
     @Test
     void multiplication_twoPositives_Ok() {
-        expected = firstOperand * secondOperand;
-        actual = calculator.calculate(firstOperand, secondOperand, MULTIPLICATION_OPERATOR);
-        assertEquals(expected, actual, delta);
+        expected = FIRST_OPERAND * SECOND_OPERAND;
+        actual = calculator.calculate(FIRST_OPERAND, SECOND_OPERAND, MULTIPLICATION_OPERATOR);
+        assertEquals(expected, actual, DELTA);
     }
 
     @Test
     void multiplication_twoNegatives_Ok() {
-        expected = -firstOperand * -secondOperand;
-        actual = calculator.calculate(firstOperand, secondOperand, MULTIPLICATION_OPERATOR);
-        assertEquals(expected, actual, delta);
+        expected = -FIRST_OPERAND * -SECOND_OPERAND;
+        actual = calculator.calculate(FIRST_OPERAND, SECOND_OPERAND, MULTIPLICATION_OPERATOR);
+        assertEquals(expected, actual, DELTA);
     }
 
     @Test
     void multiplication_PositiveAndNegative_Ok() {
-        expected = -firstOperand * secondOperand;
-        actual = calculator.calculate(-firstOperand, secondOperand, MULTIPLICATION_OPERATOR);
-        assertEquals(expected, actual, delta);
+        expected = -FIRST_OPERAND * SECOND_OPERAND;
+        actual = calculator.calculate(-FIRST_OPERAND, SECOND_OPERAND, MULTIPLICATION_OPERATOR);
+        assertEquals(expected, actual, DELTA);
     }
 
     @Test
     void multiplication_zeroValue_Ok() {
-        expected = firstOperand * ZERO_OPERAND;
-        actual = calculator.calculate(firstOperand, ZERO_OPERAND, MULTIPLICATION_OPERATOR);
-        assertEquals(expected, actual, delta);
+        expected = FIRST_OPERAND * ZERO_OPERAND;
+        actual = calculator.calculate(FIRST_OPERAND, ZERO_OPERAND, MULTIPLICATION_OPERATOR);
+        assertEquals(expected, actual, DELTA);
     }
 
     @Test
     void multiplication_maxValues_Ok() {
-        expected = Double.MAX_VALUE * secondOperand;
-        actual = calculator.calculate(Double.MAX_VALUE, secondOperand, MULTIPLICATION_OPERATOR);
-        assertEquals(expected, actual, delta);
+        expected = Double.MAX_VALUE * SECOND_OPERAND;
+        actual = calculator.calculate(Double.MAX_VALUE, SECOND_OPERAND, MULTIPLICATION_OPERATOR);
+        assertEquals(expected, actual, DELTA);
     }
 
     @Test
     void division_twoPositives_Ok() {
-        expected = firstOperand / secondOperand;
-        actual = calculator.calculate(firstOperand, secondOperand, DIVISION_OPERATOR);
-        assertEquals(expected, actual, delta);
+        expected = FIRST_OPERAND / SECOND_OPERAND;
+        actual = calculator.calculate(FIRST_OPERAND, SECOND_OPERAND, DIVISION_OPERATOR);
+        assertEquals(expected, actual, DELTA);
     }
 
     @Test
     void division_twoNegatives_Ok() {
-        expected = -firstOperand / -secondOperand;
-        actual = calculator.calculate(-firstOperand, -secondOperand, DIVISION_OPERATOR);
-        assertEquals(expected, actual, delta);
+        expected = -FIRST_OPERAND / -SECOND_OPERAND;
+        actual = calculator.calculate(-FIRST_OPERAND, -SECOND_OPERAND, DIVISION_OPERATOR);
+        assertEquals(expected, actual, DELTA);
     }
 
     @Test
     void division_PositiveAndNegative_Ok() {
-        expected = -firstOperand / secondOperand;
-        actual = calculator.calculate(-firstOperand, secondOperand, DIVISION_OPERATOR);
-        assertEquals(expected, actual, delta);
+        expected = -FIRST_OPERAND / SECOND_OPERAND;
+        actual = calculator.calculate(-FIRST_OPERAND, SECOND_OPERAND, DIVISION_OPERATOR);
+        assertEquals(expected, actual, DELTA);
     }
 
     @Test
     void division_zeroValue_Ok() {
-        expected = ZERO_OPERAND / secondOperand;
-        actual = calculator.calculate(ZERO_OPERAND, secondOperand, DIVISION_OPERATOR);
-        assertEquals(expected, actual, delta);
+        expected = ZERO_OPERAND / SECOND_OPERAND;
+        actual = calculator.calculate(ZERO_OPERAND, SECOND_OPERAND, DIVISION_OPERATOR);
+        assertEquals(expected, actual, DELTA);
     }
 
     @Test
     void division_zeroValue_NotOk() {
         assertThrows(ArithmeticException.class,
-                () -> calculator.calculate(firstOperand, ZERO_OPERAND, DIVISION_OPERATOR));
+                () -> calculator.calculate(FIRST_OPERAND, ZERO_OPERAND, DIVISION_OPERATOR));
     }
 
     @Test
     void division_maxValues_Ok() {
-        expected = Double.MAX_VALUE / secondOperand;
-        actual = calculator.calculate(Double.MAX_VALUE, secondOperand, DIVISION_OPERATOR);
-        assertEquals(expected, actual, delta);
+        expected = Double.MAX_VALUE / SECOND_OPERAND;
+        actual = calculator.calculate(Double.MAX_VALUE, SECOND_OPERAND, DIVISION_OPERATOR);
+        assertEquals(expected, actual, DELTA);
     }
 
     @Test
     void raise_ToPositivePower_Ok() {
-        expected = Math.pow(firstOperand, secondOperand);
-        actual = calculator.calculate(firstOperand, secondOperand, EXPONENT_OPERATOR);
-        assertEquals(expected, actual, delta);
+        expected = Math.pow(FIRST_OPERAND, SECOND_OPERAND);
+        actual = calculator.calculate(FIRST_OPERAND, SECOND_OPERAND, EXPONENT_OPERATOR);
+        assertEquals(expected, actual, DELTA);
 
-        expected = Math.pow(-firstOperand, secondOperand);
-        actual = calculator.calculate(-firstOperand, secondOperand, EXPONENT_OPERATOR);
-        assertEquals(expected, actual, delta);
+        expected = Math.pow(-FIRST_OPERAND, SECOND_OPERAND);
+        actual = calculator.calculate(-FIRST_OPERAND, SECOND_OPERAND, EXPONENT_OPERATOR);
+        assertEquals(expected, actual, DELTA);
     }
 
     @Test
     void raise_ToNegativePower_Ok() {
-        expected = Math.pow(firstOperand, -secondOperand);
-        actual = calculator.calculate(firstOperand, -secondOperand, EXPONENT_OPERATOR);
-        assertEquals(expected, actual, delta);
+        expected = Math.pow(FIRST_OPERAND, -SECOND_OPERAND);
+        actual = calculator.calculate(FIRST_OPERAND, -SECOND_OPERAND, EXPONENT_OPERATOR);
+        assertEquals(expected, actual, DELTA);
 
-        expected = Math.pow(-firstOperand, -secondOperand);
-        actual = calculator.calculate(-firstOperand, -secondOperand, EXPONENT_OPERATOR);
-        assertEquals(expected, actual, delta);
+        expected = Math.pow(-FIRST_OPERAND, -SECOND_OPERAND);
+        actual = calculator.calculate(-FIRST_OPERAND, -SECOND_OPERAND, EXPONENT_OPERATOR);
+        assertEquals(expected, actual, DELTA);
     }
 
     @Test
     void raise_ToZeroPower_Ok() {
-        expected = Math.pow(firstOperand, ZERO_OPERAND);
-        actual = calculator.calculate(firstOperand, ZERO_OPERAND, EXPONENT_OPERATOR);
-        assertEquals(expected, actual, delta);
+        expected = Math.pow(FIRST_OPERAND, ZERO_OPERAND);
+        actual = calculator.calculate(FIRST_OPERAND, ZERO_OPERAND, EXPONENT_OPERATOR);
+        assertEquals(expected, actual, DELTA);
 
-        expected = Math.pow(-firstOperand, ZERO_OPERAND);
-        actual = calculator.calculate(-firstOperand, ZERO_OPERAND, EXPONENT_OPERATOR);
-        assertEquals(expected, actual, delta);
+        expected = Math.pow(-FIRST_OPERAND, ZERO_OPERAND);
+        actual = calculator.calculate(-FIRST_OPERAND, ZERO_OPERAND, EXPONENT_OPERATOR);
+        assertEquals(expected, actual, DELTA);
     }
 
     @Test
     void raise_ZeroToPower_Ok() {
-        expected = Math.pow(ZERO_OPERAND, secondOperand);
-        actual = calculator.calculate(ZERO_OPERAND, secondOperand, EXPONENT_OPERATOR);
-        assertEquals(expected, actual, delta);
+        expected = Math.pow(ZERO_OPERAND, SECOND_OPERAND);
+        actual = calculator.calculate(ZERO_OPERAND, SECOND_OPERAND, EXPONENT_OPERATOR);
+        assertEquals(expected, actual, DELTA);
 
-        expected = Math.pow(ZERO_OPERAND, secondOperand);
-        actual = calculator.calculate(ZERO_OPERAND, secondOperand, EXPONENT_OPERATOR);
-        assertEquals(expected, actual, delta);
+        expected = Math.pow(ZERO_OPERAND, SECOND_OPERAND);
+        actual = calculator.calculate(ZERO_OPERAND, SECOND_OPERAND, EXPONENT_OPERATOR);
+        assertEquals(expected, actual, DELTA);
     }
 
     @Test
     void calculate_illegalOperation_notOk() {
         assertThrows(IllegalArgumentException.class,
-                () -> calculator.calculate(firstOperand, secondOperand, ILLEGAL_OPERATOR));
+                () -> calculator.calculate(FIRST_OPERAND, SECOND_OPERAND, ILLEGAL_OPERATOR));
     }
 }
