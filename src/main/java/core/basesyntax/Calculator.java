80b759f49033c5dff73e9d073ecb8baf283664aa
Koziarevskyi <koziarevskyi@gmail.com>
2021-04-22 15:12:25 +0300

    C005 - Some more fixes.

--- src/main/java/core/basesyntax/Calculator.java
+++ src/main/java/core/basesyntax/Calculator.java
@@ -10,7 +10,7 @@ public class Calculator {
             case '*':
                 return firstOperand * secondOperand;
             case '/':
-                if (secondOperand == 0) {
+                if (secondOperand == 0.0) {
                     throw new ArithmeticException("You can't divide by 0");
                 }
                 return firstOperand / secondOperand;
