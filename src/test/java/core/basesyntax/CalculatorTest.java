479e6f8250e81c2bf131730f7709eb5fb217ca91
the5tranger <mishaantonyuk00@gmail.com>
2021-04-22 11:50:48 +0300

    Test_fixed v 1.1

--- src/test/java/core/basesyntax/CalculatorTest.java
+++ src/test/java/core/basesyntax/CalculatorTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import core.basesyntax.exception.DivisionByZeroException;
 import core.basesyntax.exception.NotSupportOperationException;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 class CalculatorTest {
@@ -15,56 +15,56 @@ class CalculatorTest {
     private static final char MULTIPLICATION = '*';
     private static final char TO_POWER = '^';
     private static final double DELTA = 0.0001;
+    private static Calculator calculator;
     private static final double FIRST_POSITIVE = 16.234;
     private static final double SECOND_POSITIVE = 15.234;
     private static final double FIRST_NEGATIVE = -16.234;
     private static final double SECOND_NEGATIVE = -15.234;
     private static final double ZERO = 0;
-    private Calculator calculator;
 
-    @BeforeEach
-    void setUp() {
+    @BeforeAll
+    static void beforeAll() {
         calculator = new Calculator();
     }
 
     @Test
     void twoPositiveAddition_OK() {
-        double expected = FIRST_POSITIVE + SECOND_POSITIVE;
+        double expected = 31.468;
         assertEquals(expected, calculator.calculate(FIRST_POSITIVE, SECOND_POSITIVE,
                 ADDITION), DELTA);
     }
 
     @Test
     void twoNegativeAddition_OK() {
-        double expected = FIRST_NEGATIVE + SECOND_NEGATIVE;
+        double expected = -31.468;
         assertEquals(expected, calculator.calculate(FIRST_NEGATIVE, SECOND_NEGATIVE,
                 ADDITION), DELTA);
     }
 
     @Test
         void firstNegativeAndPositiveAddition_OK() {
-        double expected = FIRST_NEGATIVE + SECOND_POSITIVE;
+        double expected = -1.0;
         assertEquals(expected, calculator.calculate(FIRST_NEGATIVE, SECOND_POSITIVE,
                 ADDITION), DELTA);
     }
 
     @Test
     void secondNegativeAndPositiveAddition_OK() {
-        double expected = FIRST_POSITIVE + SECOND_NEGATIVE;
+        double expected = 1.0;
         assertEquals(expected, calculator.calculate(FIRST_POSITIVE, SECOND_NEGATIVE,
                 ADDITION), DELTA);
     }
 
     @Test
     void firstZeroAddition_OK() {
-        double expected = ZERO + SECOND_POSITIVE;
+        double expected = 15.234;
         assertEquals(expected, calculator.calculate(ZERO, SECOND_POSITIVE,
                 ADDITION), DELTA);
     }
 
     @Test
     void secondZeroAddition_OK() {
-        double expected = FIRST_POSITIVE + ZERO;
+        double expected = 16.234;
         assertEquals(expected, calculator.calculate(FIRST_POSITIVE, ZERO, ADDITION), DELTA);
     }
 
@@ -75,42 +75,42 @@ class CalculatorTest {
 
     @Test
     void twoPositiveSubtraction_OK() {
-        double expected = FIRST_POSITIVE - SECOND_POSITIVE;
+        double expected = 1.0;
         assertEquals(expected, calculator.calculate(FIRST_POSITIVE, SECOND_POSITIVE,
                 SUBTRACTION), DELTA);
     }
 
     @Test
     void twoNegativeSubtraction_OK() {
-        double expected = FIRST_NEGATIVE - SECOND_NEGATIVE;
+        double expected = -1.0;
         assertEquals(expected, calculator.calculate(FIRST_NEGATIVE, SECOND_NEGATIVE,
                 SUBTRACTION), DELTA);
     }
 
     @Test
     void firstNegativeAndPositiveSubtraction_OK() {
-        double expected = FIRST_NEGATIVE - SECOND_POSITIVE;
+        double expected = -31.468;
         assertEquals(expected, calculator.calculate(FIRST_NEGATIVE, SECOND_POSITIVE,
                 SUBTRACTION), DELTA);
     }
 
     @Test
     void secondNegativeAndPositiveSubtraction_OK() {
-        double expected = FIRST_POSITIVE - SECOND_NEGATIVE;
+        double expected = 31.468;
         assertEquals(expected, calculator.calculate(FIRST_POSITIVE, SECOND_NEGATIVE,
                 SUBTRACTION), DELTA);
     }
 
     @Test
     void firstZeroSubtraction_OK() {
-        double expected = ZERO - SECOND_POSITIVE;
+        double expected = -15.234;
         assertEquals(expected, calculator.calculate(ZERO, SECOND_POSITIVE,
                 SUBTRACTION), DELTA);
     }
 
     @Test
     void secondZeroSubtraction_OK() {
-        double expected = FIRST_POSITIVE - ZERO;
+        double expected = 16.234;
         assertEquals(expected, calculator.calculate(FIRST_POSITIVE, ZERO,
                 SUBTRACTION), DELTA);
     }
@@ -123,28 +123,28 @@ class CalculatorTest {
 
     @Test
     void twoPositiveDivision_OK() {
-        double expected = FIRST_POSITIVE / SECOND_POSITIVE;
+        double expected = 1.0656;
         assertEquals(expected, calculator.calculate(FIRST_POSITIVE, SECOND_POSITIVE,
                 DIVISION), DELTA);
     }
 
     @Test
     void twoNegativeDivision_OK() {
-        double expected = FIRST_NEGATIVE / SECOND_NEGATIVE;
+        double expected = 1.0656;
         assertEquals(expected, calculator.calculate(FIRST_NEGATIVE, SECOND_NEGATIVE,
                 DIVISION), DELTA);
     }
 
     @Test
     void firstNegativeAndPositiveDivision_OK() {
-        double expected = FIRST_NEGATIVE / SECOND_POSITIVE;
+        double expected = -1.0656;
         assertEquals(expected, calculator.calculate(FIRST_NEGATIVE, SECOND_POSITIVE,
                 DIVISION), DELTA);
     }
 
     @Test
     void secondNegativeAndPositiveDivision_OK() {
-        double expected = FIRST_POSITIVE / SECOND_NEGATIVE;
+        double expected = -1.0656;
         assertEquals(expected, calculator.calculate(FIRST_POSITIVE, SECOND_NEGATIVE,
                 DIVISION), DELTA);
     }
@@ -168,28 +168,28 @@ class CalculatorTest {
 
     @Test
     void twoPositiveMultiplication_OK() {
-        double expected = FIRST_POSITIVE * SECOND_POSITIVE;
+        double expected = 247.3087;
         assertEquals(expected, calculator.calculate(FIRST_POSITIVE, SECOND_POSITIVE,
                 MULTIPLICATION), DELTA);
     }
 
     @Test
     void twoNegativeMultiplication_OK() {
-        double expected = FIRST_NEGATIVE * SECOND_NEGATIVE;
+        double expected = 247.3087;
         assertEquals(expected, calculator.calculate(FIRST_NEGATIVE, SECOND_NEGATIVE,
                 MULTIPLICATION), DELTA);
     }
 
     @Test
     void firstNegativeAndPositiveMultiplication_OK() {
-        double expected = FIRST_NEGATIVE * SECOND_POSITIVE;
+        double expected = -247.3087;
         assertEquals(expected, calculator.calculate(FIRST_NEGATIVE, SECOND_POSITIVE,
                 MULTIPLICATION), DELTA);
     }
 
     @Test
     void secondNegativeAndPositiveMultiplication_OK() {
-        double expected = FIRST_POSITIVE * SECOND_NEGATIVE;
+        double expected = -247.3087;
         assertEquals(expected, calculator.calculate(FIRST_POSITIVE, SECOND_NEGATIVE,
                 MULTIPLICATION), DELTA);
     }
@@ -243,20 +243,20 @@ class CalculatorTest {
 
     @Test
     void secondZeroToPower_OK() {
-        double expected = Math.pow(SECOND_POSITIVE, ZERO);
+        double expected = 1.0;
         assertEquals(expected, calculator.calculate(SECOND_POSITIVE, ZERO, TO_POWER));
     }
 
     @Test
     void twoZeroToPower_OK() {
-        double expected = Math.pow(ZERO, ZERO);
+        double expected = 1.0;
         assertEquals(expected, calculator.calculate(ZERO, ZERO, TO_POWER));
     }
 
     @Test
     void notSupportedOperation_NotOK() {
-        char operation = '$';
+        char illegalOperation = '$';
         assertThrows(NotSupportOperationException.class, () ->
-                calculator.calculate(ZERO, ZERO, operation));
+                calculator.calculate(ZERO, ZERO, illegalOperation));
     }
 }
