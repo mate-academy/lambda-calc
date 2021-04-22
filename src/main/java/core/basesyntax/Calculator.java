e80ed72a35967c6df0f9675355bc7a56f93770f7
DeStinyShell <dmitriy4ever97@gmail.com>
2021-04-22 13:37:18 +0300

    Replaced if with switch.

--- src/main/java/core/basesyntax/Calculator.java
+++ src/main/java/core/basesyntax/Calculator.java
@@ -3,30 +3,33 @@ package core.basesyntax;
 public class Calculator {
 
     public double calculate(double firstNumber, double secondNumber, char operation) {
-        if (operation == '^') {
-            if (firstNumber < 0 && secondNumber - (int) secondNumber != 0) {
-                throw new ArithmeticException("You must divide on integer number.");
-            }
-            if (secondNumber < 0) {
-                return 1 / Math.pow(firstNumber, secondNumber * -1);
-            }
-            return Math.pow(firstNumber, secondNumber);
+        switch (operation) {
+            case '^':
+                if (firstNumber < 0 && secondNumber - (int) secondNumber != 0) {
+                    throw new ArithmeticException("You must divide on integer number.");
+                }
+                if (secondNumber < 0) {
+                    return 1 / Math.pow(firstNumber, secondNumber * -1);
+                }
+                return Math.pow(firstNumber, secondNumber);
+
+            case '+':
+                return firstNumber + secondNumber;
+
+            case '-':
+                return firstNumber - secondNumber;
+
+            case '*':
+                return firstNumber * secondNumber;
+
+            case '/':
+                if (secondNumber == 0) {
+                    throw new ArithmeticException("You can`t divide by zero");
+                }
+                return firstNumber / secondNumber;
+
+            default:
+                throw new RuntimeException("Invalid operation" + operation);
         }
-        if (operation == '+') {
-            return firstNumber + secondNumber;
-        }
-        if (operation == '-') {
-            return firstNumber - secondNumber;
-        }
-        if (operation == '*') {
-            return firstNumber * secondNumber;
-        }
-        if (operation == '/') {
-            if (secondNumber == 0) {
-                throw new ArithmeticException("You can`t divide by zero");
-            }
-            return firstNumber / secondNumber;
-        }
-        throw new RuntimeException("Invalid operation" + operation);
     }
 }
