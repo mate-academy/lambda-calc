3140544002ec6cd2a4d16d5eb23b07aa5244ee34
docer <vlasovengineer@gmail.com>
2021-06-24 18:45:26 +0300

    dell private methods in LambdaCalc

--- src/main/java/core/basesyntax/LambdaCalc.java
+++ src/main/java/core/basesyntax/LambdaCalc.java
@@ -10,40 +10,20 @@ public class LambdaCalc {
     public double calculate(double firstNumber, double secondNumber, char operation) {
         switch (operation) {
             case ADDITION:
-                return calculatorAddition(firstNumber, secondNumber);
+                return firstNumber + secondNumber;
             case SUBTRACTION:
-                return calculatorSubtraction(firstNumber, secondNumber);
+                return firstNumber - secondNumber;
             case MULTIPLICATION:
-                return calculatorMultiplication(firstNumber, secondNumber);
+                return firstNumber * secondNumber;
             case DIVISION:
-                return calculatorDivision(firstNumber, secondNumber);
+                if (secondNumber == 0) {
+                    throw new ArithmeticException("Division by zero is prohibited!");
+                }
+                return firstNumber / secondNumber;
             case POWER:
-                return calculatorPower(firstNumber, secondNumber);
+                return Math.pow(firstNumber, secondNumber);
             default:
                 throw new IllegalArgumentException("Operation don't exist!");
         }
-    }
-
-    private double calculatorAddition(double firstNumber, double secondNumber) {
-        return firstNumber + secondNumber;
-    }
-
-    private double calculatorSubtraction(double firstNumber, double secondNumber) {
-        return firstNumber - secondNumber;
-    }
-
-    private double calculatorMultiplication(double firstNumber, double secondNumber) {
-        return firstNumber * secondNumber;
-    }
-
-    private double calculatorDivision(double firstNumber, double secondNumber) {
-        if (secondNumber == 0) {
-            throw new ArithmeticException("Division by zero is prohibited!");
-        }
-        return firstNumber / secondNumber;
-    }
-
-    private double calculatorPower(double firstNumber, double secondNumber) {
-        return Math.pow(firstNumber, secondNumber);
     }
 }
