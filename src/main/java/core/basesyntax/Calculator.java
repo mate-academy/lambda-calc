6da3eecf001e0afcfc20905c48011a371ce4e0eb
nikdagaev <nikdagaev@gmail.com>
2021-06-24 18:42:36 +0300

    Calculator and CalculatorTest classes were added and implemented.

---
+++ src/main/java/core/basesyntax/Calculator.java
@@ -0,0 +1,34 @@
+package core.basesyntax;
+
+public class Calculator {
+    private static final char ADDITION = '+';
+    private static final char SUBTRACTION = '-';
+    private static final char DIVISION = '/';
+    private static final char MULTIPLICATION = '*';
+    private static final char POWER = '^';
+
+    public double calculate(double firstOperand, double secondOperand, char operation) {
+        if (operation == ADDITION) {
+            return firstOperand + secondOperand;
+        }
+        if (operation == SUBTRACTION) {
+            return firstOperand - secondOperand;
+        }
+        if (operation == MULTIPLICATION) {
+            return firstOperand * secondOperand;
+        }
+        if (operation == DIVISION) {
+            if (secondOperand == 0) {
+                throw new ArithmeticException("Can't divide by zero!");
+            }
+            return firstOperand / secondOperand;
+        }
+        if (operation == POWER) {
+            if (firstOperand == 0 && secondOperand < 0) {
+                throw new ArithmeticException("Zero can't have negative power!");
+            }
+            return Math.pow(firstOperand, secondOperand);
+        }
+        throw new UnsupportedOperationException("This operation is unsupported!");
+    }
+}
