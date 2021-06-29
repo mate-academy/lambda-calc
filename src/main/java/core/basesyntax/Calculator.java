24792038630aebd32f7741c976ab0005d04299f6
Vova Bilodon <vovabilodon@gmail.com>
2021-06-29 13:28:17 +0300

    added constants for operations, removed all "expected" variables

--- src/main/java/core/basesyntax/Calculator.java
+++ src/main/java/core/basesyntax/Calculator.java
@@ -1,20 +1,26 @@
 package core.basesyntax;
 
 public class Calculator {
+    private static final char ADDITION = '+';
+    private static final char SUBTRACTION = '-';
+    private static final char MULTIPLICATION = '*';
+    private static final char DIVIDING = '/';
+    private static final char RAISING_TO_POWER = '^';
+
     public double calculate(double first, double second, char operation) {
         switch (operation) {
-            case '+' :
+            case ADDITION:
                 return first + second;
-            case '-' :
+            case SUBTRACTION:
                 return first - second;
-            case '/' :
+            case DIVIDING:
                 if (second == 0) {
                     throw new ArithmeticException("/ by zero");
                 }
                 return first / second;
-            case '*' :
+            case MULTIPLICATION:
                 return first * second;
-            case '^' :
+            case RAISING_TO_POWER:
                 return Math.pow(first, second);
             default :
                 throw new RuntimeException("Unknown operation");
--- src/test/java/core/basesyntax/CalculatorTest.java
+++ src/test/java/core/basesyntax/CalculatorTest.java
@@ -15,214 +15,241 @@ class CalculatorTest {
     @BeforeAll
     static void beforeAll() {
         calculator = new Calculator();
-        first = 12.0;
-        second = 5.0;
+        first = 1211.0;
+        second = 565.0;
     }
 
     @Test
     void illegalOperation_NotOk() {
-        assertThrows(RuntimeException.class, () -> {
-            calculator.calculate(first, second, '$');
-        });
+        assertThrows(RuntimeException.class, () -> calculator.calculate(first, second, '$'));
     }
 
     @Test
     void addingTwoPositive_Ok() {
-        double expected = first + second;
-        assertEquals(expected, calculator.calculate(first, second, '+'), DELTA);
+        assertEquals(first + second,
+                calculator.calculate(first, second, '+'),
+                DELTA);
     }
 
     @Test
     void addingTwoNegative_Ok() {
-        double expected = -first + -second;
-        assertEquals(expected, calculator.calculate(-first, -second, '+'), DELTA);
+        assertEquals(-first + -second,
+                calculator.calculate(-first, -second, '+'),
+                DELTA);
     }
 
     @Test
     void addingNegativeAndPositive_Ok() {
-        double expected = -first + second;
-        assertEquals(expected, calculator.calculate(-first, second, '+'), DELTA);
+        assertEquals(-first + second,
+                calculator.calculate(-first, second, '+'),
+                DELTA);
     }
 
     @Test
     void addingWithZeroAtFirstPlace_Ok() {
-        double expected = 0 + second;
-        assertEquals(expected, calculator.calculate(0, second, '+'), DELTA);
+        assertEquals(0 + second,
+                calculator.calculate(0, second, '+'),
+                DELTA);
     }
 
     @Test
     void addingWithZeroAtSecondPlace_Ok() {
-        double expected = first + 0;
-        assertEquals(expected, calculator.calculate(first, 0, '+'), DELTA);
+        assertEquals(first + 0,
+                calculator.calculate(first, 0, '+'),
+                DELTA);
     }
 
     @Test
     void addingToDoubleMaxValue_Ok() {
-        double expected = Double.MAX_VALUE + second;
-        assertEquals(expected, calculator.calculate(Double.MAX_VALUE, second, '+'), DELTA);
+        assertEquals(Double.MAX_VALUE + second,
+                calculator.calculate(Double.MAX_VALUE, second, '+'),
+                DELTA);
     }
 
     @Test
     void dividingTwoPositive_Ok() {
-        double expected = first / second;
-        assertEquals(expected, calculator.calculate(first, second, '/'), DELTA);
+        assertEquals(first / second,
+                calculator.calculate(first, second, '/'),
+                DELTA);
     }
 
     @Test
     void dividingTwoNegative_Ok() {
-        double expected = -first / -second;
-        assertEquals(expected, calculator.calculate(-first, -second, '/'), DELTA);
+        assertEquals(-first / -second,
+                calculator.calculate(-first, -second, '/'),
+                DELTA);
     }
 
     @Test
     void dividingPositiveAndNegative_Ok() {
-        double expected = first / -second;
-        assertEquals(expected, calculator.calculate(first, -second, '/'), DELTA);
+        assertEquals(first / -second,
+                calculator.calculate(first, -second, '/'),
+                DELTA);
     }
 
     @Test
     void dividingWithZeroAtFirstPlace_Ok() {
-        double expected = 0 / -second;
-        assertEquals(expected, calculator.calculate(0, -second, '/'), DELTA);
+        assertEquals(0 / -second,
+                calculator.calculate(0, -second, '/'),
+                DELTA);
     }
 
     @Test
     void dividingWithZeroAtSecondPlace_NotOk() {
-        assertThrows(ArithmeticException.class, () -> {
-            calculator.calculate(first, 0, '/');
-        });
+        assertThrows(ArithmeticException.class, () -> calculator.calculate(first, 0, '/'));
     }
 
     @Test
     void dividingDoubleMaxValue_Ok() {
-        double expected = Double.MAX_VALUE / second;
-        assertEquals(expected, calculator.calculate(Double.MAX_VALUE, second, '/'), DELTA);
+        assertEquals(Double.MAX_VALUE / second,
+                calculator.calculate(Double.MAX_VALUE, second, '/'),
+                DELTA);
     }
 
     @Test
     void dividingByDoubleMaxValue_Ok() {
-        double expected = first / Double.MAX_VALUE;
-        assertEquals(expected, calculator.calculate(first, Double.MAX_VALUE, '/'), DELTA);
+        assertEquals(first / Double.MAX_VALUE,
+                calculator.calculate(first, Double.MAX_VALUE, '/'),
+                DELTA);
     }
 
     @Test
     void multiplicationTwoPositive_Ok() {
-        double expected = first * second;
-        assertEquals(expected, calculator.calculate(first, second, '*'), DELTA);
+        assertEquals(first * second,
+                calculator.calculate(first, second, '*'),
+                DELTA);
     }
 
     @Test
     void multiplicationTwoNegative_Ok() {
-        double expected = -first * -second;
-        assertEquals(expected, calculator.calculate(-first, -second, '*'), DELTA);
+        assertEquals(-first * -second,
+                calculator.calculate(-first, -second, '*'),
+                DELTA);
     }
 
     @Test
     void multiplicationPositiveAndNegative_Ok() {
-        double expected = first * -second;
-        assertEquals(expected, calculator.calculate(first, -second, '*'), DELTA);
+        assertEquals(first * -second,
+                calculator.calculate(first, -second, '*'),
+                DELTA);
     }
 
     @Test
     void multiplicationWithZeroAtFirstPlace_Ok() {
-        double expected = 0 * second;
-        assertEquals(expected, calculator.calculate(0, second, '*'), DELTA);
+        assertEquals(0 * second,
+                calculator.calculate(0, second, '*'),
+                DELTA);
     }
 
     @Test
     void multiplicationWithZeroAtSecondPlace_Ok() {
-        double expected = first * 0;
-        assertEquals(expected, calculator.calculate(first, 0, '*'), DELTA);
+        assertEquals(first * 0,
+                calculator.calculate(first, 0, '*'),
+                DELTA);
     }
 
     @Test
     void multiplicationDoubleMaxValue_Ok() {
-        char operation = '*';
-        double expected = Double.MAX_VALUE * second;
-        assertEquals(expected, calculator.calculate(Double.MAX_VALUE, second, operation), DELTA);
+        assertEquals(Double.MAX_VALUE * second,
+                calculator.calculate(Double.MAX_VALUE, second, '*'),
+                DELTA);
     }
 
     @Test
     void multiplicationByDoubleMaxValue_Ok() {
-        double expected = first * Double.MAX_VALUE;
-        assertEquals(expected, calculator.calculate(first, Double.MAX_VALUE, '*'), DELTA);
+        assertEquals(first * Double.MAX_VALUE,
+                calculator.calculate(first, Double.MAX_VALUE, '*'),
+                DELTA);
     }
 
     @Test
     void subtractingTwoPositive_Ok() {
-        double expected = first - second;
-        assertEquals(expected, calculator.calculate(first, second, '-'), DELTA);
+        assertEquals(first - second,
+                calculator.calculate(first, second, '-'),
+                DELTA);
     }
 
     @Test
     void subtractingTwoNegative_Ok() {
-        double expected = -first - -second;
-        assertEquals(expected, calculator.calculate(-first, -second, '-'), DELTA);
+        assertEquals(-first - -second,
+                calculator.calculate(-first, -second, '-'),
+                DELTA);
     }
 
     @Test
     void subtractingPositiveAndNegative_Ok() {
-        double expected = first - -second;
-        assertEquals(expected, calculator.calculate(first, -second, '-'), DELTA);
+        assertEquals(first - -second,
+                calculator.calculate(first, -second, '-'),
+                DELTA);
     }
 
     @Test
     void subtractingWithZeroAtFirstPlace_Ok() {
-        double expected = 0 - second;
-        assertEquals(expected, calculator.calculate(0, second, '-'), DELTA);
+        assertEquals(0 - second,
+                calculator.calculate(0, second, '-'),
+                DELTA);
     }
 
     @Test
     void subtractingWithZeroAtSecondPlace_Ok() {
-        double expected = first - 0;
-        assertEquals(expected, calculator.calculate(first, 0, '-'), DELTA);
+        assertEquals(first - 0,
+                calculator.calculate(first, 0, '-'),
+                DELTA);
     }
 
     @Test
     void subtractingFromDoubleMaxValue_Ok() {
-        double expected = Double.MAX_VALUE - second;
-        assertEquals(expected, calculator.calculate(Double.MAX_VALUE, second, '-'), DELTA);
+        assertEquals(Double.MAX_VALUE - second,
+                calculator.calculate(Double.MAX_VALUE, second, '-'),
+                DELTA);
     }
 
     @Test
     void subtractingDoubleMaxValue_Ok() {
-        double expected = first - Double.MAX_VALUE;
-        assertEquals(expected, calculator.calculate(first, Double.MAX_VALUE, '-'), DELTA);
+        assertEquals(first - Double.MAX_VALUE,
+                calculator.calculate(first, Double.MAX_VALUE, '-'),
+                DELTA);
     }
 
     @Test
     void raisingPositiveValueToPositivePower_Ok() {
-        double expected = Math.pow(first, second);
-        assertEquals(expected, calculator.calculate(first, second, '^'), DELTA);
+        assertEquals(Math.pow(first, second),
+                calculator.calculate(first, second, '^'),
+                DELTA);
     }
 
     @Test
     void raisingNegativeValueToPositivePower_Ok() {
-        double expected = Math.pow(-first, second);
-        assertEquals(expected, calculator.calculate(-first, second, '^'), DELTA);
+        assertEquals(Math.pow(-first, second),
+                calculator.calculate(-first, second, '^'),
+                DELTA);
     }
 
     @Test
     void raisingPositiveValueToNegativePower_Ok() {
-        double expected = Math.pow(first, -second);
-        assertEquals(expected, calculator.calculate(first, -second, '^'), DELTA);
+        assertEquals(Math.pow(first, -second),
+                calculator.calculate(first, -second, '^'),
+                DELTA);
     }
 
     @Test
     void raisingNegativeValueToNegativePower_Ok() {
-        double expected = Math.pow(-first, -second);
-        assertEquals(expected, calculator.calculate(-first, -second, '^'), DELTA);
+        assertEquals(Math.pow(-first, -second),
+                calculator.calculate(-first, -second, '^'),
+                DELTA);
     }
 
     @Test
     void raisingPositiveValueToZeroPower_Ok() {
-        double expected = Math.pow(first, 0);
-        assertEquals(expected, calculator.calculate(first, 0, '^'), DELTA);
+        assertEquals(Math.pow(first, 0),
+                calculator.calculate(first, 0, '^'),
+                DELTA);
     }
 
     @Test
     void raisingNegativeValueToZeroPower_Ok() {
-        double expected = Math.pow(-first, 0);
-        assertEquals(expected, calculator.calculate(-first, 0, '^'), DELTA);
+        assertEquals(Math.pow(-first, 0),
+                calculator.calculate(-first, 0, '^'),
+                DELTA);
     }
 }
