9ca6a0547998b35ca7893390f1054641b79da3e8
olekskov <oleksandr@kovalenko.cc>
2021-07-02 22:08:09 +0000

    JV-Lambda-Calc solution
    - tests naming corrected according to convention
    - refactored with constants over magic numbers
    - made exceptions type more specific

--- src/main/java/core/basesyntax/LambdaCalc.java
+++ src/main/java/core/basesyntax/LambdaCalc.java
@@ -1,26 +1,32 @@
 package core.basesyntax;
 
 public class LambdaCalc {
+    public static final char ADDITION = '+';
+    public static final char SUBTRACTION = '-';
+    public static final char MULTIPLICATION = '*';
+    public static final char DIVISION = '/';
+    public static final char RAISING = '^';
+
     public double calculate(double operand1, double operand2, char operation) {
         switch (operation) {
-            case '+':
+            case ADDITION:
                 return operand1 + operand2;
-            case '-':
+            case SUBTRACTION:
                 return operand1 - operand2;
-            case '*':
+            case MULTIPLICATION:
                 return operand1 * operand2;
-            case '/':
+            case DIVISION:
                 if (operand2 == 0 || operand2 == -0) {
-                    throw new RuntimeException("Division by zero result cannot be provided");
+                    throw new ArithmeticException("Division by zero result cannot be provided");
                 }
                 return operand1 / operand2;
-            case '^':
+            case RAISING:
                 if ((operand1 == 0 || operand1 == -0) && operand2 < 0) {
-                    throw new RuntimeException("Zero in negative pow cannot be provided");
+                    throw new ArithmeticException("Zero in negative pow cannot be provided");
                 }
                 return Math.pow(operand1, operand2);
             default:
-                throw new RuntimeException("Operation type not supported");
+                throw new IllegalArgumentException("Operation type not supported");
         }
     }
 }
