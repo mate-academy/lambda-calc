2511ece0cd379f584a3afb9b7573f135e753b316
mishamolnar <michaellmuller423@gmail.com>
2021-08-31 15:47:36 +0300

    build success

--- src/main/java/core/basesyntax/calculator/CalculatorImpl.java
+++ src/main/java/core/basesyntax/calculator/CalculatorImpl.java
@@ -3,6 +3,9 @@ package core.basesyntax.calculator;
 public class CalculatorImpl implements Calculator {
     @Override
     public double calculate(double one, double two, char symbol) {
+        if (two == 0 && symbol == '/') {
+            throw new ArithmeticException("Division by zero is not possible");
+        }
         switch (symbol) {
             case ('+') : return one + two;
             case ('-') : return one - two;
