347cc2e67c25fe299d850ecdad7cd29f9a19c840
Dima Petrushenko <dmytro.petrushenko@gmail.com>
2021-09-03 17:56:20 +0300

    addition, subtraction, multiplacation, division, raising

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -4,5 +4,26 @@ package core.basesyntax;
  * Feel free to remove this class and create your own.
  */
 public class HelloWorld {
+    double calculate(double number1, double number2, char sign) {
+        if (sign == '+') {
+            return number1 + number2;
+        }
+        if (sign == '-') {
+            return number1 - number2;
+        }
+        if (sign == '/') {
+            if (number2 == 0) {
+                throw new ArithmeticException("/ by zero");
+            }
+            return number1 / number2;
+        }
+        if (sign == '*') {
+            return number1 * number2;
+        }
+        if (sign == '^') {
+            return Math.pow(number1, number2);
+        }
+        return 0.0;
+    }
 
 }
