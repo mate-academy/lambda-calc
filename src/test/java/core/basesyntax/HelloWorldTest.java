33220ccc30ebe9a9612977a3ff29a5c7c2c46375
Dima Petrushenko <dmytro.petrushenko@gmail.com>
2021-09-03 18:19:34 +0300

    mvn OK

--- src/test/java/core/basesyntax/HelloWorldTest.java
+++ src/test/java/core/basesyntax/HelloWorldTest.java
@@ -1,10 +1,11 @@
 package core.basesyntax;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
-
 
 /**
  * Feel free to remove this class and create your own.
@@ -12,10 +13,10 @@ import static org.junit.jupiter.api.Assertions.*;
 public class HelloWorldTest {
     static final double delta = 0.0001;
     private static HelloWorld calculator;
-    double positive1;
-    double positive2;
-    double negative1;
-    double negative2;
+    private double positive1;
+    private double positive2;
+    private double negative1;
+    private double negative2;
 
     @BeforeAll
     static void beforeAll() {
@@ -33,35 +34,35 @@ public class HelloWorldTest {
 
     @Test
     void calculate_Addition_TwoPositive_OK() {
-        double actual = calculator.calculate(positive1, positive2 ,'+');
+        double actual = calculator.calculate(positive1, positive2,'+');
         assertEquals(105, actual);
     }
 
     @Test
     void calculate_Addition_TwoNegatives_OK() {
-        double actual = calculator.calculate(negative1, negative2 ,'+');
+        double actual = calculator.calculate(negative1, negative2,'+');
         assertEquals(-105, actual);
     }
 
     @Test
     void calculate_Addition_OneNegatives_OK() {
-        double actual = calculator.calculate(positive1, negative1 ,'+');
+        double actual = calculator.calculate(positive1, negative1,'+');
         assertEquals(0, actual);
-        actual = calculator.calculate(negative2, positive2 ,'+');
+        actual = calculator.calculate(negative2, positive2,'+');
         assertEquals(0, actual);
     }
 
     @Test
     void calculate_Addition_Zero_OK() {
-        double actual = calculator.calculate(0, 0 ,'+');
+        double actual = calculator.calculate(0, 0,'+');
         assertEquals(0, actual);
-        actual = calculator.calculate(0, positive1 ,'+');
+        actual = calculator.calculate(0, positive1,'+');
         assertEquals(positive1, actual);
-        actual = calculator.calculate(positive2, 0 ,'+');
+        actual = calculator.calculate(positive2, 0,'+');
         assertEquals(positive2, actual);
-        actual = calculator.calculate(negative1, 0 ,'+');
+        actual = calculator.calculate(negative1, 0,'+');
         assertEquals(negative1, actual);
-        actual = calculator.calculate(0, negative2 ,'+');
+        actual = calculator.calculate(0, negative2,'+');
         assertEquals(negative2, actual);
     }
 
@@ -79,168 +80,168 @@ public class HelloWorldTest {
 
     @Test
     void calculate_Subtraction_TwoPositive_OK() {
-        double actual = calculator.calculate(positive1, positive2 ,'-');
+        double actual = calculator.calculate(positive1, positive2,'-');
         assertEquals(-95, actual);
     }
 
     @Test
     void calculate_Subtraction_TwoNegatives_OK() {
-        double actual = calculator.calculate(negative1, negative2 ,'-');
+        double actual = calculator.calculate(negative1, negative2,'-');
         assertEquals(95, actual);
     }
 
     @Test
     void calculate_Subtraction_OneNegatives_OK() {
-        double actual = calculator.calculate(positive1, negative1 ,'-');
+        double actual = calculator.calculate(positive1, negative1,'-');
         assertEquals(10, actual);
-        actual = calculator.calculate(negative2, positive2 ,'-');
+        actual = calculator.calculate(negative2, positive2,'-');
         assertEquals(-200, actual);
     }
 
     @Test
     void calculate_Subtraction_Zero_OK() {
-        double actual = calculator.calculate(0, 0 ,'-');
+        double actual = calculator.calculate(0, 0,'-');
         assertEquals(0, actual);
-        actual = calculator.calculate(0, positive1 ,'-');
+        actual = calculator.calculate(0, positive1,'-');
         assertEquals(negative1, actual);
-        actual = calculator.calculate(positive2, 0 ,'-');
+        actual = calculator.calculate(positive2, 0,'-');
         assertEquals(positive2, actual);
-        actual = calculator.calculate(negative1, 0 ,'-');
+        actual = calculator.calculate(negative1, 0,'-');
         assertEquals(negative1, actual);
-        actual = calculator.calculate(0, negative2 ,'-');
+        actual = calculator.calculate(0, negative2,'-');
         assertEquals(positive2, actual);
     }
 
     @Test
     void calculate_Division_TwoPositive_OK() {
-        double actual = calculator.calculate(positive1, positive2 ,'/');
+        double actual = calculator.calculate(positive1, positive2,'/');
         assertEquals(0.05, actual);
     }
 
     @Test
     void calculate_Division_TwoNegatives_OK() {
-        double actual = calculator.calculate(negative1, negative2 ,'/');
+        double actual = calculator.calculate(negative1, negative2,'/');
         assertEquals(0.05, actual);
     }
 
     @Test
     void calculate_Division_OneNegatives_OK() {
-        double actual = calculator.calculate(positive1, negative1 ,'/');
+        double actual = calculator.calculate(positive1, negative1,'/');
         assertEquals(-1, actual);
-        actual = calculator.calculate(negative2, positive2 ,'/');
+        actual = calculator.calculate(negative2, positive2,'/');
         assertEquals(-1, actual);
     }
 
     @Test
     void calculate_Division_byZero_OK() {
         assertThrows(ArithmeticException.class, () -> {
-        calculator.calculate(0, 0 ,'/');
-        calculator.calculate(positive2, 0 ,'/');
-        calculator.calculate(negative1, 0 ,'/');
+            calculator.calculate(0, 0,'/');
+            calculator.calculate(positive2, 0,'/');
+            calculator.calculate(negative1, 0,'/');
         });
     }
 
     @Test
     void calculate_Division_ZeroByNumber_OK() {
-        double actual = calculator.calculate(0, positive1 ,'/');
+        double actual = calculator.calculate(0, positive1,'/');
         assertEquals(0.0, actual);
-        actual = calculator.calculate(0, negative2 ,'/');
+        actual = calculator.calculate(0, negative2,'/');
         assertEquals(-0.0, actual);
     }
 
     @Test
     void calculate_multiplication_TwoPositive_OK() {
-        double actual = calculator.calculate(positive1, positive2 ,'*');
+        double actual = calculator.calculate(positive1, positive2,'*');
         assertEquals(500.0, actual);
     }
 
     @Test
     void calculate_multiplication_TwoNegatives_OK() {
-        double actual = calculator.calculate(negative1, negative2 ,'*');
+        double actual = calculator.calculate(negative1, negative2,'*');
         assertEquals(500.0, actual);
     }
 
     @Test
     void calculate_multiplication_OneNegatives_OK() {
-        double actual = calculator.calculate(positive1, negative1 ,'*');
+        double actual = calculator.calculate(positive1, negative1,'*');
         assertEquals(-25, actual);
-        actual = calculator.calculate(negative2, positive2 ,'*');
+        actual = calculator.calculate(negative2, positive2,'*');
         assertEquals(-10000.0, actual);
     }
 
     @Test
     void calculate_multiplication_byZero_OK() {
-        double actual = calculator.calculate(0, 0 ,'*');
+        double actual = calculator.calculate(0, 0,'*');
         assertEquals(0, actual);
-        actual = calculator.calculate(negative1, 0 ,'*');
+        actual = calculator.calculate(negative1, 0,'*');
         assertEquals(-0.0, actual);
-        actual = calculator.calculate(positive1, 0 ,'*');
+        actual = calculator.calculate(positive1, 0,'*');
         assertEquals(0.0, actual);
     }
 
     @Test
     void calculate_multiplication_ZeroByNumber_OK() {
-        double actual = calculator.calculate(0, positive1 ,'*');
+        double actual = calculator.calculate(0, positive1,'*');
         assertEquals(0.0, actual);
-        actual = calculator.calculate(0, negative2 ,'*');
+        actual = calculator.calculate(0, negative2,'*');
         assertEquals(-0.0, actual);
     }
 
     @Test
     void calculate_multiplication_Max_OK() {
-        double actual = calculator.calculate(0, Double.MAX_VALUE ,'*');
+        double actual = calculator.calculate(0, Double.MAX_VALUE,'*');
         assertEquals(0.0, actual);
-        actual = calculator.calculate(positive1, Double.MAX_VALUE ,'*');
+        actual = calculator.calculate(positive1, Double.MAX_VALUE,'*');
         assertEquals(Double.POSITIVE_INFINITY, actual);
-        actual = calculator.calculate(negative1, Double.MAX_VALUE ,'*');
+        actual = calculator.calculate(negative1, Double.MAX_VALUE,'*');
         assertEquals(Double.NEGATIVE_INFINITY, actual);
     }
 
     @Test
     void calculate_multiplication_ZeroMin_OK() {
-        double actual = calculator.calculate(0, Double.MIN_VALUE ,'*');
+        double actual = calculator.calculate(0, Double.MIN_VALUE,'*');
         assertEquals(0.0, actual);
     }
 
     @Test
     void calculate_Raising_PositivePower_OK() {
-        double actual = calculator.calculate(positive1,2 ,'^');
+        double actual = calculator.calculate(positive1,2,'^');
         assertEquals(25.0, actual);
-        actual = calculator.calculate(negative1,2 ,'^');
+        actual = calculator.calculate(negative1,2,'^');
         assertEquals(25.0, actual);
     }
 
     @Test
     void calculate_Raising_NegativePower_OK() {
-        double actual = calculator.calculate(positive1, -2 ,'^');
+        double actual = calculator.calculate(positive1, -2,'^');
         assertEquals(0.04, actual);
-        actual = calculator.calculate(negative1, -2 ,'^');
+        actual = calculator.calculate(negative1, -2,'^');
         assertEquals(0.04, actual);
     }
 
     @Test
     void calculate_Raising_ZeroPower_OK() {
-        double actual = calculator.calculate(positive1, 0 ,'^');
+        double actual = calculator.calculate(positive1, 0,'^');
         assertEquals(1.0, actual);
-        actual = calculator.calculate(negative1, 0 ,'^');
+        actual = calculator.calculate(negative1, 0,'^');
         assertEquals(1.0, actual);
     }
 
     @Test
     void calculate_Raising_ZeroToPower_OK() {
-        double actual = calculator.calculate(0, negative1 ,'^');
+        double actual = calculator.calculate(0, negative1,'^');
         assertEquals(Double.POSITIVE_INFINITY, actual);
-        actual = calculator.calculate(0, positive1 ,'^');
+        actual = calculator.calculate(0, positive1,'^');
         assertEquals(0, actual);
     }
 
     @Test
     void calculate_IllegalOperation_NotOk() {
         assertThrows(RuntimeException.class, () -> {
-            calculator.calculate(positive1, positive2 ,'?');
-            calculator.calculate(positive1, positive2 ,'!');
-            calculator.calculate(positive1, positive2 ,'a');
-                });
+            calculator.calculate(positive1, positive2,'?');
+            calculator.calculate(positive1, positive2,'!');
+            calculator.calculate(positive1, positive2,'a');
+        });
     }
 
 }
