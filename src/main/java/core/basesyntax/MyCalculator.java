d6e0cf6b1723ae6a911d7383f335d66480b4321e
vitalii_yelinskyi <yelinskyizp@gmail.com>
2021-09-06 11:38:13 +0300

    renamed sing to sign

--- src/main/java/core/basesyntax/MyCalculator.java
+++ src/main/java/core/basesyntax/MyCalculator.java
@@ -4,34 +4,34 @@ public class MyCalculator implements Calculator {
     private static final double POSITIVE_MAX_VALUE = Double.MAX_VALUE;
     private static final double NEGATIVE_MAX_VALUE = -Double.MAX_VALUE;
     private static final String TOO_LARGE_VALUE = "Too large or little result!";
-    private static final char SING_ADDITION = '+';
-    private static final char SING_SUBTRACTION = '-';
-    private static final char SING_DIVISION = '/';
-    private static final char SING_MULTIPLICATION = '*';
-    private static final char SING_RAISING_TO_A_POWER = '^';
+    private static final char SIGN_ADDITION = '+';
+    private static final char SIGN_SUBTRACTION = '-';
+    private static final char SIGN_DIVISION = '/';
+    private static final char SIGN_MULTIPLICATION = '*';
+    private static final char SIGN_RAISING_TO_A_POWER = '^';
 
     public double calculate(double a, double b, char sing) throws MyCalculateException {
         switch (sing) {
-            case SING_MULTIPLICATION: {
+            case SIGN_MULTIPLICATION: {
                 checkException(a * b);
                 return a * b;
             }
-            case SING_DIVISION: {
+            case SIGN_DIVISION: {
                 if (b == 0) {
                     throw new MyCalculateException("Can't be divided by zero!");
                 }
                 checkException(a / b);
                 return a / b;
             }
-            case SING_ADDITION: {
+            case SIGN_ADDITION: {
                 checkException(a + b);
                 return a + b;
             }
-            case SING_SUBTRACTION: {
+            case SIGN_SUBTRACTION: {
                 checkException(a - b);
                 return a - b;
             }
-            case SING_RAISING_TO_A_POWER: {
+            case SIGN_RAISING_TO_A_POWER: {
                 checkException(Math.pow(a, b));
                 return Math.pow(a, b);
             }
