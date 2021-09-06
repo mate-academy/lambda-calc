7a4644534c31e90baba7876cf21186919741ed1d
Vladimir Nedashkovskiy <vo-v4ik@hotmail.com>
2021-09-06 23:33:32 +0300

    implemented method calculate()

--- src/main/java/core/basesyntax/Calculator.java
+++ src/main/java/core/basesyntax/Calculator.java
@@ -1,4 +1,31 @@
 package core.basesyntax;
 
 public class Calculator {
+
+    public double calculate(double a, double b, char operation) {
+        if (operation == '+') {
+            return a + b;
+        }
+
+        if (operation == '-') {
+            return a - b;
+        }
+
+        if (operation == '/') {
+            return a / b;
+        }
+
+        if (operation == '*') {
+            return a * b;
+        }
+
+        if (operation == '^') {
+            double result = 1;
+            for (int i = 1; i <= b; i++) {
+                result = result * a;
+            }
+            return result;
+        }
+        return 0;
+    }
 }
