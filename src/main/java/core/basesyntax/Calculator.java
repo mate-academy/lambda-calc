0996914b67d1da6090f7c6f52bfe5cbebd9d7675
Vladimir Nedashkovskiy <vo-v4ik@hotmail.com>
2021-09-07 11:16:20 +0300

    refactoring method calculate

--- src/main/java/core/basesyntax/Calculator.java
+++ src/main/java/core/basesyntax/Calculator.java
@@ -3,29 +3,26 @@ package core.basesyntax;
 public class Calculator implements Calc {
 
     public double calculate(double firstOperand, double secondOperand, char operation) {
-        if (operation == '+') {
-            return firstOperand + secondOperand;
+        switch (operation) {
+            case '+':
+                return firstOperand + secondOperand;
+            case '-':
+                return firstOperand - secondOperand;
+            case '/':
+                if (secondOperand == 0) {
+                    throw new ArithmeticException();
+                }
+                return firstOperand / secondOperand;
+            case '*':
+                return firstOperand * secondOperand;
+            case '^':
+                double result = 1;
+                for (int i = 1; i <= secondOperand; i++) {
+                    result = result * firstOperand;
+                }
+                return result;
+            default:
+                throw new RuntimeException("such an operation is not possible" + operation);
         }
-
-        if (operation == '-') {
-            return firstOperand - secondOperand;
-        }
-
-        if (operation == '/') {
-            return firstOperand / secondOperand;
-        }
-
-        if (operation == '*') {
-            return firstOperand * secondOperand;
-        }
-
-        if (operation == '^') {
-            double result = 1;
-            for (int i = 1; i <= secondOperand; i++) {
-                result = result * firstOperand;
-            }
-            return result;
-        }
-        return 0;
     }
 }
