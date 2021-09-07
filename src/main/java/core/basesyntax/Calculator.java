6ff0c887f11b0d78cfc2283a9ed8de4fdd342f2b
Vladimir Nedashkovskiy <vo-v4ik@hotmail.com>
2021-09-07 10:10:19 +0300

    creating unit tests for the sum

--- src/main/java/core/basesyntax/Calculator.java
+++ src/main/java/core/basesyntax/Calculator.java
@@ -2,27 +2,27 @@ package core.basesyntax;
 
 public class Calculator {
 
-    public double calculate(double a, double b, char operation) {
+    public double calculate(double value1, double value2, char operation) {
         if (operation == '+') {
-            return a + b;
+            return value1 + value2;
         }
 
         if (operation == '-') {
-            return a - b;
+            return value1 - value2;
         }
 
         if (operation == '/') {
-            return a / b;
+            return value1 / value2;
         }
 
         if (operation == '*') {
-            return a * b;
+            return value1 * value2;
         }
 
         if (operation == '^') {
             double result = 1;
-            for (int i = 1; i <= b; i++) {
-                result = result * a;
+            for (int i = 1; i <= value2; i++) {
+                result = result * value1;
             }
             return result;
         }
--- src/test/java/core/basesyntax/CalculatorTest.java
+++ src/test/java/core/basesyntax/CalculatorTest.java
@@ -1,7 +1,68 @@
 package core.basesyntax;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
+    private static Calculator calc;
+    private double value1;
+    private double value2;
+    private double actual;
+    //private static final double MIN_DOUBLE = Double.MIN_VALUE;
+    //private static final double MAX_DOUBLE = Double.MAX_VALUE;
 
+    @BeforeAll
+    static void beforeAll() {
+        calc = new Calculator();
+    }
+
+    @Test
+    void addPositiveOperands() {
+        value1 = 5.0;
+        value2 = 12.0;
+        actual = calc.calculate(value1, value2, '+');
+        assertEquals(actual, 17.0);
+    }
+
+    @Test
+    void addNegativeOperands() {
+        value1 = -5.0;
+        value2 = -12.0;
+        actual = calc.calculate(value1, value2, '+');
+        assertEquals(actual, -17.0);
+    }
+
+    @Test
+    void addPositiveAndNegativeOperands() {
+        value1 = 5.0;
+        value2 = -12.0;
+        actual = calc.calculate(value1, value2, '+');
+        assertEquals(actual, -7.0);
+    }
+
+    @Test
+    void addZeroInDifferentPlaces() {
+        value1 = 5.0;
+        actual = calc.calculate(value1, 0, '+');
+        assertEquals(actual, 5.0);
+        actual = calc.calculate(0, value1, '+');
+        assertEquals(actual, 5.0);
+    }
+
+    @Test
+    void addMaxAndMinDoubleValues() {
+        double result = 1.7976931348623157E308;
+        actual = calc.calculate(Double.MIN_VALUE, Double.MAX_VALUE, '+');
+        assertEquals(actual, result);
+    }
+
+    @Test
+    void subPositiveOperands() {
+        value1 = 5.0;
+        value2 = 12.0;
+        actual = calc.calculate(value1, value2, '-');
+        assertEquals(actual, -7.0);
+    }
 }
