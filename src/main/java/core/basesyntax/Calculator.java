6540d9076236ce87d8a645e474c08f07d5024cf5
dogovor <dogovor@lan.com.ua>
2021-09-07 23:23:15 +0300

    changed if to switch

--- src/main/java/core/basesyntax/Calculator.java
+++ src/main/java/core/basesyntax/Calculator.java
@@ -5,24 +5,22 @@ import java.util.NoSuchElementException;
 public class Calculator {
 
     public double calculate(double x, double y, String operation) {
-        if (operation.equals("+")) {
-            return x + y;
+        switch (operation) {
+            case "+":
+                return x + y;
+            case "-":
+                return x - y;
+            case "*":
+                return x * y;
+            case "/":
+                if (y == 0) {
+                    throw new ArithmeticException("Can't divide by zero...");
+                }
+                return x / y;
+            case "^":
+                return Math.pow(x, y);
+            default:
+                throw new NoSuchElementException("Your data is incorrect...");
         }
-        if (operation.equals("-")) {
-            return x - y;
-        }
-        if (operation.equals("*")) {
-            return x * y;
-        }
-        if (operation.equals("/") && y != 0) {
-            return x / y;
-        }
-        if (operation.equals("/") && y == 0) {
-            throw new ArithmeticException("Can't divide by zero...");
-        }
-        if (operation.equals("^")) {
-            return Math.pow(x, y);
-        }
-        throw new NoSuchElementException("Your data is incorrect...");
     }
 }
