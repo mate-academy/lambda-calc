d92daa354a5844675b917e5bd0fa2bd54397b261
Yuliya Senchenko <usencenko70@gmail.com>
2021-09-13 18:52:05 +0300

    Created jv-lambda-calc (fixed)

--- src/test/java/core/basesyntax/LambdaCalculatorTest.java
+++ src/test/java/core/basesyntax/LambdaCalculatorTest.java
@@ -8,12 +8,12 @@ import org.junit.jupiter.api.Test;
 
 class LambdaCalculatorTest {
     private static final double DELTA = 0.0001;
-    private static final double firstParameter = 0.1;
-    private static final double secondParameter = 0.2;
-    private static final double zeroParameter = 0;
-    private static final double degreeOfPower = 3;
-    private static final double minDouble = Double.MIN_VALUE;
-    private static final double maxDouble = Double.MAX_VALUE;
+    private static final double FIRST_PARAMETER = 0.1;
+    private static final double SECOND_PARAMETER = 0.2;
+    private static final double ZERO_PARAMETER = 0;
+    private static final double DEGREE_OF_POWER = 3;
+    private static final double MIN_DOUBLE = Double.MIN_VALUE;
+    private static final double MAX_DOUBLE = Double.MAX_VALUE;
     private static Calculator calculator;
     private double expected;
     private double actual;
@@ -25,187 +25,187 @@ class LambdaCalculatorTest {
 
     @Test
     void calculate_additionWithTwoPositiveOperands_ok() {
-        actual = calculator.calculate(firstParameter, secondParameter, '+');
+        actual = calculator.calculate(FIRST_PARAMETER, SECOND_PARAMETER, '+');
         expected = 0.3;
         assertEquals(expected, actual, DELTA);
     }
 
     @Test
     void calculate_additionWithTwoNegativeOperands_ok() {
-        actual = calculator.calculate(-firstParameter, -secondParameter, '+');
+        actual = calculator.calculate(-FIRST_PARAMETER, -SECOND_PARAMETER, '+');
         expected = -0.3;
         assertEquals(expected, actual, DELTA);
     }
 
     @Test
     void calculate_additionWithPositiveAndNegativeOperands_ok() {
-        actual = calculator.calculate(firstParameter, -secondParameter, '+');
+        actual = calculator.calculate(FIRST_PARAMETER, -SECOND_PARAMETER, '+');
         expected = -0.1;
         assertEquals(expected, actual);
-        actual = calculator.calculate(-firstParameter, secondParameter, '+');
+        actual = calculator.calculate(-FIRST_PARAMETER, SECOND_PARAMETER, '+');
         expected = 0.1;
         assertEquals(expected, actual);
     }
 
     @Test
     void calculate_additionWithFirthZeroOperands_ok() {
-        actual = calculator.calculate(zeroParameter, secondParameter, '+');
+        actual = calculator.calculate(ZERO_PARAMETER, SECOND_PARAMETER, '+');
         expected = 0.2;
         assertEquals(expected, actual);
     }
 
     @Test
     void calculate_additionWithSecondZeroOperands_ok() {
-        actual = calculator.calculate(firstParameter, zeroParameter, '+');
+        actual = calculator.calculate(FIRST_PARAMETER, ZERO_PARAMETER, '+');
         expected = 0.1;
         assertEquals(expected, actual);
     }
 
     @Test
     void calculate_additionWithMinDoubleValues_ok() {
-        actual = calculator.calculate(minDouble, minDouble, '+');
+        actual = calculator.calculate(MIN_DOUBLE, MIN_DOUBLE, '+');
         expected = 0;
         assertEquals(expected, actual, DELTA);
     }
 
     @Test
     void calculate_additionWithMaxDoubleValues_ok() {
-        actual = calculator.calculate(maxDouble, maxDouble, '+');
+        actual = calculator.calculate(MAX_DOUBLE, MAX_DOUBLE, '+');
         expected = Double.POSITIVE_INFINITY;
         assertEquals(expected, actual, DELTA);
     }
 
     @Test
     void calculate_subtractionWithTwoPositiveOperands_ok() {
-        actual = calculator.calculate(firstParameter, secondParameter, '-');
+        actual = calculator.calculate(FIRST_PARAMETER, SECOND_PARAMETER, '-');
         expected = -0.1;
         assertEquals(expected, actual);
     }
 
     @Test
     void calculate_subtractionWithTwoNegativeOperands_ok() {
-        actual = calculator.calculate(-firstParameter, -secondParameter, '-');
+        actual = calculator.calculate(-FIRST_PARAMETER, -SECOND_PARAMETER, '-');
         expected = 0.1;
         assertEquals(expected, actual);
     }
 
     @Test
     void calculate_subtractionWithPositiveAndNegativeOperands_ok() {
-        actual = calculator.calculate(firstParameter, -secondParameter, '-');
+        actual = calculator.calculate(FIRST_PARAMETER, -SECOND_PARAMETER, '-');
         expected = 0.3;
         assertEquals(expected, actual, DELTA);
-        actual = calculator.calculate(-firstParameter, secondParameter, '-');
+        actual = calculator.calculate(-FIRST_PARAMETER, SECOND_PARAMETER, '-');
         expected = -0.3;
         assertEquals(expected, actual, DELTA);
     }
 
     @Test
     void calculate_subtractionWithFirthZeroOperands_ok() {
-        actual = calculator.calculate(zeroParameter, secondParameter, '-');
+        actual = calculator.calculate(ZERO_PARAMETER, SECOND_PARAMETER, '-');
         expected = -0.2;
         assertEquals(expected, actual);
     }
 
     @Test
     void calculate_subtractionWithSecondZeroOperands_ok() {
-        actual = calculator.calculate(firstParameter, zeroParameter, '-');
+        actual = calculator.calculate(FIRST_PARAMETER, ZERO_PARAMETER, '-');
         expected = 0.1;
         assertEquals(expected, actual);
     }
 
     @Test
     void calculate_subtractionWithMinDoubleValues_ok() {
-        actual = calculator.calculate(minDouble, minDouble, '-');
+        actual = calculator.calculate(MIN_DOUBLE, MIN_DOUBLE, '-');
         expected = 0;
         assertEquals(expected, actual, DELTA);
     }
 
     @Test
     void calculate_subtractionWithMaxDoubleValues_ok() {
-        actual = calculator.calculate(maxDouble, maxDouble, '-');
+        actual = calculator.calculate(MAX_DOUBLE, MAX_DOUBLE, '-');
         expected = 0;
         assertEquals(expected, actual, DELTA);
     }
 
     @Test
     void calculate_multiplicationWithTwoPositiveOperands_ok() {
-        actual = calculator.calculate(firstParameter, secondParameter, '*');
+        actual = calculator.calculate(FIRST_PARAMETER, SECOND_PARAMETER, '*');
         expected = 0.02;
         assertEquals(expected, actual, DELTA);
     }
 
     @Test
     void calculate_multiplicationWithTwoNegativeOperands_ok() {
-        actual = calculator.calculate(-firstParameter, -secondParameter, '*');
+        actual = calculator.calculate(-FIRST_PARAMETER, -SECOND_PARAMETER, '*');
         expected = 0.02;
         assertEquals(expected, actual, DELTA);
     }
 
     @Test
     void calculate_multiplicationWithPositiveAndNegativeOperands_ok() {
-        actual = calculator.calculate(firstParameter, -secondParameter, '*');
+        actual = calculator.calculate(FIRST_PARAMETER, -SECOND_PARAMETER, '*');
         expected = -0.02;
         assertEquals(expected, actual, DELTA);
-        actual = calculator.calculate(-firstParameter, secondParameter, '*');
+        actual = calculator.calculate(-FIRST_PARAMETER, SECOND_PARAMETER, '*');
         expected = -0.02;
         assertEquals(expected, actual, DELTA);
     }
 
     @Test
     void calculate_multiplicationWithFirthZeroOperands_ok() {
-        actual = calculator.calculate(zeroParameter, secondParameter, '*');
+        actual = calculator.calculate(ZERO_PARAMETER, SECOND_PARAMETER, '*');
         expected = 0;
         assertEquals(expected, actual);
     }
 
     @Test
     void calculate_multiplicationWithSecondZeroOperands_ok() {
-        actual = calculator.calculate(firstParameter, zeroParameter, '*');
+        actual = calculator.calculate(FIRST_PARAMETER, ZERO_PARAMETER, '*');
         expected = 0;
         assertEquals(expected, actual);
     }
 
     @Test
     void calculate_multiplicationWithMinDoubleValues_ok() {
-        actual = calculator.calculate(minDouble, minDouble, '*');
+        actual = calculator.calculate(MIN_DOUBLE, MIN_DOUBLE, '*');
         expected = 0;
         assertEquals(expected, actual, DELTA);
     }
 
     @Test
     void calculate_multiplicationWithMaxDoubleValues_ok() {
-        actual = calculator.calculate(maxDouble, maxDouble, '*');
+        actual = calculator.calculate(MAX_DOUBLE, MAX_DOUBLE, '*');
         expected = Double.POSITIVE_INFINITY;
         assertEquals(expected, actual, DELTA);
     }
 
     @Test
     void calculate_divisionWithTwoPositiveOperands_ok() {
-        actual = calculator.calculate(firstParameter, secondParameter, '/');
+        actual = calculator.calculate(FIRST_PARAMETER, SECOND_PARAMETER, '/');
         expected = 0.5;
         assertEquals(expected, actual, DELTA);
     }
 
     @Test
     void calculate_divisionWithTwoNegativeOperands_ok() {
-        actual = calculator.calculate(-firstParameter, -secondParameter, '/');
+        actual = calculator.calculate(-FIRST_PARAMETER, -SECOND_PARAMETER, '/');
         expected = 0.5;
         assertEquals(expected, actual, DELTA);
     }
 
     @Test
     void calculate_divisionWithPositiveAndNegativeOperands_ok() {
-        actual = calculator.calculate(firstParameter, -secondParameter, '/');
+        actual = calculator.calculate(FIRST_PARAMETER, -SECOND_PARAMETER, '/');
         expected = -0.5;
         assertEquals(expected, actual, DELTA);
-        actual = calculator.calculate(-firstParameter, secondParameter, '/');
+        actual = calculator.calculate(-FIRST_PARAMETER, SECOND_PARAMETER, '/');
         expected = -0.5;
         assertEquals(expected, actual, DELTA);
     }
 
     @Test
     void calculate_divisionWithFirthZeroOperands_ok() {
-        actual = calculator.calculate(zeroParameter, secondParameter, '/');
+        actual = calculator.calculate(ZERO_PARAMETER, SECOND_PARAMETER, '/');
         expected = 0;
         assertEquals(expected, actual);
     }
@@ -213,68 +213,68 @@ class LambdaCalculatorTest {
     @Test
     void calculate_divisionWithSecondZeroOperands_not0k() {
         assertThrows(ArithmeticException.class, () ->
-                calculator.calculate(firstParameter, zeroParameter, '/'));
+                calculator.calculate(FIRST_PARAMETER, ZERO_PARAMETER, '/'));
     }
 
     @Test
     void calculate_divisionWithMinDoubleValues_ok() {
-        actual = calculator.calculate(minDouble, minDouble, '/');
+        actual = calculator.calculate(MIN_DOUBLE, MIN_DOUBLE, '/');
         expected = 1;
         assertEquals(expected, actual, DELTA);
     }
 
     @Test
     void calculate_divisionWithMaxDoubleValues_ok() {
-        actual = calculator.calculate(maxDouble, maxDouble, '/');
+        actual = calculator.calculate(MAX_DOUBLE, MAX_DOUBLE, '/');
         expected = 1;
         assertEquals(expected, actual, DELTA);
     }
 
     @Test
     void calculate_raisingPositiveValueToThePositivePower_ok() {
-        actual = calculator.calculate(firstParameter, degreeOfPower, '^');
+        actual = calculator.calculate(FIRST_PARAMETER, DEGREE_OF_POWER, '^');
         expected = 0.001;
         assertEquals(expected, actual, DELTA);
     }
 
     @Test
     void calculate_raisingNegativeValueToThePositivePower_ok() {
-        actual = calculator.calculate(-firstParameter, degreeOfPower, '^');
+        actual = calculator.calculate(-FIRST_PARAMETER, DEGREE_OF_POWER, '^');
         expected = -0.001;
         assertEquals(expected, actual, DELTA);
     }
 
     @Test
     void calculate_raisingPositiveValueToTheNegativePower_ok() {
-        actual = calculator.calculate(firstParameter, -degreeOfPower, '^');
+        actual = calculator.calculate(FIRST_PARAMETER, -DEGREE_OF_POWER, '^');
         expected = 1.0 / 0.1 / 0.1 / 0.1;
         assertEquals(expected, actual, DELTA);
     }
 
     @Test
     void calculate_raisingNegativeValueToTheNegativePower_ok() {
-        actual = calculator.calculate(-firstParameter, -degreeOfPower, '^');
+        actual = calculator.calculate(-FIRST_PARAMETER, -DEGREE_OF_POWER, '^');
         expected = 1.0 / -0.1 / -0.1 / -0.1;
         assertEquals(expected, actual, DELTA);
     }
 
     @Test
     void calculate_raisingPositiveValueToZeroPower_ok() {
-        actual = calculator.calculate(firstParameter, zeroParameter, '^');
+        actual = calculator.calculate(FIRST_PARAMETER, ZERO_PARAMETER, '^');
         expected = 1;
         assertEquals(expected, actual);
     }
 
     @Test
     void calculate_raisingNegativeValueToZeroPower_ok() {
-        actual = calculator.calculate(-firstParameter, zeroParameter, '^');
+        actual = calculator.calculate(-FIRST_PARAMETER, ZERO_PARAMETER, '^');
         expected = 1;
         assertEquals(expected, actual);
     }
 
     @Test
     void calculate_raisingZeroToPower_ok() {
-        actual = calculator.calculate(zeroParameter, degreeOfPower, '^');
+        actual = calculator.calculate(ZERO_PARAMETER, DEGREE_OF_POWER, '^');
         expected = 0;
         assertEquals(expected, actual);
     }
@@ -282,6 +282,6 @@ class LambdaCalculatorTest {
     @Test
     void calculate_illegalOperation_notOk() {
         assertThrows(IllegalOperationException.class, () ->
-                calculator.calculate(firstParameter, secondParameter, '#'));
+                calculator.calculate(FIRST_PARAMETER, SECOND_PARAMETER, '#'));
     }
 }
