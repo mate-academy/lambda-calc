1aee46aa5c19c15f4556820bf5932e08ab1cdad2
Den4uc <den.smei.ds@gmail.com>
2021-09-14 13:00:57 +0300

    Remarks after mentor review

--- src/test/java/core/basesyntax/HelloWorldTest.java
+++ src/test/java/core/basesyntax/HelloWorldTest.java
@@ -8,35 +8,8 @@ import org.junit.jupiter.api.Test;
 
 public class HelloWorldTest {
     private static HelloWorld helloWorld;
-    private static final double SUM_OF_TWO_POSITIVE = 12;
-    private static final double SUM_OF_TWO_NEGATIVE = -15;
-    private static final double SUM_OF_NEGATIVE_AND_POSITIVE = 2;
-    private static final double SUM_OF_MAX_AND_MIN = Double.MAX_VALUE + Double.MIN_VALUE;
-    private static final double SUBTRACTION_OF_TWO_POSITIVE = -2;
-    private static final double SUBTRACTION_OF_TWO_NEGATIVE = -9;
-    private static final double SUBTRACTION_OF_NEGATIVE_AND_POSITIVE = -17;
-    private static final double SUBTRACTION_OF_MAX_AND_MIN = Double.MAX_VALUE - Double.MIN_VALUE;
-    private static final double MULTIPLICATION_OF_TWO_POSITIVE = 35;
-    private static final double MULTIPLICATION_OF_TWO_NEGATIVE = 36;
-    private static final double MULTIPLICATION_OF_NEGATIVE_AND_POSITIVE = -60;
-    private static final double MULTIPLICATION_OF_MAX_AND_MIN = Double.MAX_VALUE * Double.MIN_VALUE;
-    private static final double DIVISION_OF_TWO_POSITIVE = 0.7142857142857143;
-    private static final double DIVISION_OF_TWO_NEGATIVE = 4;
-    private static final double DIVISION_OF_NEGATIVE_AND_POSITIVE = -0.4166666666666667;
-    private static final double DIVISION_OF_MAX_AND_MIN = Double.MAX_VALUE / Double.MIN_VALUE;
-    private static final double RAISE_POSITIVE_TO_POSITIVE = 3125;
-    private static final double RAISE_NEGATIVE_TO_POSITIVE = -243;
-    private static final double RAISE_POSITIVE_TO_NEGATIVE = 0.008;
-    private static final double RAISE_NEGATIVE_TO_NEGATIVE = -0.037037037037037;
-    private static final double RAISE_TO_ZERO = 1;
-    private static final double FIRST_POSITIVE_VALUE = 5;
-    private static final double SECOND_POSITIVE_VALUE = 7;
-    private static final double FIRST_NEGATIVE_VALUE = -12;
-    private static final double SECOND_NEGATIVE_VALUE = -3;
-    private static final char ILLEGAL_OPERATION = '#';
-    private static final double MAX_DOUBLE = Double.MAX_VALUE;
-    private static final double MIN_DOUBLE = Double.MIN_VALUE;
-    private static final double ZERO = 0;
+    private static final double MAX_VALUE = Double.MAX_VALUE;
+    private static final double MIN_VALUE = Double.MIN_VALUE;
     private static final double DELTA = 0.0001;
     private static final char ADDITION = '+';
     private static final char SUBTRACTION = '-';
@@ -51,170 +24,230 @@ public class HelloWorldTest {
 
     @Test
     void calculate_SumOfTwoPositiveNumbers_Ok() {
-        assertEquals(SUM_OF_TWO_POSITIVE, helloWorld.calculate(FIRST_POSITIVE_VALUE,
-                SECOND_POSITIVE_VALUE, ADDITION), DELTA);
+        double actual = helloWorld.calculate(5,
+                7, ADDITION);
+        assertEquals(12, actual, DELTA);
     }
 
     @Test
     void calculate_SumOfTwoNegativeNumbers_Ok() {
-        assertEquals(SUM_OF_TWO_NEGATIVE, helloWorld.calculate(FIRST_NEGATIVE_VALUE,
-                SECOND_NEGATIVE_VALUE, ADDITION), DELTA);
+        double actual = helloWorld.calculate(-12,
+                -3, ADDITION);
+        assertEquals(-15, actual, DELTA);
     }
 
     @Test
     void calculate_SumOfNegativeAndPositiveNumbers_Ok() {
-        assertEquals(SUM_OF_NEGATIVE_AND_POSITIVE, helloWorld.calculate(SECOND_NEGATIVE_VALUE,
-                FIRST_POSITIVE_VALUE, ADDITION), DELTA);
+        double actual = helloWorld.calculate(-3,
+                5, ADDITION);
+        assertEquals(2, actual, DELTA);
     }
 
     @Test
-    void calculate_SumOfZeroInDifferentPlaces_Ok() {
-        assertEquals(FIRST_POSITIVE_VALUE, helloWorld.calculate(ZERO,
-                FIRST_POSITIVE_VALUE, ADDITION), DELTA);
-        assertEquals(FIRST_POSITIVE_VALUE, helloWorld.calculate(FIRST_POSITIVE_VALUE,
-                ZERO, ADDITION), DELTA);
+    void calculate_SumOf0InDifferentPlaces_Ok() {
+        double actual = helloWorld.calculate(0,
+                5, ADDITION);
+        assertEquals(5, actual, DELTA);
+        actual = helloWorld.calculate(5,
+                0, ADDITION);
+        assertEquals(5, actual, DELTA);
     }
 
     @Test
     void calculate_SumWithMaxAndMinDoubleValues_Ok() {
-        assertEquals(SUM_OF_MAX_AND_MIN, helloWorld.calculate(MAX_DOUBLE,
-                MIN_DOUBLE, ADDITION), DELTA);
+        double actual = helloWorld.calculate(MAX_VALUE,
+                MIN_VALUE, ADDITION);
+        assertEquals(MAX_VALUE, actual, DELTA);
+        actual = helloWorld.calculate(MAX_VALUE,
+                MAX_VALUE, ADDITION);
+        assertEquals(Double.POSITIVE_INFINITY, actual, DELTA);
+        actual = helloWorld.calculate(MIN_VALUE,
+                MIN_VALUE, ADDITION);
+        assertEquals(0, actual, DELTA);
     }
 
     @Test
     void calculate_SubtractionOfTwoPositiveNumbers_Ok() {
-        assertEquals(SUBTRACTION_OF_TWO_POSITIVE, helloWorld.calculate(FIRST_POSITIVE_VALUE,
-                SECOND_POSITIVE_VALUE, SUBTRACTION), DELTA);
+        double actual = helloWorld.calculate(5,
+                7, SUBTRACTION);
+        assertEquals(-2, actual, DELTA);
     }
 
     @Test
     void calculate_SubtractionOfTwoNegativeNumbers_Ok() {
-        assertEquals(SUBTRACTION_OF_TWO_NEGATIVE, helloWorld.calculate(FIRST_NEGATIVE_VALUE,
-                SECOND_NEGATIVE_VALUE, SUBTRACTION), DELTA);
+        double actual = helloWorld.calculate(-12,
+                -3, SUBTRACTION);
+        assertEquals(-9, actual, DELTA);
     }
 
     @Test
     void calculate_SubtractionOfNegativeAndPositiveNumbers_Ok() {
-        assertEquals(SUBTRACTION_OF_NEGATIVE_AND_POSITIVE,
-                helloWorld.calculate(FIRST_NEGATIVE_VALUE,
-                FIRST_POSITIVE_VALUE, SUBTRACTION), DELTA);
+        double actual = helloWorld.calculate(-12,
+                5, SUBTRACTION);
+        assertEquals(-17, actual, DELTA);
     }
 
     @Test
-    void calculate_SubtractionOfZeroInDifferentPlaces_Ok() {
-        assertEquals(FIRST_POSITIVE_VALUE * (-1), helloWorld.calculate(ZERO,
-                FIRST_POSITIVE_VALUE, SUBTRACTION), DELTA);
-        assertEquals(FIRST_POSITIVE_VALUE, helloWorld.calculate(FIRST_POSITIVE_VALUE,
-                ZERO, SUBTRACTION), DELTA);
+    void calculate_SubtractionOf0InDifferentPlaces_Ok() {
+        double actual = helloWorld.calculate(0,
+                5, SUBTRACTION);
+        assertEquals(5 * (-1), actual, DELTA);
+        actual = helloWorld.calculate(5,
+                0, SUBTRACTION);
+        assertEquals(5, actual, DELTA);
     }
 
     @Test
     void calculate_SubtractionWithMaxAndMinDoubleValues_Ok() {
-        assertEquals(SUBTRACTION_OF_MAX_AND_MIN, helloWorld.calculate(MAX_DOUBLE,
-                MIN_DOUBLE, SUBTRACTION), DELTA);
+        double actual = helloWorld.calculate(MAX_VALUE,
+                MIN_VALUE, SUBTRACTION);
+        assertEquals(MAX_VALUE, actual, DELTA);
+        actual = helloWorld.calculate(-MAX_VALUE,
+                MAX_VALUE, SUBTRACTION);
+        assertEquals(Double.NEGATIVE_INFINITY, actual, DELTA);
+        actual = helloWorld.calculate(MAX_VALUE,
+                MAX_VALUE, SUBTRACTION);
+        assertEquals(0, actual, DELTA);
+        actual = helloWorld.calculate(MIN_VALUE,
+                MIN_VALUE, SUBTRACTION);
+        assertEquals(0, actual, DELTA);
     }
 
     @Test
     void calculate_MultiplicationOfTwoPositiveNumbers_Ok() {
-        assertEquals(MULTIPLICATION_OF_TWO_POSITIVE, helloWorld.calculate(FIRST_POSITIVE_VALUE,
-                SECOND_POSITIVE_VALUE, MULTIPLICATION), DELTA);
+        double actual = helloWorld.calculate(5,
+                7, MULTIPLICATION);
+        assertEquals(35, actual, DELTA);
     }
 
     @Test
     void calculate_MultiplicationOfTwoNegativeNumbers_Ok() {
-        assertEquals(MULTIPLICATION_OF_TWO_NEGATIVE, helloWorld.calculate(FIRST_NEGATIVE_VALUE,
-                SECOND_NEGATIVE_VALUE, MULTIPLICATION), DELTA);
+        double actual = helloWorld.calculate(-12,
+                -3, MULTIPLICATION);
+        assertEquals(36, actual, DELTA);
     }
 
     @Test
     void calculate_MultiplicationOfNegativeAndPositiveNumbers_Ok() {
-        assertEquals(MULTIPLICATION_OF_NEGATIVE_AND_POSITIVE,
-                helloWorld.calculate(FIRST_POSITIVE_VALUE,
-                FIRST_NEGATIVE_VALUE, MULTIPLICATION), DELTA);
+        double actual = helloWorld.calculate(5,
+                -12, MULTIPLICATION);
+        assertEquals(-60, actual, DELTA);
     }
 
     @Test
-    void calculate_MultiplicationOfZeroInDifferentPlaces_Ok() {
-        assertEquals(ZERO, helloWorld.calculate(ZERO,
-                FIRST_POSITIVE_VALUE, MULTIPLICATION), DELTA);
-        assertEquals(ZERO, helloWorld.calculate(FIRST_POSITIVE_VALUE,
-                ZERO, MULTIPLICATION), DELTA);
+    void calculate_MultiplicationOf0InDifferentPlaces_Ok() {
+        double actual = helloWorld.calculate(0,
+                5, MULTIPLICATION);
+        assertEquals(0, actual, DELTA);
+        actual = helloWorld.calculate(5,
+                0, MULTIPLICATION);
+        assertEquals(0, actual, DELTA);
     }
 
     @Test
     void calculate_MultiplicationWithMaxAndMinDoubleValues_Ok() {
-        assertEquals(MULTIPLICATION_OF_MAX_AND_MIN, helloWorld.calculate(MAX_DOUBLE,
-                MIN_DOUBLE, MULTIPLICATION), DELTA);
+        double actual = helloWorld.calculate(MAX_VALUE,
+                MIN_VALUE, MULTIPLICATION);
+        assertEquals(0, actual, DELTA);
+        actual = helloWorld.calculate(MAX_VALUE,
+                MAX_VALUE, MULTIPLICATION);
+        assertEquals(Double.POSITIVE_INFINITY, actual, DELTA);
+        actual = helloWorld.calculate(MIN_VALUE,
+                MIN_VALUE, MULTIPLICATION);
+        assertEquals(0, actual, DELTA);
     }
 
     @Test
     void calculate_DivisionOfTwoPositiveNumbers_Ok() {
-        assertEquals(DIVISION_OF_TWO_POSITIVE, helloWorld.calculate(FIRST_POSITIVE_VALUE,
-                SECOND_POSITIVE_VALUE, DIVISION), DELTA);
+        double actual = helloWorld.calculate(5,
+                7, DIVISION);
+        assertEquals(0.7142857142857143, actual, DELTA);
     }
 
     @Test
     void calculate_DivisionOfTwoNegativeNumbers_Ok() {
-        assertEquals(DIVISION_OF_TWO_NEGATIVE, helloWorld.calculate(FIRST_NEGATIVE_VALUE,
-                SECOND_NEGATIVE_VALUE, DIVISION), DELTA);
+        double actual = helloWorld.calculate(-12,
+                -3, DIVISION);
+        assertEquals(4, actual, DELTA);
     }
 
     @Test
     void calculate_DivisionOfNegativeAndPositiveNumbers_Ok() {
-        assertEquals(DIVISION_OF_NEGATIVE_AND_POSITIVE,
-                helloWorld.calculate(FIRST_POSITIVE_VALUE,
-                FIRST_NEGATIVE_VALUE, DIVISION), DELTA);
+        double actual = helloWorld.calculate(5,
+                -12, DIVISION);
+        assertEquals(-0.4166666666666667, actual, DELTA);
     }
 
     @Test
-    void calculate_DivisionOfZeroInDifferentPlaces_Ok() {
-        assertEquals(ZERO, helloWorld.calculate(ZERO,
-                FIRST_POSITIVE_VALUE, DIVISION), DELTA);
+    void calculate_DivisionOf0InDifferentPlaces_Ok() {
+        double actual = helloWorld.calculate(0,
+                5, DIVISION);
+        assertEquals(0, actual, DELTA);
         assertThrows(IllegalArgumentException.class, () -> {
-            helloWorld.calculate(FIRST_POSITIVE_VALUE, ZERO, DIVISION);
+            helloWorld.calculate(5, 0, DIVISION);
         });
     }
 
     @Test
     void calculate_DivisionWithMaxAndMinDoubleValues_Ok() {
-        assertEquals(DIVISION_OF_MAX_AND_MIN, helloWorld.calculate(MAX_DOUBLE,
-                MIN_DOUBLE, DIVISION), DELTA);
+        double actual = helloWorld.calculate(MAX_VALUE,
+                MIN_VALUE, DIVISION);
+        assertEquals(Double.POSITIVE_INFINITY, actual, DELTA);
+        actual = helloWorld.calculate(MAX_VALUE,
+                MAX_VALUE, DIVISION);
+        assertEquals(1, actual, DELTA);
+        actual = helloWorld.calculate(MIN_VALUE,
+                MIN_VALUE, DIVISION);
+        assertEquals(1, actual, DELTA);
+        actual = helloWorld.calculate(MIN_VALUE,
+                MAX_VALUE, DIVISION);
+        assertEquals(0, actual, DELTA);
     }
 
     @Test
     void calculate_RaisingPositiveAndNegativeValueToPositive_Ok() {
-        assertEquals(RAISE_POSITIVE_TO_POSITIVE, helloWorld.calculate(FIRST_POSITIVE_VALUE,
-                FIRST_POSITIVE_VALUE, POWER), DELTA);
-        assertEquals(RAISE_NEGATIVE_TO_POSITIVE, helloWorld.calculate(SECOND_NEGATIVE_VALUE,
-                FIRST_POSITIVE_VALUE, POWER), DELTA);
+        double actual = helloWorld.calculate(5,
+                5, POWER);
+        assertEquals(3125, actual, DELTA);
+        actual = helloWorld.calculate(-3,
+                5, POWER);
+        assertEquals(-243, actual, DELTA);
     }
 
     @Test
     void calculate_RaisingPositiveAndNegativeValueToNegative_Ok() {
-        assertEquals(RAISE_POSITIVE_TO_NEGATIVE, helloWorld.calculate(FIRST_POSITIVE_VALUE,
-                SECOND_NEGATIVE_VALUE, POWER), DELTA);
-        assertEquals(RAISE_NEGATIVE_TO_NEGATIVE, helloWorld.calculate(SECOND_NEGATIVE_VALUE,
-                SECOND_NEGATIVE_VALUE, POWER), DELTA);
+        double actual = helloWorld.calculate(5,
+                -3, POWER);
+        assertEquals(0.008, actual, DELTA);
+        actual = helloWorld.calculate(-3,
+                -3, POWER);
+        assertEquals(-0.037037037037037, actual, DELTA);
     }
 
     @Test
-    void calculate_RaisingPositiveAndNegativeValueToZero_Ok() {
-        assertEquals(RAISE_TO_ZERO, helloWorld.calculate(FIRST_POSITIVE_VALUE,
-                ZERO, POWER), DELTA);
-        assertEquals(RAISE_TO_ZERO, helloWorld.calculate(SECOND_NEGATIVE_VALUE,
-                ZERO, POWER), DELTA);
+    void calculate_RaisingPositiveAndNegativeValueTo0_Ok() {
+        double actual = helloWorld.calculate(5,
+                0, POWER);
+        assertEquals(1, actual, DELTA);
+        actual = helloWorld.calculate(-3,
+                0, POWER);
+        assertEquals(1, actual, DELTA);
     }
 
     @Test
-    void calculate_RaisingZeroToPower_Ok() {
-        assertEquals(ZERO, helloWorld.calculate(ZERO,
-                FIRST_POSITIVE_VALUE, POWER), DELTA);
+    void calculate_Raising0ToPower_Ok() {
+        double actual = helloWorld.calculate(0,
+                5, POWER);
+        assertEquals(0, actual, DELTA);
+        actual = helloWorld.calculate(0,
+                -3, POWER);
+        assertEquals(Double.POSITIVE_INFINITY, actual, DELTA);
     }
 
     @Test
     void calculate_illegalOperation_Ok() {
         assertThrows(IllegalArgumentException.class, () -> {
-            helloWorld.calculate(FIRST_NEGATIVE_VALUE, SECOND_POSITIVE_VALUE, ILLEGAL_OPERATION);
+            helloWorld.calculate(-12, 7, '#');
         });
     }
 }
