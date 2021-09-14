e7be9c7e8471b3c15dbd1480277a3259530e2772
Roman Legendzevych <green3942@gmail.com>
2021-09-14 18:26:14 +0300

    fixed problems after student review

--- src/test/java/core/basesyntax/CalculatorTest.java
+++ src/test/java/core/basesyntax/CalculatorTest.java
@@ -24,28 +24,28 @@ class CalculatorTest {
     // ------------Addition------------
 
     @Test
-    void additionWithTwoPositiveValues_Ok() throws ValidationException {
+    void calculate_additionWithTwoPositiveValues_Ok() throws ValidationException {
         actual = calculator.calculate(10.1, 20.2,'+');
         expected = 30.3;
         assertEquals(expected, actual, DELTA, ADDITION_MESSAGE);
     }
 
     @Test
-    void additionWithTwoNegativeValues_Ok() throws ValidationException {
+    void calculate_additionWithTwoNegativeValues_Ok() throws ValidationException {
         actual = calculator.calculate(-51.5, -3.55,'+');
         expected = -55.05;
         assertEquals(expected, actual, DELTA, ADDITION_MESSAGE);
     }
 
     @Test
-    void additionWithPositiveAndNegativeValues_Ok() throws ValidationException {
+    void calculate_additionWithPositiveAndNegativeValues_Ok() throws ValidationException {
         actual = calculator.calculate(13.67, -3.51,'+');
         expected = 10.16;
         assertEquals(expected, actual, DELTA, ADDITION_MESSAGE);
     }
 
     @Test
-    void additionWithZeroInDifferentPlaces_Ok() throws ValidationException {
+    void calculate_additionWithZeroInDifferentPlaces_Ok() throws ValidationException {
         actual = calculator.calculate(0, 25.3,'+');
         expected = 25.3;
         assertEquals(expected, actual, DELTA, ADDITION_MESSAGE);
@@ -60,21 +60,21 @@ class CalculatorTest {
     }
 
     @Test
-    void additionForMinAndMinValues_Ok() throws ValidationException {
+    void calculate_additionForMinAndMinValues_Ok() throws ValidationException {
         actual = calculator.calculate(Double.MIN_VALUE, Double.MIN_VALUE, '+');
         expected = Double.MIN_VALUE + Double.MIN_VALUE;
         assertEquals(expected, actual, DELTA, ADDITION_MESSAGE);
     }
 
     @Test
-    void additionForMinAndMaxValues_Ok() throws ValidationException {
+    void calculate_additionForMinAndMaxValues_Ok() throws ValidationException {
         actual = calculator.calculate(Double.MIN_VALUE, Double.MAX_VALUE, '+');
         expected = Double.MIN_VALUE + Double.MAX_VALUE;
         assertEquals(expected, actual, DELTA, ADDITION_MESSAGE);
     }
 
     @Test
-    void additionForMaxAndMaxValues_Ok() throws ValidationException {
+    void calculate_additionForMaxAndMaxValues_Ok() throws ValidationException {
         actual = calculator.calculate(Double.MAX_VALUE, Double.MAX_VALUE, '+');
         expected = Double.MAX_VALUE + Double.MAX_VALUE;
         assertEquals(expected, actual, DELTA, ADDITION_MESSAGE);
@@ -83,28 +83,28 @@ class CalculatorTest {
     // ----------Subtraction-----------
 
     @Test
-    void subtractionWithTwoPositiveValues_Ok() throws ValidationException {
+    void calculate_subtractionWithTwoPositiveValues_Ok() throws ValidationException {
         actual = calculator.calculate(10.1, 20.2,'-');
         expected = -10.1;
         assertEquals(expected, actual, DELTA, SUBTRACTION_MESSAGE);
     }
 
     @Test
-    void subtractionWithTwoNegativeValues_Ok() throws ValidationException {
+    void calculate_subtractionWithTwoNegativeValues_Ok() throws ValidationException {
         actual = calculator.calculate(-51.5, -3.55,'-');
         expected = -47.95;
         assertEquals(expected, actual, DELTA, SUBTRACTION_MESSAGE);
     }
 
     @Test
-    void subtractionWithPositiveAndNegativeValues_Ok() throws ValidationException {
+    void calculate_subtractionWithPositiveAndNegativeValues_Ok() throws ValidationException {
         actual = calculator.calculate(13.67, -3.51,'-');
         expected = 17.18;
         assertEquals(expected, actual, DELTA, SUBTRACTION_MESSAGE);
     }
 
     @Test
-    void subtractionWithZeroInDifferentPlaces_Ok() throws ValidationException {
+    void calculate_subtractionWithZeroInDifferentPlaces_Ok() throws ValidationException {
         actual = calculator.calculate(0, 25.3,'-');
         expected = -25.3;
         assertEquals(expected, actual, DELTA, SUBTRACTION_MESSAGE);
@@ -119,21 +119,21 @@ class CalculatorTest {
     }
 
     @Test
-    void subtractionForMinAndMinValues_Ok() throws ValidationException {
+    void calculate_subtractionForMinAndMinValues_Ok() throws ValidationException {
         actual = calculator.calculate(Double.MIN_VALUE, Double.MIN_VALUE, '-');
         expected = Double.MIN_VALUE - Double.MIN_VALUE;
         assertEquals(expected, actual, DELTA, SUBTRACTION_MESSAGE);
     }
 
     @Test
-    void subtractionForMinAndMaxValues_Ok() throws ValidationException {
+    void calculate_subtractionForMinAndMaxValues_Ok() throws ValidationException {
         actual = calculator.calculate(Double.MIN_VALUE, Double.MAX_VALUE, '-');
         expected = Double.MIN_VALUE - Double.MAX_VALUE;
         assertEquals(expected, actual, DELTA, SUBTRACTION_MESSAGE);
     }
 
     @Test
-    void subtractionForMaxAndMaxValues_Ok() throws ValidationException {
+    void calculate_subtractionForMaxAndMaxValues_Ok() throws ValidationException {
         actual = calculator.calculate(Double.MAX_VALUE, Double.MAX_VALUE, '-');
         expected = Double.MAX_VALUE - Double.MAX_VALUE;
         assertEquals(expected, actual, DELTA, SUBTRACTION_MESSAGE);
@@ -142,55 +142,55 @@ class CalculatorTest {
     // ------------Division------------
 
     @Test
-    void divisionWithTwoPositiveValues_Ok() throws ValidationException {
+    void calculate_divisionWithTwoPositiveValues_Ok() throws ValidationException {
         actual = calculator.calculate(10.1, 20.2,'/');
         expected = 0.5;
         assertEquals(expected, actual, DELTA, DIVISION_MESSAGE);
     }
 
     @Test
-    void divisionWithTwoNegativeValues_Ok() throws ValidationException {
+    void calculate_divisionWithTwoNegativeValues_Ok() throws ValidationException {
         actual = calculator.calculate(-51.5, -3.55,'/');
         expected = 14.507;
         assertEquals(expected, actual, DELTA, DIVISION_MESSAGE);
     }
 
     @Test
-    void divisionWithPositiveAndNegativeValues_Ok() throws ValidationException {
+    void calculate_divisionWithPositiveAndNegativeValues_Ok() throws ValidationException {
         actual = calculator.calculate(13.67, -3.51,'/');
         expected = -3.8945;
         assertEquals(expected, actual, DELTA, DIVISION_MESSAGE);
     }
 
     @Test
-    void divisionWithZeroInDifferentPlaces_Ok() throws ValidationException {
+    void calculate_divisionWithZeroInDifferentPlaces_Ok() throws ValidationException {
         actual = calculator.calculate(0, 25.3, '/');
         expected = 0;
         assertEquals(expected, actual, DELTA, DIVISION_MESSAGE);
     }
 
     @Test
-    void divisionWithZeroInDifferentPlaces_NotOk() {
+    void calculate_divisionWithZeroInDifferentPlaces_NotOk() {
         assertThrows(ArithmeticException.class, () -> calculator.calculate(21.15, 0, '/'));
         assertThrows(ArithmeticException.class, () -> calculator.calculate(0, 0, '/'));
     }
 
     @Test
-    void divisionForMinAndMaxValues_Ok() throws ValidationException {
+    void calculate_divisionForMinAndMaxValues_Ok() throws ValidationException {
         actual = calculator.calculate(Double.MIN_VALUE, Double.MAX_VALUE, '/');
         expected = Double.MIN_VALUE / Double.MAX_VALUE;
         assertEquals(expected, actual, DELTA, DIVISION_MESSAGE);
     }
 
     @Test
-    void divisionForMinAndMinValues_Ok() throws ValidationException {
+    void calculate_divisionForMinAndMinValues_Ok() throws ValidationException {
         actual = calculator.calculate(Double.MIN_VALUE, Double.MIN_VALUE, '/');
         expected = Double.MIN_VALUE / Double.MIN_VALUE;
         assertEquals(expected, actual, DELTA, DIVISION_MESSAGE);
     }
 
     @Test
-    void divisionForMaxAndMaxValues_Ok() throws ValidationException {
+    void calculate_divisionForMaxAndMaxValues_Ok() throws ValidationException {
         actual = calculator.calculate(Double.MAX_VALUE, Double.MAX_VALUE, '/');
         expected = Double.MAX_VALUE / Double.MAX_VALUE;
         assertEquals(expected, actual, DELTA, DIVISION_MESSAGE);
@@ -199,28 +199,28 @@ class CalculatorTest {
     // ---------Multiplication---------
 
     @Test
-    void multiplicationWithTwoPositiveValues_Ok() throws ValidationException {
+    void calculate_multiplicationWithTwoPositiveValues_Ok() throws ValidationException {
         actual = calculator.calculate(10.1, 20.2,'*');
         expected = 204.0199;
         assertEquals(expected, actual, DELTA, MULTIPLICATION_MESSAGE);
     }
 
     @Test
-    void multiplicationWithTwoNegativeValues_Ok() throws ValidationException {
+    void calculate_multiplicationWithTwoNegativeValues_Ok() throws ValidationException {
         actual = calculator.calculate(-51.5, -3.55,'*');
         expected = 182.825;
         assertEquals(expected, actual, DELTA, MULTIPLICATION_MESSAGE);
     }
 
     @Test
-    void multiplicationWithPositiveAndNegativeValues_Ok() throws ValidationException {
+    void calculate_multiplicationWithPositiveAndNegativeValues_Ok() throws ValidationException {
         actual = calculator.calculate(13.67, -3.51,'*');
         expected = -47.9817;
         assertEquals(expected, actual, DELTA, MULTIPLICATION_MESSAGE);
     }
 
     @Test
-    void multiplicationWithZeroInDifferentPlaces_Ok() throws ValidationException {
+    void calculate_multiplicationWithZeroInDifferentPlaces_Ok() throws ValidationException {
         actual = calculator.calculate(0, 25.3,'*');
         expected = 0;
         assertEquals(expected, actual, DELTA, MULTIPLICATION_MESSAGE);
@@ -235,21 +235,21 @@ class CalculatorTest {
     }
 
     @Test
-    void multiplicationForMinAndMaxValues_Ok() throws ValidationException {
+    void calculate_multiplicationForMinAndMaxValues_Ok() throws ValidationException {
         actual = calculator.calculate(Double.MIN_VALUE, Double.MAX_VALUE, '*');
         expected = Double.MIN_VALUE * Double.MAX_VALUE;
         assertEquals(expected, actual, DELTA, MULTIPLICATION_MESSAGE);
     }
 
     @Test
-    void multiplicationForMinAndMinValues_Ok() throws ValidationException {
+    void calculate_multiplicationForMinAndMinValues_Ok() throws ValidationException {
         actual = calculator.calculate(Double.MIN_VALUE, Double.MIN_VALUE, '*');
         expected = Double.MIN_VALUE * Double.MIN_VALUE;
         assertEquals(expected, actual, DELTA, MULTIPLICATION_MESSAGE);
     }
 
     @Test
-    void multiplicationForMaxAndMaxValues_Ok() throws ValidationException {
+    void calculate_multiplicationForMaxAndMaxValues_Ok() throws ValidationException {
         actual = calculator.calculate(Double.MAX_VALUE, Double.MAX_VALUE, '*');
         expected = Double.MAX_VALUE * Double.MAX_VALUE;
         assertEquals(expected, actual, DELTA, MULTIPLICATION_MESSAGE);
@@ -258,58 +258,65 @@ class CalculatorTest {
     // --------Rising to a power-------
 
     @Test
-    void raisingPositiveValueToTheNegativePower_Ok() throws ValidationException {
+    void calculate_raisingPositiveValueToTheNegativePower_Ok() throws ValidationException {
         actual = calculator.calculate(3,-5,'^');
         expected = 0.0041;
         assertEquals(expected, actual, DELTA, RAISING_TO_A_POWER_MESSAGE);
     }
 
     @Test
-    void raisingPositiveValueToThePositivePower_Ok() throws ValidationException {
+    void calculate_raisingPositiveValueToThePositivePower_Ok() throws ValidationException {
         actual = calculator.calculate(3,5,'^');
         expected = 243.0;
         assertEquals(expected, actual, DELTA, RAISING_TO_A_POWER_MESSAGE);
     }
 
     @Test
-    void raisingNegativeValueToThePositivePower_Ok() throws ValidationException {
+    void calculate_raisingNegativeValueToThePositivePower_Ok() throws ValidationException {
         actual = calculator.calculate(-3,5,'^');
         expected = -243.0;
         assertEquals(expected, actual, DELTA, RAISING_TO_A_POWER_MESSAGE);
     }
 
     @Test
-    void raisingNegativeValueToTheNegativePower_Ok() throws ValidationException {
+    void calculate_raisingNegativeValueToTheNegativePower_Ok() throws ValidationException {
         actual = calculator.calculate(-3,-5,'^');
         expected = -0.0041;
         assertEquals(expected, actual, DELTA, RAISING_TO_A_POWER_MESSAGE);
     }
 
     @Test
-    void raisingNegativeValueToZeroPower_Ok() throws ValidationException {
+    void calculate_raisingNegativeValueToZeroPower_Ok() throws ValidationException {
         actual = calculator.calculate(-3,0,'^');
         expected = 1;
         assertEquals(expected, actual, DELTA, RAISING_TO_A_POWER_MESSAGE);
     }
 
     @Test
-    void raisingPositiveValueToZeroPower_Ok() throws ValidationException {
+    void calculate_raisingPositiveValueToZeroPower_Ok() throws ValidationException {
         actual = calculator.calculate(3,0,'^');
         expected = 1;
         assertEquals(expected, actual, DELTA, RAISING_TO_A_POWER_MESSAGE);
     }
 
     @Test
-    void raisingZeroValueToAPower_Ok() throws ValidationException {
+    void calculate_raisingZeroValueToAPower_Ok() throws ValidationException {
         actual = calculator.calculate(0,5,'^');
         expected = 0;
+        assertEquals(expected, actual, DELTA, RAISING_TO_A_POWER_MESSAGE);
+    }
+
+    @Test
+    void calculate_raisingZeroValueToNegativePower_Ok() throws ValidationException {
+        actual = calculator.calculate(0,-5,'^');
+        expected = Double.POSITIVE_INFINITY;
         assertEquals(expected, actual, DELTA, RAISING_TO_A_POWER_MESSAGE);
     }
     // --------Rising to a power-------
     // -----------Other tests----------
 
     @Test
-    void calculateIllegalOperation_NotOk() {
+    void calculate_calculateIllegalOperation_NotOk() {
         assertThrows(ValidationException.class, () -> calculator.calculate(10.1, 20.2, '&'));
     }
 }
