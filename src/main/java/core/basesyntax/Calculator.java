89ac67923272a9728ba574a24e3cac072d930ecc
SolnalarmJulia <solnalarm@meta.ua>
2021-09-14 11:40:19 +0300

    refactoring calculate() method

--- src/main/java/core/basesyntax/Calculator.java
+++ src/main/java/core/basesyntax/Calculator.java
@@ -2,37 +2,38 @@ package core.basesyntax;
 
 public class Calculator {
 
-    public double calculate(double a, double b, char operation) throws IllegalArgumentException {
+    public double calculate(double firstOperand, double secondOperand, char operation) {
         switch (operation) {
             case '+':
-                return a + b;
+                return firstOperand + secondOperand;
             case '-':
-                return a - b;
+                return firstOperand - secondOperand;
             case '*':
-                return a * b;
+                return firstOperand * secondOperand;
             case '/':
-                return divide(a, b);
+                return divide(firstOperand, secondOperand);
             case '^':
-                return raiseValueToPower(a, b);
+                return raiseValueToPower(firstOperand, secondOperand);
             default:
                 throw new IllegalArgumentException("Operation " + operation
                         + " is not allowed. Please input operation from +, - , *, /, ^ ");
         }
     }
 
-    private double divide(double a, double b) {
-        if (b == Double.NEGATIVE_INFINITY || b == Double.POSITIVE_INFINITY || b == 0) {
-            throw new IllegalArgumentException("IllegalArgumentException is occured. "
+    private double divide(double firstOperand, double secondOperand) {
+        if (secondOperand == Double.NEGATIVE_INFINITY
+                || secondOperand == Double.POSITIVE_INFINITY || secondOperand == 0) {
+            throw new ArithmeticException("ArithmeticException is occured. "
                     + " Divider must not be 0.");
         }
-        return a / b;
+        return firstOperand / secondOperand;
     }
 
-    private double raiseValueToPower(double a, double b) {
-        if (a == 0 && b <= 0) {
+    private double raiseValueToPower(double firstOperand, double power) {
+        if (firstOperand == 0 && power <= 0) {
             throw new IllegalArgumentException("IllegalArgumentException is occured."
                     + " Power must be > 0. ");
         }
-        return Math.pow(a, b);
+        return Math.pow(firstOperand, power);
     }
 }
--- src/test/java/core/basesyntax/CalculatorTest.java
+++ src/test/java/core/basesyntax/CalculatorTest.java
@@ -17,301 +17,301 @@ class CalculatorTest {
 
     @Test
     void calculate_illegalOperation_notOk() {
-        double firstNumber = 5;
-        double secondNumber = 0;
+        double firstOperand = 5;
+        double secondOperand = 0;
         assertThrows(IllegalArgumentException.class, () -> {
-            calculator.calculate(firstNumber, secondNumber, '&');
+            calculator.calculate(firstOperand, secondOperand, '&');
         });
     }
 
     @Test
     void additionTwoPositive_Ok() {
-        double firstNumber = 10;
-        double secondNumber = 13;
-        assertEquals(23, calculator.calculate(firstNumber, secondNumber, '+'));
+        double firstOperand = 10;
+        double secondOperand = 13;
+        assertEquals(23, calculator.calculate(firstOperand, secondOperand, '+'));
     }
 
     @Test
     void additionTwoNegative_Ok() {
-        double firstNumber = -4;
-        double secondNumber = -100;
-        assertEquals(-104, calculator.calculate(firstNumber, secondNumber, '+'));
+        double firstOperand = -4;
+        double secondOperand = -100;
+        assertEquals(-104, calculator.calculate(firstOperand, secondOperand, '+'));
     }
 
     @Test
     void additionPositiveAndNegative_Ok() {
-        double firstNumber = 100;
-        double secondNumber = -56;
-        assertEquals(44, calculator.calculate(firstNumber, secondNumber, '+'));
+        double firstOperand = 100;
+        double secondOperand = -56;
+        assertEquals(44, calculator.calculate(firstOperand, secondOperand, '+'));
     }
 
     @Test
     void additionNegativeAndPositive_Ok() {
-        double firstNumber = -100;
-        double secondNumber = 56;
-        assertEquals(-44, calculator.calculate(firstNumber, secondNumber, '+'));
+        double firstOperand = -100;
+        double secondOperand = 56;
+        assertEquals(-44, calculator.calculate(firstOperand, secondOperand, '+'));
     }
 
     @Test
     void additionMinValues_Ok() {
-        double firstNumber = Double.MIN_VALUE;
-        double secondNumber = Double.MIN_VALUE;
-        assertEquals(0, calculator.calculate(firstNumber, secondNumber, '+'), DELTA);
+        double firstOperand = Double.MIN_VALUE;
+        double secondOperand = Double.MIN_VALUE;
+        assertEquals(0, calculator.calculate(firstOperand, secondOperand, '+'), DELTA);
     }
 
     @Test
     void additionMaxValues_Ok() {
-        double firstNumber = Double.MAX_VALUE;
-        double secondNumber = Double.MAX_VALUE;
-        assertEquals(Double.POSITIVE_INFINITY, calculator.calculate(firstNumber,
-                secondNumber, '+'), DELTA);
+        double firstOperand = Double.MAX_VALUE;
+        double secondOperand = Double.MAX_VALUE;
+        assertEquals(Double.POSITIVE_INFINITY, calculator.calculate(firstOperand,
+                secondOperand, '+'), DELTA);
     }
 
     @Test
     void additionMinAndMaxValues_Ok() {
-        double firstNumber = Double.MIN_VALUE;
-        double secondNumber = Double.MAX_VALUE;
-        assertEquals(Double.MAX_VALUE, calculator.calculate(firstNumber,
-                secondNumber, '+'), DELTA);
+        double firstOperand = Double.MIN_VALUE;
+        double secondOperand = Double.MAX_VALUE;
+        assertEquals(Double.MAX_VALUE, calculator.calculate(firstOperand,
+                secondOperand, '+'), DELTA);
     }
 
     @Test
     void additionWithZero_Ok() {
-        double firstNumber = 45;
-        double secondNumber = 0;
-        assertEquals(firstNumber, calculator.calculate(firstNumber, secondNumber, '+'));
+        double firstOperand = 45;
+        double secondOperand = 0;
+        assertEquals(firstOperand, calculator.calculate(firstOperand, secondOperand, '+'));
     }
 
     @Test
     void subtractionTwoPositive_Ok() {
-        double firstNumber = 30;
-        double secondNumber = 13;
-        assertEquals(17, calculator.calculate(firstNumber, secondNumber, '-'));
+        double firstOperand = 30;
+        double secondOperand = 13;
+        assertEquals(17, calculator.calculate(firstOperand, secondOperand, '-'));
     }
 
     @Test
     void subtractionTwoNegative_Ok() {
-        double firstNumber = -4;
-        double secondNumber = -100;
-        assertEquals(96, calculator.calculate(firstNumber, secondNumber, '-'));
+        double firstOperand = -4;
+        double secondOperand = -100;
+        assertEquals(96, calculator.calculate(firstOperand, secondOperand, '-'));
     }
 
     @Test
     void subtractionPositiveAndNegative_Ok() {
-        double firstNumber = 100;
-        double secondNumber = -56;
-        assertEquals(156, calculator.calculate(firstNumber, secondNumber, '-'));
+        double firstOperand = 100;
+        double secondOperand = -56;
+        assertEquals(156, calculator.calculate(firstOperand, secondOperand, '-'));
     }
 
     @Test
     void subtractionNegativeAndPositive_Ok() {
-        double firstNumber = -100;
-        double secondNumber = 56;
-        assertEquals(-156, calculator.calculate(firstNumber, secondNumber, '-'));
+        double firstOperand = -100;
+        double secondOperand = 56;
+        assertEquals(-156, calculator.calculate(firstOperand, secondOperand, '-'));
     }
 
     @Test
     void subtractionMinAndMaxDoubleValue_Ok() {
-        double firstNumber = Double.MIN_VALUE;
-        double secondNumber = Double.MAX_VALUE;
-        assertEquals(-Double.MAX_VALUE, calculator.calculate(firstNumber,
-                secondNumber, '-'), DELTA);
+        double firstOperand = Double.MIN_VALUE;
+        double secondOperand = Double.MAX_VALUE;
+        assertEquals(-Double.MAX_VALUE, calculator.calculate(firstOperand,
+                secondOperand, '-'), DELTA);
     }
 
     @Test
     void subtractionMinValues_Ok() {
-        double firstNumber = Double.MIN_VALUE;
-        double secondNumber = Double.MIN_VALUE;
-        assertEquals(0, calculator.calculate(firstNumber,
-                secondNumber, '-'), DELTA);
+        double firstOperand = Double.MIN_VALUE;
+        double secondOperand = Double.MIN_VALUE;
+        assertEquals(0, calculator.calculate(firstOperand,
+                secondOperand, '-'), DELTA);
     }
 
     @Test
     void subtractionMaxValues_Ok() {
-        double firstNumber = Double.MAX_VALUE;
-        double secondNumber = Double.MAX_VALUE;
-        assertEquals(0, calculator.calculate(firstNumber, secondNumber, '-'), DELTA);
+        double firstOperand = Double.MAX_VALUE;
+        double secondOperand = Double.MAX_VALUE;
+        assertEquals(0, calculator.calculate(firstOperand, secondOperand, '-'), DELTA);
     }
 
     @Test
     void subtractionWithZero_Ok() {
-        double firstNumber = 45;
-        double secondNumber = 0;
-        assertEquals(firstNumber, calculator.calculate(firstNumber, secondNumber, '-'));
+        double firstOperand = 45;
+        double secondOperand = 0;
+        assertEquals(firstOperand, calculator.calculate(firstOperand, secondOperand, '-'));
     }
 
     @Test
     void multiplicationTwoPositive_Ok() {
-        double firstNumber = 10.01;
-        double secondNumber = 13.02;
-        assertEquals(130.3302, calculator.calculate(firstNumber, secondNumber, '*'));
+        double firstOperand = 10.01;
+        double secondOperand = 13.02;
+        assertEquals(130.3302, calculator.calculate(firstOperand, secondOperand, '*'));
     }
 
     @Test
     void multiplicationTwoNegative_Ok() {
-        double firstNumber = -4;
-        double secondNumber = -100.02;
-        assertEquals(400.08, calculator.calculate(firstNumber, secondNumber, '*'));
+        double firstOperand = -4;
+        double secondOperand = -100.02;
+        assertEquals(400.08, calculator.calculate(firstOperand, secondOperand, '*'));
     }
 
     @Test
     void multiplicationPositiveAndNegative_Ok() {
-        double firstNumber = 100.2;
-        double secondNumber = -56;
-        assertEquals(-5611.2, calculator.calculate(firstNumber, secondNumber, '*'));
+        double firstOperand = 100.2;
+        double secondOperand = -56;
+        assertEquals(-5611.2, calculator.calculate(firstOperand, secondOperand, '*'));
     }
 
     @Test
     void multiplicationNegativeAndPositive_Ok() {
-        double firstNumber = -100.2;
-        double secondNumber = 56;
-        assertEquals(-5611.2, calculator.calculate(firstNumber, secondNumber, '*'));
+        double firstOperand = -100.2;
+        double secondOperand = 56;
+        assertEquals(-5611.2, calculator.calculate(firstOperand, secondOperand, '*'));
     }
 
     @Test
     void multiplicationWithZero_Ok() {
-        double firstNumber = 45;
-        double secondNumber = 0;
-        assertEquals(0, calculator.calculate(firstNumber, secondNumber, '*'));
+        double firstOperand = 45;
+        double secondOperand = 0;
+        assertEquals(0, calculator.calculate(firstOperand, secondOperand, '*'));
     }
 
     @Test
     void multiplicationMinAndMaxDoubleValues_Ok() {
-        double firstNumber = Double.MIN_VALUE;
-        double secondNumber = Double.MAX_VALUE;
-        assertEquals(0, calculator.calculate(firstNumber, secondNumber, '*'), DELTA);
+        double firstOperand = Double.MIN_VALUE;
+        double secondOperand = Double.MAX_VALUE;
+        assertEquals(0, calculator.calculate(firstOperand, secondOperand, '*'), DELTA);
     }
 
     @Test
     void multiplicationMinDoubleValues_Ok() {
-        double firstNumber = Double.MIN_VALUE;
-        double secondNumber = Double.MIN_VALUE;
-        assertEquals(0, calculator.calculate(firstNumber, secondNumber, '*'), DELTA);
+        double firstOperand = Double.MIN_VALUE;
+        double secondOperand = Double.MIN_VALUE;
+        assertEquals(0, calculator.calculate(firstOperand, secondOperand, '*'), DELTA);
     }
 
     @Test
     void multiplicationMaxDoubleValues_Ok() {
-        double firstNumber = Double.MAX_VALUE;
-        double secondNumber = Double.MAX_VALUE;
-        assertEquals(Double.POSITIVE_INFINITY, calculator.calculate(firstNumber,
-                secondNumber, '*'), DELTA);
+        double firstOperand = Double.MAX_VALUE;
+        double secondOperand = Double.MAX_VALUE;
+        assertEquals(Double.POSITIVE_INFINITY, calculator.calculate(firstOperand,
+                secondOperand, '*'), DELTA);
     }
 
     @Test
     void divisionTwoPositive_Ok() {
-        double firstNumber = 130;
-        double secondNumber = 13;
-        assertEquals(10, calculator.calculate(firstNumber, secondNumber, '/'));
+        double firstOperand = 130;
+        double secondOperand = 13;
+        assertEquals(10, calculator.calculate(firstOperand, secondOperand, '/'));
     }
 
     @Test
     void divisionTwoNegative_Ok() {
-        double firstNumber = -4;
-        double secondNumber = -2;
-        assertEquals(2, calculator.calculate(firstNumber, secondNumber, '/'));
+        double firstOperand = -4;
+        double secondOperand = -2;
+        assertEquals(2, calculator.calculate(firstOperand, secondOperand, '/'));
     }
 
     @Test
     void divisionPositiveAndNegative_Ok() {
-        double firstNumber = 100;
-        double secondNumber = -20;
-        assertEquals(-5, calculator.calculate(firstNumber, secondNumber, '/'));
+        double firstOperand = 100;
+        double secondOperand = -20;
+        assertEquals(-5, calculator.calculate(firstOperand, secondOperand, '/'));
     }
 
     @Test
     void divisionNegativeAndPositive_Ok() {
-        double firstNumber = -100;
-        double secondNumber = 20;
-        assertEquals(-5, calculator.calculate(firstNumber, secondNumber, '/'));
+        double firstOperand = -100;
+        double secondOperand = 20;
+        assertEquals(-5, calculator.calculate(firstOperand, secondOperand, '/'));
     }
 
     @Test
     void divisionWithZero_Ok() {
-        double firstNumber = 0;
-        double secondNumber = 2;
-        assertEquals(firstNumber, calculator.calculate(firstNumber, secondNumber, '/'));
+        double firstOperand = 0;
+        double secondOperand = 2;
+        assertEquals(firstOperand, calculator.calculate(firstOperand, secondOperand, '/'));
     }
 
     @Test
     void divisionMinByMaxDoubleValues_Ok() {
-        double firstNumber = Double.MIN_VALUE;
-        double secondNumber = Double.MAX_VALUE;
-        assertEquals(0, calculator.calculate(firstNumber,
-                secondNumber, '/'), DELTA);
+        double firstOperand = Double.MIN_VALUE;
+        double secondOperand = Double.MAX_VALUE;
+        assertEquals(0, calculator.calculate(firstOperand,
+                secondOperand, '/'), DELTA);
     }
 
     @Test
     void divisionMaxByMinDoubleValues_Ok() {
-        double firstNumber = Double.MAX_VALUE;
-        double secondNumber = Double.MIN_VALUE;
-        assertEquals(Double.POSITIVE_INFINITY, calculator.calculate(firstNumber,
-                secondNumber, '/'), DELTA);
+        double firstOperand = Double.MAX_VALUE;
+        double secondOperand = Double.MIN_VALUE;
+        assertEquals(Double.POSITIVE_INFINITY, calculator.calculate(firstOperand,
+                secondOperand, '/'), DELTA);
     }
 
     @Test
     void divisionByZero_NotOk() {
-        double firstNumber = 5;
-        double secondNumber = 0;
-        assertThrows(IllegalArgumentException.class, () -> {
-            calculator.calculate(firstNumber, secondNumber, '/');
+        double firstOperand = 5;
+        double secondOperand = 0;
+        assertThrows(ArithmeticException.class, () -> {
+            calculator.calculate(firstOperand, secondOperand, '/');
         });
     }
 
     @Test
     void raisingPositiveToPositivePower_Ok() {
-        double firstNumber = 4;
+        double firstOperand = 4;
         double power = 2;
-        assertEquals(16, calculator.calculate(firstNumber, power, '^'));
+        assertEquals(16, calculator.calculate(firstOperand, power, '^'));
     }
 
     @Test
     void raisingNegativeToPositivePower_Ok() {
-        double firstNumber = -2;
+        double firstOperand = -2;
         double power = 3;
-        assertEquals(-8, calculator.calculate(firstNumber, power, '^'));
+        assertEquals(-8, calculator.calculate(firstOperand, power, '^'));
     }
 
     @Test
     void raisingPositiveToNegativePower_Ok() {
-        double firstNumber = 4;
+        double firstOperand = 4;
         double power = -2;
-        assertEquals(0.0625, calculator.calculate(firstNumber, power, '^'));
+        assertEquals(0.0625, calculator.calculate(firstOperand, power, '^'));
     }
 
     @Test
     void raisingNegativeToNegativePower_Ok() {
-        double firstNumber = -2;
+        double firstOperand = -2;
         double power = -2;
-        assertEquals(0.25, calculator.calculate(firstNumber, power, '^'));
+        assertEquals(0.25, calculator.calculate(firstOperand, power, '^'));
     }
 
     @Test
     void raisingPositiveToZeroPower_Ok() {
-        double firstNumber = -2;
+        double firstOperand = -2;
         double power = 0;
-        assertEquals(1, calculator.calculate(firstNumber, power, '^'));
+        assertEquals(1, calculator.calculate(firstOperand, power, '^'));
     }
 
     @Test
     void raisingNegativeToZeroPower_Ok() {
-        double firstNumber = -2;
+        double firstOperand = -2;
         double power = 0;
-        assertEquals(1, calculator.calculate(firstNumber, power, '^'));
+        assertEquals(1, calculator.calculate(firstOperand, power, '^'));
     }
 
     @Test
     void raisingZeroToPositivePower_Ok() {
-        double firstNumber = 0;
+        double firstOperand = 0;
         double power = 67;
-        assertEquals(0, calculator.calculate(firstNumber, power, '^'));
+        assertEquals(0, calculator.calculate(firstOperand, power, '^'));
     }
 
     @Test
     void raisingZeroToNegativePower_NotOk() {
-        double firstNumber = 0;
+        double firstOperand = 0;
         double power = -2;
         assertThrows(IllegalArgumentException.class, () -> {
-            calculator.calculate(firstNumber, power, '^');
+            calculator.calculate(firstOperand, power, '^');
         });
     }
 }
